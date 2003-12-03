a1ad8da3e05b81a8c451a6ffd04f50c3d35fd0eb
bhenden <devnull@localhost>
2003-12-03 18:34:44 +0000

    New class.

---
+++ yafsrc/classes/AdminPage.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace yaf
+{
+	/// <summary>
+	/// Summary description for AdminPage.
+	/// </summary>
+	public class AdminPage : pages.ForumPage
+	{
+		public AdminPage() : base(null)
+		{
+			this.Load += new EventHandler(AdminPage_Load);
+		}
+
+		private void AdminPage_Load(object sender,EventArgs e)
+		{
+			if(!IsAdmin)
+				Data.AccessDenied();
+
+#if false
+			if(!IsPostBack)
+			{
+				controls.PageLinks ctl = new controls.PageLinks();
+				ctl.AddLink(Config.BoardSettings.Name,Forum.GetLink(Pages.forum));
+				ctl.AddLink("Administration",Forum.GetLink(Pages.admin_admin));
+				Controls.AddAt(0,ctl);
+			}
+#endif
+		}
+	}
+}
