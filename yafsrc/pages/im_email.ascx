d7d8bc13c37fbbda924aca17ee9332077c9913e8
bhenden <devnull@localhost>
2003-12-21 19:54:39 +0000

    New file

---
+++ yafsrc/pages/im_email.ascx
@@ -0,0 +1,27 @@
+<%@ Control language="c#" Codebehind="im_email.ascx.cs" AutoEventWireup="false" Inherits="yaf.pages.im_email" %>
+<%@ Register TagPrefix="yaf" Namespace="yaf.controls" Assembly="yaf" %>
+
+<yaf:PageLinks runat="server" id="PageLinks"/>
+
+	<table class="content" width="400px" border="0" cellpadding="0" cellspacing="1" align="center">
+	<tr class="header2">
+		<td colspan="2"><%=GetText("TITLE")%></td>
+	</tr>
+	<tr>
+		<td class="postheader"><%=GetText("SUBJECT")%></td>
+		<td class="post"><asp:textbox runat="server" id="Subject"/></td>
+	</tr>
+	<tr>
+		<td class="postheader" valign='top'><%=GetText("BODY")%></td>
+		<td class="post">
+			<asp:textbox runat="server" id="Body" textmode="multiline" rows='10' style='width:100%'/>
+		</td>
+	</tr>
+	<tr class="postfooter">
+		<td colspan="2" align="center">
+			<asp:button runat="server" id="Send"/>
+		</td>
+	</tr>
+	</table>
+
+<yaf:savescrollpos runat="server"/>
---
+++ yafsrc/pages/im_email.ascx.cs
@@ -0,0 +1,106 @@
+/* Yet Another Forum.net
+ * Copyright (C) 2003 Bjørnar Henden
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace yaf.pages
+{
+	/// <summary>
+	/// Summary description for active.
+	/// </summary>
+	public class im_email : ForumPage
+	{
+		protected controls.PageLinks PageLinks;
+		protected Button Send;
+		protected TextBox Subject, Body;
+
+		public im_email() : base("IM_EMAIL")
+		{
+		}
+
+		private void Page_Load(object sender, System.EventArgs e)
+		{
+			if(!User.IsAuthenticated)
+				Data.AccessDenied();
+
+			if(!IsPostBack) 
+			{
+				using(DataTable dt=DB.user_list(PageBoardID,Request.QueryString["u"],null)) 
+				{
+					foreach(DataRow row in dt.Rows) 
+					{
+						PageLinks.AddLink(Config.BoardSettings.Name,Forum.GetLink(Pages.forum));
+						PageLinks.AddLink(row["Name"].ToString(),Forum.GetLink(Pages.profile,"u={0}",row["UserID"]));
+						PageLinks.AddLink(GetText("TITLE"),Forum.GetLink(Pages.im_email,"u={0}",row["UserID"]));
+						break;
+					}
+				}
+				Send.Text = GetText("SEND");
+			}
+		}
+
+		private void Send_Click(object sender,EventArgs e)
+		{
+			try
+			{
+				string	from = string.Empty, to = string.Empty;
+				using(DataTable dt=DB.user_list(PageBoardID,Request.QueryString["u"],null)) 
+				{
+					foreach(DataRow row in dt.Rows) 
+					{
+						to = string.Format("{0} ({1})",row["Email"],row["Name"]);
+						break;
+					}
+				}
+				using(DataTable dt=DB.user_list(PageBoardID,PageUserID,null)) 
+				{
+					foreach(DataRow row in dt.Rows) 
+					{
+						from = string.Format("{0} ({1})",row["Email"],row["Name"]);
+						break;
+					}
+				}
+				Utils.SendMail(from,to,Subject.Text,Body.Text);
+				Forum.Redirect(Pages.profile,"u={0}",Request.QueryString["u"]);
+			}
+			catch(Exception x) 
+			{
+				if(IsAdmin)
+					AddLoadMessage(x.Message);
+				else
+					AddLoadMessage(GetText("ERROR"));
+			}
+		}
+
+		override protected void OnInit(EventArgs e)
+		{
+			this.Load += new System.EventHandler(this.Page_Load);
+			this.Send.Click += new EventHandler(Send_Click);
+			base.OnInit(e);
+		}
+	}
+}
