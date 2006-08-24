6586acdd91f302252034c1651eb9800fe56f66dc
Jaben <jaben@yetanotherforum.net>
2006-08-24 13:05:18 +0000

    proper file

--- yafsrc/pages/login.ascx.designer.cs
+++ yafsrc/pages/login.ascx.designer.cs
@@ -12,16 +12,7 @@ namespace yaf.pages {
     
     public partial class login {
         protected yaf.controls.PageLinks PageLinks;
-        protected System.Web.UI.HtmlControls.HtmlTable LoginView;
-        protected System.Web.UI.WebControls.TextBox UserName;
-        protected System.Web.UI.WebControls.TextBox Password;
-        protected System.Web.UI.WebControls.CheckBox AutoLogin;
-        protected System.Web.UI.WebControls.Button ForumLogin;
-        protected System.Web.UI.WebControls.Button LostPassword;
-        protected System.Web.UI.HtmlControls.HtmlTable RecoverView;
-        protected System.Web.UI.WebControls.TextBox LostUserName;
-        protected System.Web.UI.WebControls.TextBox LostEmail;
-        protected System.Web.UI.WebControls.Button Recover;
+        protected System.Web.UI.WebControls.Login Login1;
         protected yaf.controls.SmartScroller SmartScroller1;
     }
 }
