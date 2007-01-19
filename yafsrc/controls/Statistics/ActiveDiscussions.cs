359cb82e466074b6bab74068c3ef6938d46f3ff9
Jaben <jaben@yetanotherforum.net>
2007-01-19 13:24:56 +0000

    fixed

--- yafsrc/controls/Statistics/ActiveDiscussions.cs
+++ yafsrc/controls/Statistics/ActiveDiscussions.cs
@@ -7,66 +7,67 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using YAF.Classes.Utils;
 
 namespace YAF.Controls.Statistics
 {
-    [ToolboxData("<{0}:ActiveDiscussions runat=\"server\"></{0}:ActiveDiscussions>")]
-    public class ActiveDiscussions : BaseControl
-    {
-        private int _displayNumber = 10;
+	[ToolboxData( "<{0}:ActiveDiscussions runat=\"server\"></{0}:ActiveDiscussions>" )]
+	public class ActiveDiscussions : YAF.Classes.Base.BaseControl
+	{
+		private int _displayNumber = 10;
 
-        /// <summary>
-        /// The default constructor for ActiveDiscussions.
-        /// </summary>
-        public ActiveDiscussions()
-        {
+		/// <summary>
+		/// The default constructor for ActiveDiscussions.
+		/// </summary>
+		public ActiveDiscussions()
+		{
 
-        }
+		}
 
-        public int DisplayNumber
-        {
-            get
-            {
-                return _displayNumber;
-            }
-            set
-            {
-                _displayNumber = value;
-            }
-        }
+		public int DisplayNumber
+		{
+			get
+			{
+				return _displayNumber;
+			}
+			set
+			{
+				_displayNumber = value;
+			}
+		}
 
-        /// <summary>
-        /// Renders the ActiveDiscussions class.
-        /// </summary>
-        /// <param name="writer"></param>
-        protected override void Render(System.Web.UI.HtmlTextWriter writer)
-        {
-            string htmlOutput = "";
+		/// <summary>
+		/// Renders the ActiveDiscussions class.
+		/// </summary>
+		/// <param name="writer"></param>
+		protected override void Render( System.Web.UI.HtmlTextWriter writer )
+		{
+			string htmlOutput = "";
 
-            htmlOutput += "<table width=\"90%\" class=\"content\" cellspacing=\"1\" border=\"0\" cellpadding=\"0\">";
-            htmlOutput += "<tr><td class=\"header1\">" + ForumPage.GetText("LATEST_POSTS") + "</td></tr>";
-            System.Data.DataTable dt = YAF.Classes.Data.DB.topic_latest(ForumPage.PageBoardID, 7, ForumPage.PageUserID);
-            int i = 1;
+			htmlOutput += "<table width=\"90%\" class=\"content\" cellspacing=\"1\" border=\"0\" cellpadding=\"0\">";
+			htmlOutput += "<tr><td class=\"header1\">" + PageContext.Localization.GetText( "LATEST_POSTS" ) + "</td></tr>";
+			System.Data.DataTable dt = YAF.Classes.Data.DB.topic_latest( PageContext.PageBoardID, 7, PageContext.PageUserID );
+			int i = 1;
 
-            htmlOutput += "<tr><td class=post><table cellspacing=0 cellpadding=0 align=center>";
+			htmlOutput += "<tr><td class=post><table cellspacing=0 cellpadding=0 align=center>";
 
-            foreach (System.Data.DataRow r in dt.Rows)
-            {
-                i++;
-                htmlOutput += "<tr class=\"post\">";
-                
-                htmlOutput += "<td width=\"75%\">";
-                //Output Topic Link
-                htmlOutput += string.Format("&nbsp;<a href=\"{1}\">{0}</a></td>", YAF.Utils.BadWordReplace(Convert.ToString(r["Topic"])), YAF.Forum.GetLink(YAF.ForumPages.posts,"m={0}#{0}",r["LastMessageID"]));
-                //Output Message Icon
-                htmlOutput += "<img src=\"" + ForumPage.GetThemeContents("ICONS","ICON_LATEST") + "\" border=\"0\" alt=\"\"></a>";
-                htmlOutput += "</td></tr>";
-            }
+			foreach ( System.Data.DataRow r in dt.Rows )
+			{
+				i++;
+				htmlOutput += "<tr class=\"post\">";
 
-            htmlOutput += "</table></td></tr>";
+				htmlOutput += "<td width=\"75%\">";
+				//Output Topic Link
+				htmlOutput += string.Format( "&nbsp;<a href=\"{1}\">{0}</a></td>", General.BadWordReplace( Convert.ToString( r ["Topic"] ) ), yaf_BuildLink.GetLink( ForumPages.posts, "m={0}#{0}", r ["LastMessageID"] ) );
+				//Output Message Icon
+				htmlOutput += "<img src=\"" + PageContext.Theme.GetItem( "ICONS", "ICON_LATEST" ) + "\" border=\"0\" alt=\"\"></a>";
+				htmlOutput += "</td></tr>";
+			}
 
-            htmlOutput += "</table>";
-            writer.Write(htmlOutput);
-        }
-    }
+			htmlOutput += "</table></td></tr>";
+
+			htmlOutput += "</table>";
+			writer.Write( htmlOutput );
+		}
+	}
 }
