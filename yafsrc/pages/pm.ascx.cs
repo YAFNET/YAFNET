4029882b63a16fec6fa960b7faffb0170f43234e
Jaben <jaben@yetanotherforum.net>
2007-08-24 12:31:33 +0000

    Reformatted the code.

--- yafsrc/pages/pm.ascx.cs
+++ yafsrc/pages/pm.ascx.cs
@@ -32,82 +32,84 @@ using YAF.Controls;
 
 namespace YAF.Pages
 {
-    // Note: As of AjaxControlToolkit v1.0.10301.0 (build 10531), there is a bug in the Tabs control that prevents some postbacks from 
-    // remembering the previous active tab.  
-    // See here: http://www.codeplex.com/WorkItem/View.aspx?ProjectName=AtlasControlToolkit&WorkItemId=8255
-    //
-    // Workaround: 
-    // Wrapping the Tabs control in an UpdatePanel, as suggested in forum 
-    // discussion: http://forums.asp.net/p/1068120/1579184.aspx
+  // Note: As of AjaxControlToolkit v1.0.10301.0 (build 10531), there is a bug in the Tabs control that prevents some postbacks from 
+  // remembering the previous active tab.  
+  // See here: http://www.codeplex.com/WorkItem/View.aspx?ProjectName=AtlasControlToolkit&WorkItemId=8255
+  //
+  // Workaround: 
+  // Wrapping the Tabs control in an UpdatePanel, as suggested in forum 
+  // discussion: http://forums.asp.net/p/1068120/1579184.aspx
 
 
-    public partial class pm : Classes.Base.ForumPage
+  public partial class pm : Classes.Base.ForumPage
+  {
+    private PMView _View;
+
+    public pm()
+      : base( "PM" )
+    { }
+
+    protected void Page_Load( object sender, EventArgs e )
     {
-        private PMView _View;
+      if ( User == null || PageContext.IsGuest )
+        yaf_BuildLink.Redirect( ForumPages.login, "ReturnUrl={0}", General.GetSafeRawUrl() );
 
-        public pm()
-            : base("PM")
-        {}
+      // check if this feature is disabled
+      if ( !PageContext.BoardSettings.AllowPrivateMessages )
+        yaf_BuildLink.Redirect( ForumPages.info, "i=5" );
 
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (User == null)
-                yaf_BuildLink.Redirect(ForumPages.login, "ReturnUrl={0}", General.GetSafeRawUrl());
-            if (PageContext.IsGuest)
-                yaf_BuildLink.Redirect(ForumPages.login, "ReturnUrl={0}", General.GetSafeRawUrl());
+      if ( !IsPostBack )
+      {
+        _View = PMView.FromQueryString( Request.QueryString ["v"] );
+        if ( _View == PMView.Inbox )
+          this.PMTabs.ActiveTab = this.InboxTab;
+        else if ( _View == PMView.Outbox )
+          this.PMTabs.ActiveTab = this.OutboxTab;
+        else if ( _View == PMView.Archive )
+          this.PMTabs.ActiveTab = this.ArchiveTab;
 
-            if (!IsPostBack)
-            {
-                _View = PMView.FromQueryString(Request.QueryString["v"]);
-                if (_View == PMView.Inbox)
-                    this.PMTabs.ActiveTab = this.InboxTab;
-                else if (_View == PMView.Outbox)
-                    this.PMTabs.ActiveTab = this.OutboxTab;
-                else if (_View == PMView.Archive)
-                    this.PMTabs.ActiveTab = this.ArchiveTab;
+        PageLinks.AddLink( PageContext.BoardSettings.Name, yaf_BuildLink.GetLink( ForumPages.forum ) );
+        PageLinks.AddLink( PageContext.PageUserName, yaf_BuildLink.GetLink( ForumPages.cp_profile ) );
+        PageLinks.AddLink( GetText( "TITLE" ) );
 
-                PageLinks.AddLink(PageContext.BoardSettings.Name, yaf_BuildLink.GetLink(ForumPages.forum));
-                PageLinks.AddLink(PageContext.PageUserName, yaf_BuildLink.GetLink(ForumPages.cp_profile));
-                PageLinks.AddLink(GetText("TITLE"));
+        InboxTab.HeaderText = GetText( "INBOX" );
+        OutboxTab.HeaderText = GetText( "SENTITEMS" );
+        ArchiveTab.HeaderText = GetText( "ARCHIVE" );
 
-                InboxTab.HeaderText = GetText("INBOX");
-                OutboxTab.HeaderText = GetText("SENTITEMS");
-                ArchiveTab.HeaderText = GetText("ARCHIVE");
+        NewMessageLink.NavigateUrl = yaf_BuildLink.GetLink( ForumPages.pmessage );
+        NewMessageLink.Text = GetText( "NEW_MESSAGE" );
 
-                NewMessageLink.NavigateUrl = yaf_BuildLink.GetLink(ForumPages.pmessage);
-                NewMessageLink.Text = GetText("NEW_MESSAGE");
-
-                // DOCTYPE COMPATIBILITY ISSUE
-                // Adding this css style block to the page's output fixes rendering of
-                // tabs in Internet Explorer quirks mode.
-                // If the page's DOCTYPE were set to XHTML 1.0 Transition, this call would not be needed.
-                // This must be attached to the PreRenderComplete event handler so that the css block
-                // is registered *after* the ASP.NET AJAX Control Toolkit adds in it's HTML link reference to
-                // its default CSS styles.  This is necessary in order for the styles defined below to override the
-                // default ones.
-                this.Page.PreRenderComplete += delegate(object s, EventArgs evt)
-                   {
-                       this.RegisterClientCssBlock(
-                           ".ajax__tab_default .ajax__tab_inner {height : 100%;}" + "\n" +
-                           ".ajax__tab_default .ajax__tab_tab {height : 100%;}" + "\n" +
-                           "\n" +
-                           ".ajax__tab_xp .ajax__tab_hover .ajax__tab_tab {height : 100%;}" +
-                           "\n" +
-                           ".ajax__tab_xp .ajax__tab_active .ajax__tab_tab {height : 100%;}" +
-                           "\n" +
-                           "\n" +
-                           ".ajax__tab_xp .ajax__tab_inner {height : 100%;}" + "\n" +
-                           ".ajax__tab_xp .ajax__tab_tab {height : 100%;}" + "\n" +
-                           ".ajax__tab_xp .ajax__tab_hover .ajax__tab_inner {height : 100%;}" +
-                           "\n" +
-                           ".ajax__tab_xp .ajax__tab_active .ajax__tab_inner {height : 100%;}");
-                   };
-            }
-        }
-
-        protected PMView View
-        {
-            get { return _View; }
-        }
+        // DOCTYPE COMPATIBILITY ISSUE
+        // Adding this css style block to the page's output fixes rendering of
+        // tabs in Internet Explorer quirks mode.
+        // If the page's DOCTYPE were set to XHTML 1.0 Transition, this call would not be needed.
+        // This must be attached to the PreRenderComplete event handler so that the css block
+        // is registered *after* the ASP.NET AJAX Control Toolkit adds in it's HTML link reference to
+        // its default CSS styles.  This is necessary in order for the styles defined below to override the
+        // default ones.
+        this.Page.PreRenderComplete += delegate( object s, EventArgs evt )
+           {
+             this.RegisterClientCssBlock(
+                 ".ajax__tab_default .ajax__tab_inner {height : 100%;}" + "\n" +
+                 ".ajax__tab_default .ajax__tab_tab {height : 100%;}" + "\n" +
+                 "\n" +
+                 ".ajax__tab_xp .ajax__tab_hover .ajax__tab_tab {height : 100%;}" +
+                 "\n" +
+                 ".ajax__tab_xp .ajax__tab_active .ajax__tab_tab {height : 100%;}" +
+                 "\n" +
+                 "\n" +
+                 ".ajax__tab_xp .ajax__tab_inner {height : 100%;}" + "\n" +
+                 ".ajax__tab_xp .ajax__tab_tab {height : 100%;}" + "\n" +
+                 ".ajax__tab_xp .ajax__tab_hover .ajax__tab_inner {height : 100%;}" +
+                 "\n" +
+                 ".ajax__tab_xp .ajax__tab_active .ajax__tab_inner {height : 100%;}" );
+           };
+      }
     }
+
+    protected PMView View
+    {
+      get { return _View; }
+    }
+  }
 }
