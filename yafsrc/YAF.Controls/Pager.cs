144568d7352951def5fb25b40a439ea12b19244f
Jaben <jaben@yetanotherforum.net>
2007-08-24 12:50:00 +0000

    Added ability to NOT use postback.

--- yafsrc/YAF.Controls/Pager.cs
+++ yafsrc/YAF.Controls/Pager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using YAF.Classes.Utils;
 
 namespace YAF.Controls
 {
@@ -9,64 +10,141 @@ namespace YAF.Controls
 	/// </summary>
 	public class Pager : BaseControl, System.Web.UI.IPostBackEventHandler
 	{
+		private bool usePostBack = true;
+
 		public Pager()
 		{
-			this.Load += new EventHandler(Pager_Load);
+			this.Load += new EventHandler( Pager_Load );
 		}
 
-		private void Pager_Load(object sender,EventArgs e)
+		private void Pager_Load( object sender, EventArgs e )
 		{
 		}
 
-		protected override void Render(HtmlTextWriter output)
+		protected override void Render( HtmlTextWriter output )
 		{
-			if(LinkedPager!=null)
+			if ( LinkedPager != null )
 			{
-				Pager linkedPager = (Pager)Parent.FindControl(LinkedPager);
-				if(linkedPager==null)
-					throw new Exception(string.Format("Failed to link pager to '{0}'.",LinkedPager));
-				linkedPager.Render(output);
+				Pager linkedPager = ( Pager ) Parent.FindControl( LinkedPager );
+				if ( linkedPager == null )
+					throw new Exception( string.Format( "Failed to link pager to '{0}'.", LinkedPager ) );
+				linkedPager.Render( output );
 				return;
 			}
-				
-			if(PageCount<2) return;
 
-			output.WriteLine("<span>");
-			output.WriteLine("{0:N0} pages:",PageCount);
+			if ( PageCount < 2 ) return;
+
+			output.WriteLine( "<span>" );
+			output.WriteLine( "{0:N0} pages:", PageCount );
+
+			if ( UsePostBack ) OutputPostback( output );
+			else OutputLinked( output );
+
+			output.WriteLine( "</span>" );
+		}
+
+		private void OutputPostback( HtmlTextWriter output )
+		{
 			int iStart = CurrentPageIndex - 6;
 			int iEnd = CurrentPageIndex + 7;
-			if(iStart<0) iStart = 0;
-			if(iEnd>PageCount) iEnd = PageCount;
+			if ( iStart < 0 ) iStart = 0;
+			if ( iEnd > PageCount ) iEnd = PageCount;
 
-			if(iStart>0)
-				output.WriteLine("<a href=\"{0}\">First</a> ...",Page.ClientScript.GetPostBackClientHyperlink(this,"0"));
+			if ( iStart > 0 )
+				output.WriteLine( "<a href=\"{0}\">First</a> ...", Page.ClientScript.GetPostBackClientHyperlink( this, "0" ) );
 
-			for(int i=iStart;i<iEnd;i++)
+			for ( int i = iStart; i < iEnd; i++ )
 			{
-				if(i==CurrentPageIndex)
-					output.WriteLine("[{0}]",i+1);
+				if ( i == CurrentPageIndex )
+					output.WriteLine( "[{0}]", i + 1 );
 				else
 					output.WriteLine( "<a href=\"{0}\">{1}</a>", Page.ClientScript.GetPostBackClientHyperlink( this, i.ToString() ), i + 1 );
 			}
 
-			if(iEnd<PageCount)
+			if ( iEnd < PageCount )
 				output.WriteLine( "... <a href=\"{0}\">Last</a>", Page.ClientScript.GetPostBackClientHyperlink( this, ( PageCount - 1 ).ToString() ) );
+		}
 
-			output.WriteLine("</span>");
+		private void OutputLinked( HtmlTextWriter output )
+		{
+			int iStart = CurrentPageIndex - 6;
+			int iEnd = CurrentPageIndex + 7;
+			if ( iStart < 0 ) iStart = 0;
+			if ( iEnd > PageCount ) iEnd = PageCount;
+
+			if ( iStart > 0 )
+				output.WriteLine( "<a href=\"{0}\">First</a> ...", GetPageURL(0) );
+
+			for ( int i = iStart; i < iEnd; i++ )
+			{
+				if ( i == CurrentPageIndex )
+					output.WriteLine( "[{0}]", i + 1 );
+				else
+					output.WriteLine( "<a href=\"{0}\">{1}</a>", GetPageURL( i+1 ), i + 1 );
+			}
+
+			if ( iEnd < PageCount )
+				output.WriteLine( "... <a href=\"{0}\">Last</a>", GetPageURL(PageCount) );
+		}
+
+		protected string GetPageURL(int page)
+		{
+			string url = "";
+
+			// create proper query string...
+			SimpleURLParameterParser parser = new SimpleURLParameterParser( System.Web.HttpContext.Current.Request.QueryString.ToString() );
+
+			// get the current page
+      YAF.Classes.Utils.ForumPages currentPage = ( YAF.Classes.Utils.ForumPages ) Enum.Parse( typeof( YAF.Classes.Utils.ForumPages ), parser ["g"], true );
+
+			if ( parser ["m"] != null )
+			{
+				// must be converted to by topic...
+				parser.Parameters.Remove( "m" );
+				parser.Parameters.Add( "t", yaf_Context.Current.PageTopicID.ToString() );
+			}
+
+			if ( page > 1 )
+			{
+				string tmp = parser.CreateQueryString( new string [] { "g", "p" } );
+				if (tmp.Length > 0) tmp += "&";
+
+				tmp += "p={0}";
+
+				url = yaf_BuildLink.GetLink( currentPage, tmp, page );
+			}
+			else
+			{
+        url = yaf_BuildLink.GetLink( currentPage, parser.CreateQueryString( new string [] { "g", "p" } ) );
+			}
+
+			return url;
+		}
+
+		public bool UsePostBack
+		{
+			get
+			{
+				return usePostBack;
+			}
+			set
+			{
+				usePostBack = value;
+			}
 		}
 
 		public int Count
 		{
 			get
 			{
-				if(ViewState["Count"]!=null)
-					return (int)ViewState["Count"];
+				if ( ViewState ["Count"] != null )
+					return ( int ) ViewState ["Count"];
 				else
 					return 0;
 			}
 			set
 			{
-				ViewState["Count"] = value;
+				ViewState ["Count"] = value;
 			}
 		}
 
@@ -74,14 +152,14 @@ namespace YAF.Controls
 		{
 			get
 			{
-				if(ViewState["CurrentPageIndex"]!=null)
-					return (int)ViewState["CurrentPageIndex"];
+				if ( ViewState ["CurrentPageIndex"] != null )
+					return ( int ) ViewState ["CurrentPageIndex"];
 				else
 					return 0;
 			}
 			set
 			{
-				ViewState["CurrentPageIndex"] = value;
+				ViewState ["CurrentPageIndex"] = value;
 			}
 		}
 
@@ -89,14 +167,14 @@ namespace YAF.Controls
 		{
 			get
 			{
-				if(ViewState["PageSize"]!=null)
-					return (int)ViewState["PageSize"];
+				if ( ViewState ["PageSize"] != null )
+					return ( int ) ViewState ["PageSize"];
 				else
 					return 20;
 			}
 			set
 			{
-				ViewState["PageSize"] = value;
+				ViewState ["PageSize"] = value;
 			}
 		}
 
@@ -104,7 +182,7 @@ namespace YAF.Controls
 		{
 			get
 			{
-				return (int)Math.Ceiling((double)Count/PageSize);
+				return ( int ) Math.Ceiling( ( double ) Count / PageSize );
 			}
 		}
 
@@ -112,23 +190,23 @@ namespace YAF.Controls
 		{
 			get
 			{
-				return (string)ViewState["LinkedPager"];
+				return ( string ) ViewState ["LinkedPager"];
 			}
 			set
 			{
-				ViewState["LinkedPager"] = value;
+				ViewState ["LinkedPager"] = value;
 			}
 		}
 
 		#region IPostBackEventHandler
 		public event EventHandler PageChange;
 
-		public void RaisePostBackEvent(string eventArgument)
+		public void RaisePostBackEvent( string eventArgument )
 		{
-			if(PageChange!=null)
+			if ( PageChange != null )
 			{
-				CurrentPageIndex = int.Parse(eventArgument);
-				PageChange(this,new EventArgs());
+				CurrentPageIndex = int.Parse( eventArgument );
+				PageChange( this, new EventArgs() );
 			}
 		}
 		#endregion
