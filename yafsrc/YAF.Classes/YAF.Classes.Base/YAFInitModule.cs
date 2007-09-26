51569e9de953c09fb2a22a41cb90988daeb76808
ederon-net <devnull@localhost>
2007-09-26 22:04:19 +0000

    Few comments added

--- yafsrc/YAF.Classes/YAF.Classes.Base/YAFInitModule.cs
+++ yafsrc/YAF.Classes/YAF.Classes.Base/YAFInitModule.cs
@@ -5,51 +5,59 @@ using System.Web;
 
 namespace YAF.Classes.Base
 {
-  /// <summary>
-  /// Runs 
-  /// </summary>
-  public class YafInitModule : System.Web.IHttpModule 
-  {
-    protected int BoardID
-    {
-      get
-      {
-        int boardID;
-
-        try { boardID = int.Parse( YAF.Classes.Config.BoardID ); }
-        catch { boardID = 1; }
-
-        return boardID;
-      }
-    }
-
+	/// <summary>
+	/// Runs 
+	/// </summary>
+	public class YafInitModule : System.Web.IHttpModule
+	{
+		// Emailing thread
 		private YAF.Classes.Utils.SendMailThread _sendMailThread = null;
 
-    #region IHttpModule Members
+		/// <summary>
+		/// Gets ID of board to serve
+		/// </summary>
+		protected int BoardID
+		{
+			get
+			{
+				int boardID;
 
-    void System.Web.IHttpModule.Dispose()
-    {
-			if ( _sendMailThread != null )
+				// retrieve board id from config, otherwise default is 1
+				try { boardID = int.Parse(YAF.Classes.Config.BoardID); }
+				catch { boardID = 1; }
+
+				return boardID;
+			}
+		}
+
+
+		#region IHttpModule Members
+
+		void System.Web.IHttpModule.Dispose()
+		{
+			// stop emailing thread if it's not
+			if (_sendMailThread != null)
 				_sendMailThread.StopThread();
-    }
+		}
 
-    void System.Web.IHttpModule.Init( System.Web.HttpApplication context )
-    {
+
+		void System.Web.IHttpModule.Init(System.Web.HttpApplication context)
+		{
 			try
 			{
-        // attempt to sync roles. Assumes a perfect world in which this version is completely up to date... which might not be the case.
-        YAF.Classes.Utils.RoleMembershipHelper.SyncRoles( BoardID );
+				// attempt to sync roles. Assumes a perfect world in which this version is completely up to date... which might not be the case.
+				YAF.Classes.Utils.RoleMembershipHelper.SyncRoles(BoardID);
 				// start the mail sending thread...
-				YAF.Classes.Utils.SendMailThread _sendMailThread = new YAF.Classes.Utils.SendMailThread( context );
+				YAF.Classes.Utils.SendMailThread _sendMailThread = new YAF.Classes.Utils.SendMailThread(context);
 				// run it for the lifetime of the application... (it checks and sends new e-mail every 10 seconds)
 				_sendMailThread.StartThread();
 			}
 			catch
 			{
-			  // do nothing here--upgrading/DB connectivity issues will be handled in ForumPage.cs
+				// do nothing here--upgrading/DB connectivity issues will be handled in ForumPage.cs
 			}
-    }
+		}
 
-    #endregion
-  }
+		#endregion
+	}
 }
