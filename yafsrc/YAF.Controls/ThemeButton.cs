36dae094b5805ae18eb8197c6955c94a4f46cab6
Jaben <jaben@yetanotherforum.net>
2007-10-24 10:48:18 +0000

    Added some "features" to the button such as Theme Image and Localized Text (for Title too).

--- yafsrc/YAF.Controls/ThemeButton.cs
+++ yafsrc/YAF.Controls/ThemeButton.cs
@@ -6,92 +6,182 @@ using System.Web.UI.WebControls;
 
 namespace YAF.Controls
 {
-    public class ThemeButton : BaseControl, IPostBackEventHandler
-    {
-        protected string _cssClass;
-        protected string _text;
-        protected string _link;
-        protected string _title;
-        protected static object _clickEvent = new object();
+	public class ThemeButton : BaseControl, IPostBackEventHandler
+	{
+		protected string _cssClass = "yafcssbutton";
+		protected string _text;
+		protected string _link;
+		protected string _titlePage = string.Empty;
+		protected string _titleTag = string.Empty;
+		protected static object _clickEvent = new object();
+		protected ThemeImage _themeImage = new ThemeImage();
+		protected LocalizedLabel _localizedLabel = new LocalizedLabel();
 
-        public ThemeButton()
-            : base()
-        {
-            // Constructor logic
-        }
-
-        protected virtual void OnClick(EventArgs e)
-        {
-            EventHandler handler = (EventHandler)Events[_clickEvent];
-            if (handler != null)
-                handler(this, e);
-        }
-
-        // ????????????????????????????????????????????????????????????
-		protected virtual void RaisePostBackEvent (string eventArgument)
+		public ThemeButton()
+			: base()
 		{
-			throw new NotImplementedException ();
+			this.Load += new EventHandler( ThemeButton_Load );
 		}
 
-		void IPostBackEventHandler.RaisePostBackEvent (string ea)
+		/// <summary>
+		/// Setup the controls before render
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void ThemeButton_Load( object sender, EventArgs e )
 		{
-			OnClick (EventArgs.Empty);
+			if ( !String.IsNullOrEmpty( _themeImage.ThemeTag ) )
+			{
+				// add the theme image...
+				this.Controls.Add( _themeImage );
+			}
+
+			// render the text if available
+			if ( !String.IsNullOrEmpty( _localizedLabel.LocalizedTag ) )
+			{
+				this.Controls.Add( _localizedLabel );
+			}	
 		}
 
+		protected override void Render( HtmlTextWriter output )
+		{
+			// get the title...
+			string title = GetLocalizedTitle();
 
+			output.BeginRender();
+			output.WriteBeginTag( "a" );
+			if ( !String.IsNullOrEmpty( _cssClass ) ) output.WriteAttribute( "class", _cssClass );
+			if ( !String.IsNullOrEmpty( title ) ) output.WriteAttribute( "title", title );
+			if ( !String.IsNullOrEmpty( _link ) )
+			{
+				output.WriteAttribute( "href", _link );
+			}
+			else
+			{
+				//string.Format("javascript:__doPostBack('{0}','{1}')",this.ClientID,""));
+				output.WriteAttribute( "href", Page.ClientScript.GetPostBackClientHyperlink( this, "" ) );
+			}
 
-        protected override void Render(HtmlTextWriter output)
-        {
-            output.BeginRender();
-            output.WriteBeginTag("a");
-            if (!String.IsNullOrEmpty(_cssClass)) output.WriteAttribute("class", _cssClass);
-            if (!String.IsNullOrEmpty(_title)) output.WriteAttribute("title", _title);
-            if (!String.IsNullOrEmpty(_link))
-                output.WriteAttribute("href", _link);
-            else
-                output.WriteAttribute("href", string.Format("javascript:__doPostBack('{0}','{1}')",this.ClientID,""));
-            
-            // IE fix
-            // output.WriteAttribute("onclick", "this.blur();");
-            output.Write(">");
-            output.WriteFullBeginTag("span");
-            if (!String.IsNullOrEmpty(_text)) output.WriteEncodedText(_text);
-            output.WriteEndTag("span");
-            output.WriteEndTag("a");
-            output.EndRender();
-        }
+			// IE fix
+			output.WriteAttribute("onclick", "this.blur();");
+			output.Write( HtmlTextWriter.TagRightChar );
+			output.WriteFullBeginTag( "span" );
 
-        public event EventHandler Click
-        {
-            add { Events.AddHandler(_clickEvent, value); }
-            remove { Events.RemoveHandler(_clickEvent, value); }
-        }
+			// render the optional controls (if any)
+			base.Render( output );
 
-        public string CssClass
-        {
-            get { return _cssClass; }
-            set { _cssClass = value; }
-        }
+			output.WriteEndTag( "span" );
+			output.WriteEndTag( "a" );
+			output.EndRender();
+		}
 
-        public string Text
-        {
-            get { return _text; }
-            set { _text = value; }
-        }
+		protected string GetLocalizedTitle()
+		{
+			if ( !String.IsNullOrEmpty( _titlePage ) && !String.IsNullOrEmpty( _titleTag ) )
+			{
+				return PageContext.Localization.GetText( _titlePage, _titleTag );
+			}
+			else if ( !String.IsNullOrEmpty( _titleTag ) )
+			{
+				return PageContext.Localization.GetText( _titleTag );
+			}
 
-        public string Link
-        {
-            get { return _link; }
-            set { _link = value; }
+			return null;
+		}
 
-        }
+		protected virtual void OnClick( EventArgs e )
+		{
+			EventHandler handler = ( EventHandler ) Events [_clickEvent];
+			if ( handler != null )
+				handler( this, e );
+		}
 
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
+		protected virtual void RaisePostBackEvent( string eventArgument )
+		{
+			throw new NotImplementedException();
+		}
 
+		void IPostBackEventHandler.RaisePostBackEvent( string ea )
+		{
+			OnClick( EventArgs.Empty );
+		}
 
-    }
+		public event EventHandler Click
+		{
+			add { Events.AddHandler( _clickEvent, value ); }
+			remove { Events.RemoveHandler( _clickEvent, value ); }
+		}
+
+		/// <summary>
+		/// ThemePage for the optional button image
+		/// </summary>
+		public string ImageThemePage
+		{
+			get { return _themeImage.ThemePage; }
+			set { _themeImage.ThemePage = value; }
+		}
+
+		/// <summary>
+		/// ThemeTag for the optional button image
+		/// </summary>
+		public string ImageThemeTag
+		{
+			get { return _themeImage.ThemeTag; }
+			set { _themeImage.ThemeTag = value; }
+		}
+
+		/// <summary>
+		/// Localized Page for the optional button text
+		/// </summary>
+		public string TextLocalizedPage
+		{
+			get { return _localizedLabel.LocalizedPage; }
+			set { _localizedLabel.LocalizedPage = value; }
+		}
+
+		/// <summary>
+		/// Localized Tag for the optional button text
+		/// </summary>
+		public string TextLocalizedTag
+		{
+			get { return _localizedLabel.LocalizedTag; }
+			set { _localizedLabel.LocalizedTag = value; }
+		}
+
+		/// <summary>
+		/// Defaults to "yafcssbutton"
+		/// </summary>
+		public string CssClass
+		{
+			get { return _cssClass; }
+			set { _cssClass = value; }
+		}
+
+		/// <summary>
+		/// Setting the link property will make this control non-postback.
+		/// </summary>
+		public string Link
+		{
+			get { return _link; }
+			set { _link = value; }
+		}
+
+		/// <summary>
+		/// Localized Page for the optional link description (title)
+		/// </summary>
+		public string TitleLocalizedPage
+		{
+			get { return _titlePage; }
+			set { _titlePage = value; }
+		}
+
+		/// <summary>
+		/// Localized Tag for the optional link description (title)
+		/// </summary>
+		public string TitleLocalizedTag
+		{
+			get { return _titleTag; }
+			set { _titleTag = value; }
+		}
+	}
 }
