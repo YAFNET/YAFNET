6de16b54b9c4bc1f18b814684a0d8c93515ff446
Jaben <jaben@yetanotherforum.net>
2007-11-25 00:57:06 +0000

    Added exception.

--- yafsrc/YAF.Providers/Utils/ExceptionReporter.cs
+++ yafsrc/YAF.Providers/Utils/ExceptionReporter.cs
@@ -28,78 +28,86 @@ using System.Configuration.Provider;
 
 namespace YAF.Providers.Utils
 {
-    public static class ExceptionReporter
-    {
-        /// <summary>
-        /// Get Exception XML File Name from AppSettings
-        /// </summary>
-        private static string XMLFile()
-        {
-            string temp = ConfigurationManager.AppSettings["providerExceptionXML"];
-            if (String.IsNullOrEmpty(temp))
-                return "ProviderExceptions.xml";
-            else
-                return temp;
-        }
+	public static class ExceptionReporter
+	{
+		/// <summary>
+		/// Get Exception XML File Name from AppSettings
+		/// </summary>
+		private static string XMLFile()
+		{
+			string temp = ConfigurationManager.AppSettings ["providerExceptionXML"];
+			if ( String.IsNullOrEmpty( temp ) )
+				return "ProviderExceptions.xml";
+			else
+				return temp;
+		}
 
-        /// <summary>
-        /// Return XMLDocument containing text for the Exceptions
-        /// </summary>
-        private static XmlDocument ExceptionXML()
-        {
-            string exceptionFile = XMLFile();
-            if (String.IsNullOrEmpty(exceptionFile))
-                throw new ApplicationException("Exceptionfile cannot be null or empty!");
-            XmlDocument exceptionXmlDoc = new XmlDocument();
-            exceptionXmlDoc.Load(System.Web.HttpContext.Current.Server.MapPath(String.Format("{0}resources/{1}", YafForumInfo.ForumRoot, exceptionFile)));
-            return exceptionXmlDoc;
-        }
+		/// <summary>
+		/// Return XMLDocument containing text for the Exceptions
+		/// </summary>
+		private static XmlDocument ExceptionXML()
+		{
+			string exceptionFile = XMLFile();
+			if ( String.IsNullOrEmpty( exceptionFile ) )
+				throw new ApplicationException( "Exceptionfile cannot be null or empty!" );
+			XmlDocument exceptionXmlDoc = new XmlDocument();
+			exceptionXmlDoc.Load( System.Web.HttpContext.Current.Server.MapPath( String.Format( "{0}resources/{1}", YafForumInfo.ForumRoot, exceptionFile ) ) );
+			return exceptionXmlDoc;
+		}
 
-        /// <summary>
-        /// Get Exception String
-        /// </summary>
-        public static string GetReport(string providerSection, string tag)
-        {
-            string select = string.Format("//provider[@name='{0}']/Resource[@tag='{1}']", providerSection.ToUpper(), tag.ToUpper());
-            XmlNode node = ExceptionXML().SelectSingleNode(select);
- 
-            if (node != null)
-                return node.InnerText;
-            else
-                return String.Format("Exception({1}:{0}) cannot be found in Exception file!", tag, providerSection);
+		/// <summary>
+		/// Get Exception String
+		/// </summary>
+		public static string GetReport( string providerSection, string tag )
+		{
+			string select = string.Format( "//provider[@name='{0}']/Resource[@tag='{1}']", providerSection.ToUpper(), tag.ToUpper() );
+			XmlNode node = ExceptionXML().SelectSingleNode( select );
 
-        }
+			if ( node != null )
+				return node.InnerText;
+			else
+				return String.Format( "Exception({1}:{0}) cannot be found in Exception file!", tag, providerSection );
 
-        /// <summary>
-        /// Throw Exception
-        /// </summary>
-        public static string Throw(string providerSection, string tag)
-        {
-            throw new ApplicationException(GetReport(providerSection, tag));
-        }
+		}
 
-        /// <summary>
-        /// Throw ArgumentException
-        /// </summary>
-        public static string ThrowArgument(string providerSection, string tag)
-        {
-            throw new ArgumentException(GetReport(providerSection, tag));
-        }
+		/// <summary>
+		/// Throw Exception
+		/// </summary>
+		public static string Throw( string providerSection, string tag )
+		{
+			throw new ApplicationException( GetReport( providerSection, tag ) );
+		}
 
-        /// <summary>
-        /// Throw NotSupportedException
-        /// </summary>
-        public static string ThrowNotSupported(string providerSection, string tag)
-        {
-            throw new NotSupportedException(GetReport(providerSection, tag));
-        }
+		/// <summary>
+		/// Throw ArgumentException
+		/// </summary>
+		public static string ThrowArgument( string providerSection, string tag )
+		{
+			throw new ArgumentException( GetReport( providerSection, tag ) );
+		}
 
-        /// <summary>
-        /// Throw ProviderException
-        /// </summary>
-        public static string ThrowProvider(string providerSection, string tag)
-        {
-            throw new ProviderException(GetReport(providerSection, tag));
-        }
-    }
+		/// <summary>
+		/// Throw ArgumentNullException
+		/// </summary>
+		public static string ThrowArgumentNull( string providerSection, string tag )
+		{
+			throw new ArgumentNullException( GetReport( providerSection, tag ) );
+		}
+
+		/// <summary>
+		/// Throw NotSupportedException
+		/// </summary>
+		public static string ThrowNotSupported( string providerSection, string tag )
+		{
+			throw new NotSupportedException( GetReport( providerSection, tag ) );
+		}
+
+		/// <summary>
+		/// Throw ProviderException
+		/// </summary>
+		public static string ThrowProvider( string providerSection, string tag )
+		{
+			throw new ProviderException( GetReport( providerSection, tag ) );
+		}
+	}
 }
