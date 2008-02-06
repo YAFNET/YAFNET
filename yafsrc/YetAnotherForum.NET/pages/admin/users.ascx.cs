d1a8ffd8e473ecd3b4f50ddeae91b5764b9c18cb
Jaben <jaben@yetanotherforum.net>
2008-02-06 04:13:16 +0000

    Fix for "IsAdmin" being null issue when trying to delete a user.

--- yafsrc/YetAnotherForum.NET/pages/admin/users.ascx.cs
+++ yafsrc/YetAnotherForum.NET/pages/admin/users.ascx.cs
@@ -32,123 +32,123 @@ using YAF.Classes.Data;
 
 namespace YAF.Pages.Admin
 {
-  /// <summary>
-  /// Summary description for members.
-  /// </summary>
-  public partial class users : YAF.Classes.Base.AdminPage
-  {
+	/// <summary>
+	/// Summary description for members.
+	/// </summary>
+	public partial class users : YAF.Classes.Base.AdminPage
+	{
 
-    protected void Page_Load( object sender, System.EventArgs e )
-    {
-      if ( !IsPostBack )
-      {
-        PageLinks.AddLink( PageContext.BoardSettings.Name, YAF.Classes.Utils.YafBuildLink.GetLink( YAF.Classes.Utils.ForumPages.forum ) );
-        PageLinks.AddLink( "Administration", YAF.Classes.Utils.YafBuildLink.GetLink( YAF.Classes.Utils.ForumPages.admin_admin ) );
-        PageLinks.AddLink( "Users", "" );
+		protected void Page_Load( object sender, System.EventArgs e )
+		{
+			if ( !IsPostBack )
+			{
+				PageLinks.AddLink( PageContext.BoardSettings.Name, YAF.Classes.Utils.YafBuildLink.GetLink( YAF.Classes.Utils.ForumPages.forum ) );
+				PageLinks.AddLink( "Administration", YAF.Classes.Utils.YafBuildLink.GetLink( YAF.Classes.Utils.ForumPages.admin_admin ) );
+				PageLinks.AddLink( "Users", "" );
 
-        using ( DataTable dt = YAF.Classes.Data.DB.group_list( PageContext.PageBoardID, null ) )
-        {
-          DataRow newRow = dt.NewRow();
-          newRow ["Name"] = string.Empty;
-          newRow ["GroupID"] = DBNull.Value;
-          dt.Rows.InsertAt( newRow, 0 );
+				using ( DataTable dt = YAF.Classes.Data.DB.group_list( PageContext.PageBoardID, null ) )
+				{
+					DataRow newRow = dt.NewRow();
+					newRow ["Name"] = string.Empty;
+					newRow ["GroupID"] = DBNull.Value;
+					dt.Rows.InsertAt( newRow, 0 );
 
-          group.DataSource = dt;
-          group.DataTextField = "Name";
-          group.DataValueField = "GroupID";
-          group.DataBind();
-        }
+					group.DataSource = dt;
+					group.DataTextField = "Name";
+					group.DataValueField = "GroupID";
+					group.DataBind();
+				}
 
-        using ( DataTable dt = YAF.Classes.Data.DB.rank_list( PageContext.PageBoardID, null ) )
-        {
-          DataRow newRow = dt.NewRow();
-          newRow ["Name"] = string.Empty;
-          newRow ["RankID"] = DBNull.Value;
-          dt.Rows.InsertAt( newRow, 0 );
+				using ( DataTable dt = YAF.Classes.Data.DB.rank_list( PageContext.PageBoardID, null ) )
+				{
+					DataRow newRow = dt.NewRow();
+					newRow ["Name"] = string.Empty;
+					newRow ["RankID"] = DBNull.Value;
+					dt.Rows.InsertAt( newRow, 0 );
 
-          rank.DataSource = dt;
-          rank.DataTextField = "Name";
-          rank.DataValueField = "RankID";
-          rank.DataBind();
-        }
-      }
-    }
+					rank.DataSource = dt;
+					rank.DataTextField = "Name";
+					rank.DataValueField = "RankID";
+					rank.DataBind();
+				}
+			}
+		}
 
-    private void BindData()
-    {
-      using ( DataTable dt =
-            YAF.Classes.Data.DB.user_list( PageContext.PageBoardID, null, null,
-            group.SelectedIndex <= 0 ? null : group.SelectedValue,
-            rank.SelectedIndex <= 0 ? null : rank.SelectedValue
-            ) )
-      {
-        using ( DataView dv = dt.DefaultView )
-        {
-          if ( name.Text.Trim().Length > 0 || ( Email.Text.Trim().Length > 0 ) )
-            dv.RowFilter = string.Format( "Name like '%{0}%' and Email like '%{1}%'", name.Text.Trim(), Email.Text.Trim() );
-          UserList.DataSource = dv;
-          UserList.DataBind();
-        }
-      }
-    }
+		private void BindData()
+		{
+			using ( DataTable dt =
+						YAF.Classes.Data.DB.user_list( PageContext.PageBoardID, null, null,
+						group.SelectedIndex <= 0 ? null : group.SelectedValue,
+						rank.SelectedIndex <= 0 ? null : rank.SelectedValue
+						) )
+			{
+				using ( DataView dv = dt.DefaultView )
+				{
+					if ( name.Text.Trim().Length > 0 || ( Email.Text.Trim().Length > 0 ) )
+						dv.RowFilter = string.Format( "Name like '%{0}%' and Email like '%{1}%'", name.Text.Trim(), Email.Text.Trim() );
+					UserList.DataSource = dv;
+					UserList.DataBind();
+				}
+			}
+		}
 
-    public void Delete_Load( object sender, System.EventArgs e )
-    {
-      ( ( LinkButton ) sender ).Attributes ["onclick"] = "return confirm('Delete this user?')";
-    }
+		public void Delete_Load( object sender, System.EventArgs e )
+		{
+			( ( LinkButton )sender ).Attributes ["onclick"] = "return confirm('Delete this user?')";
+		}
 
-    public void UserList_ItemCommand( object source, System.Web.UI.WebControls.RepeaterCommandEventArgs e )
-    {
-      switch ( e.CommandName )
-      {
-        case "edit":
-          YAF.Classes.Utils.YafBuildLink.Redirect( YAF.Classes.Utils.ForumPages.admin_edituser, "u={0}", e.CommandArgument );
-          break;
-        case "delete":
-          if ( PageContext.PageUserID == int.Parse( e.CommandArgument.ToString() ) )
-          {
-            PageContext.AddLoadMessage( "You can't delete yourself." );
-            return;
-          }
-          string userName = string.Empty;
-          using ( DataTable dt = YAF.Classes.Data.DB.user_list( PageContext.PageBoardID, e.CommandArgument, DBNull.Value ) )
-          {
-            foreach ( DataRow row in dt.Rows )
-            {
-              userName = ( string ) row ["Name"];
-              if ( ( int ) row ["IsGuest"] > 0 )
-              {
-                PageContext.AddLoadMessage( "You can't delete the Guest." );
-                return;
-              }
-              if ( ( int ) row ["IsAdmin"] > 0 || ( int ) row ["IsHostAdmin"] > 0 )
-              {
-                PageContext.AddLoadMessage( "You can't delete the Admin." );
-                return;
-              }
-            }
-          }
-          UserMembershipHelper.DeleteUser( Convert.ToInt32(e.CommandArgument) );
-          BindData();
-          break;
-      }
-    }
+		public void UserList_ItemCommand( object source, System.Web.UI.WebControls.RepeaterCommandEventArgs e )
+		{
+			switch ( e.CommandName )
+			{
+				case "edit":
+					YAF.Classes.Utils.YafBuildLink.Redirect( YAF.Classes.Utils.ForumPages.admin_edituser, "u={0}", e.CommandArgument );
+					break;
+				case "delete":
+					if ( PageContext.PageUserID == int.Parse( e.CommandArgument.ToString() ) )
+					{
+						PageContext.AddLoadMessage( "You can't delete yourself." );
+						return;
+					}
+					string userName = string.Empty;
+					using ( DataTable dt = YAF.Classes.Data.DB.user_list( PageContext.PageBoardID, e.CommandArgument, DBNull.Value ) )
+					{
+						foreach ( DataRow row in dt.Rows )
+						{
+							userName = ( string )row ["Name"];
+							if ( ( int )row ["IsGuest"] > 0 )
+							{
+								PageContext.AddLoadMessage( "You can't delete the Guest." );
+								return;
+							}
+							if ( ( row ["IsAdmin"] != DBNull.Value && Convert.ToInt32( row ["IsAdmin"] ) > 0 ) || ( row ["IsHostAdmin"] != DBNull.Value && Convert.ToInt32( row ["IsHostAdmin"] ) > 0 ) )
+							{
+								PageContext.AddLoadMessage( "You can't delete the Admin." );
+								return;
+							}
+						}
+					}
+					UserMembershipHelper.DeleteUser( Convert.ToInt32( e.CommandArgument ) );
+					BindData();
+					break;
+			}
+		}
 
-    public void NewUser_Click( object sender, System.EventArgs e )
-    {
-      YAF.Classes.Utils.YafBuildLink.Redirect( YAF.Classes.Utils.ForumPages.admin_reguser );
-    }
+		public void NewUser_Click( object sender, System.EventArgs e )
+		{
+			YAF.Classes.Utils.YafBuildLink.Redirect( YAF.Classes.Utils.ForumPages.admin_reguser );
+		}
 
 
-    public void search_Click( object sender, EventArgs e )
-    {
-      BindData();
-    }
+		public void search_Click( object sender, EventArgs e )
+		{
+			BindData();
+		}
 
-    protected bool BitSet( object _o, int bitmask )
-    {
-      int i = ( int ) _o;
-      return ( i & bitmask ) != 0;
-    }
-  }
+		protected bool BitSet( object _o, int bitmask )
+		{
+			int i = ( int )_o;
+			return ( i & bitmask ) != 0;
+		}
+	}
 }
