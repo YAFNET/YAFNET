9973666890d17b0f684fc5c7daec8c7ebf795a3e
Jaben <jaben@yetanotherforum.net>
2009-04-24 01:54:57 +0000

    Added a SimpleURLParameterParser Test...

--- yafsrc/YAF.Classes/YAF.Classes.UnitTests/UtilTests.cs
+++ yafsrc/YAF.Classes/YAF.Classes.UnitTests/UtilTests.cs
@@ -19,5 +19,18 @@ namespace YAF.Classes.UnitTests
 			Assert.AreEqual( RoleMembershipHelper.RoleInRoleArray( "role2", roleArray ), true );
 			Assert.AreEqual( RoleMembershipHelper.RoleInRoleArray( "norole", roleArray ), false );
 		}
+
+		[Test]
+		public void SimpleURLParameterParserTest()
+		{
+			SimpleURLParameterParser parser = new SimpleURLParameterParser( "g=forum&t=1&url=&pg=43#cool" );
+
+			Assert.IsTrue( parser.HasAnchor );
+			Assert.AreEqual( parser.Anchor, "cool" );
+			Assert.AreEqual( parser.Parameters["g"], "forum" );
+			Assert.AreEqual( parser.CreateQueryString( new string[] { "g", "t" } ), "url=&pg=43" );
+			Assert.IsNotNull( parser.Parameters[3] );
+			Assert.AreEqual( parser.Parameters[3], "43" );
+		}
 	}
 }
