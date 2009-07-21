662350a63d46971ec7edc546dc0551320ccfc905
Jaben <jaben@yetanotherforum.net>
2009-07-21 20:16:34 +0000

    Works with DNN v5 and fixed issues on compile with different versions of YAF dlls.

--- yaf_dnn/DotNetNukeModule.ascx.cs
+++ yaf_dnn/DotNetNukeModule.ascx.cs
@@ -1,174 +1,239 @@
 ﻿using System;
-using System.Collections;
-using System.Configuration;
 using System.Data;
-using System.Linq;
 using System.Web;
 using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
-using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Xml.Linq;
 using DotNetNuke;
-using DotNetNuke.Common;
-using DotNetNuke.Services.Search;
 using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Modules.Actions;
-using DotNetNuke.Services.Localization;
-using DotNetNuke.Services.Exceptions;
-using YAF.Classes;
 using YAF.Classes.Data;
-using YAF.Classes.Base;
+using YAF.Classes.Utils;
 
 namespace yaf_dnn
 {
-/// <summary>
-    ///        Summary description for DotNetNukeModule.
-    /// </summary>
-    public partial class DotNetNukeModule : DotNetNuke.Entities.Modules.PortalModuleBase, IActionable
-    {
-        private bool m_isSuperAdmin;
-        private bool createNewBoard = false;
+	/// <summary>
+	/// Summary description for DotNetNukeModule.
+	/// </summary>
+	public partial class DotNetNukeModule : DotNetNuke.Entities.Modules.PortalModuleBase, IActionable
+	{
+		private bool createNewBoard = false;
 
-        private void DotNetNukeModule_Load(object sender, System.EventArgs e)
-        {
-            try
-            {
-                createNewBoard = false;
-                // This will create an error if there is no setting for forumboardid
-                Forum1.BoardID = int.Parse(Settings["forumboardid"].ToString());
+		DotNetNuke.Entities.Portals.PortalSettings _portalSettings = null;
+		DotNetNuke.Entities.Portals.PortalSettings CurrentPortalSettings
+		{
+			get
+			{
+				if (_portalSettings == null)
+				{
+					_portalSettings = (DotNetNuke.Entities.Portals.PortalSettings)HttpContext.Current.Items["PortalSettings"];
+				}
 
-                string cID = Settings["forumcategoryid"].ToString();
-                if (cID != string.Empty)
-                    Forum1.CategoryID = int.Parse(cID);
-            }
-            catch (Exception)
-            {
-                //A forum does not exist for this module
-                //Create a new board
-                createNewBoard = true;
-                //Forum1.BoardID = 1;
-            }
-            
-            // Check for user
-            if (HttpContext.Current.User.Identity.IsAuthenticated)
-            {
-                DotNetNuke.Entities.Users.UserInfo DnnUserInfo;
-                //Get current portal settings
-                DotNetNuke.Entities.Portals.PortalSettings _portalSettings = (DotNetNuke.Entities.Portals.PortalSettings)HttpContext.Current.Items["PortalSettings"];
-                //Get current user
-                DnnUserInfo = DotNetNuke.Entities.Users.UserController.GetUser(_portalSettings.PortalId, this.UserId, false);
-                
-                //get the roles from dnn for this user and copy them to the aspnet roles if required.
-                DotNetNuke.Security.Roles.RoleController _DnnRoleController = new DotNetNuke.Security.Roles.RoleController();
-                ArrayList DnnRoleList;
-                DnnRoleList = _DnnRoleController.GetUserRoles(_portalSettings.PortalId, DnnUserInfo.UserID);
-                foreach (DotNetNuke.Entities.Users.UserRoleInfo rolenamelist in DnnRoleList)
-                    {
-                        if (!Roles.RoleExists(rolenamelist.RoleName))
-                        {
-                            Roles.CreateRole(rolenamelist.RoleName);
-                            Roles.AddUserToRole(DnnUserInfo.Username, rolenamelist.RoleName);
-                        }
-                        else
-                        {
-                            if (!Roles.IsUserInRole(DnnUserInfo.Username, rolenamelist.RoleName))
-                            {
-                                Roles.AddUserToRole(DnnUserInfo.Username, rolenamelist.RoleName);
-                            }
-                        }
-                    }
-             
-                //Admin or Host user?
-                if (DnnUserInfo.UserID == _portalSettings.AdministratorId | DnnUserInfo.IsSuperUser)
-                    {
-                    //Do we have to create a new board
-                    if (createNewBoard == true)
-                    {
-                        // Add new admin users to group
-                        if (!Roles.IsUserInRole(DnnUserInfo.Username, "Administrators"))
-                        {
-                            Roles.AddUserToRole(DnnUserInfo.Username, "Administrators");
-                        }
+				return _portalSettings;
+			}
+		}
 
-                        if (m_isSuperAdmin)
-                        {
-                            //This is HOST and probably the first board.
-                            //The install routine already created the first board.
-                            //Make sure Module settings are in place
-                            DotNetNuke.Entities.Modules.ModuleController objForumSettings = new DotNetNuke.Entities.Modules.ModuleController();
-                            objForumSettings.UpdateModuleSetting(ModuleId, "forumboardid", "1");
-                            objForumSettings.UpdateModuleSetting(ModuleId, "forumcategoryid", string.Empty);
-                            Forum1.BoardID = 1;
-                        }
-                        else
-                        {
-                            // This is an admin adding a new forum.
+		public string SessionUserKeyName
+		{
+			get
+			{
+				return String.Format("yaf_dnn_boardid{0}_userid{1}_portalid{2}", Forum1.BoardID, this.UserId, CurrentPortalSettings.PortalId);
+			}
+		}
 
-                            //get the user from the membership provider
-                            //Need the user provider user key
-                            MembershipUser DnnUser = Membership.GetUser(DnnUserInfo.Username, true);
-                           
-                            string newBoardName = "New Forum - Module " + ModuleId.ToString();
-                            //Create the board
-                            DB.board_create(DnnUserInfo.Username, DnnUser.ProviderUserKey, newBoardName, "DotNetNuke", "DotNetNuke");
+		private void DotNetNukeModule_Load(object sender, System.EventArgs e)
+		{
+			// Check for user
+			if (HttpContext.Current.User.Identity.IsAuthenticated)
+			{
+				//Get current Dnn user
+				DotNetNuke.Entities.Users.UserInfo DnnUserInfo = DotNetNuke.Entities.Users.UserController.GetUser(CurrentPortalSettings.PortalId, this.UserId, false);
 
-                            //The newly created board will be the last one in the DB and have the highest BoardID
-                            DataTable tbl = DB.board_list(null);
-                            int largestBoardID = 0;
-                            foreach (DataRow row in tbl.Rows)
-                            {
-                                if (Convert.ToInt32(row["BoardID"]) > largestBoardID)
-                                {
-                                    largestBoardID = Convert.ToInt32(row["BoardID"]);
-                                }
-                            }
-                            //Assign the new forum to this module
-                            DotNetNuke.Entities.Modules.ModuleController objForumSettings = new DotNetNuke.Entities.Modules.ModuleController();
-                            objForumSettings.UpdateModuleSetting(ModuleId, "forumboardid", largestBoardID.ToString());
-                            objForumSettings.UpdateModuleSetting(ModuleId, "forumcategoryid", string.Empty);
-                            Forum1.BoardID = largestBoardID;
-                        }
-                    }
-                }
-            }
-        }
+				// see if the roles have been syncronized...
+				if (Session[SessionUserKeyName + "_rolesloaded"] == null)
+				{
+					//Get roles from DNN for this user and put into aspnet role tables
+					foreach (string _rolename in DnnUserInfo.Roles)
+						if (_rolename != null)
+						{
+							//Create role if not existing
+							if (!Roles.RoleExists(_rolename))
+							{
+								Roles.CreateRole(_rolename);
 
-        void Forum1_PageTitleSet(object sender, YAF.Classes.Base.ForumPageArgs e)
-        {
-            BasePage.Title = e.Title + " - " + BasePage.Title;
-        }
+							}
+							//Add user to role if not already there
+							if (!Roles.IsUserInRole(DnnUserInfo.Username, _rolename))
+							{
+								Roles.AddUserToRole(DnnUserInfo.Username, _rolename);
+							}
+						}
 
-        override protected void OnInit(EventArgs e)
-        {
-            Load += new EventHandler(DotNetNukeModule_Load);
-            Forum1.PageTitleSet += new EventHandler<YAF.Classes.Base.ForumPageArgs>(Forum1_PageTitleSet);
-            base.OnInit(e);
-        }
+					Session[SessionUserKeyName + "_rolesloaded"] = true;
+				}
 
-        public DotNetNuke.Framework.CDefault BasePage
-        {
-            get
-            {
-                return (DotNetNuke.Framework.CDefault)this.Page;
-            }
-        }
+				//get the user from the membership provider
+				//Need the user provider user key
+				MembershipUser DnnUser = Membership.GetUser(DnnUserInfo.Username, true);
 
-        #region IActionable Members
+				//Admin or Host user?
+				if ((DnnUserInfo.IsSuperUser || DnnUserInfo.UserID == _portalSettings.AdministratorId) && createNewBoard)
+				{
+					CreateNewBoard(DnnUserInfo, DnnUser);
+				}
 
-        public ModuleActionCollection ModuleActions
-        {
-            get
-            {
-                ModuleActionCollection actions = new ModuleActionCollection();
-                //Change
-                //actions.Add(GetNextActionID(), "Edit YAF Settings", ModuleActionType.AddContent, String.Empty, String.Empty, EditUrl(), false, DotNetNuke.Security.SecurityAccessLevel.Edit, true, false);
-                actions.Add(GetNextActionID(), "Edit YAF Settings", ModuleActionType.AddContent, String.Empty, String.Empty, EditUrl(), false, DotNetNuke.Security.SecurityAccessLevel.Host, true, false);
-                return actions;
-            }
-        }
+				// Has this user been registered in YAF already?
+				if (Session[SessionUserKeyName + "_userSync"] == null)
+				{
+					int yafUserId = 0;
 
-        #endregion
-    }
+					try
+					{
+						yafUserId = DB.user_get(Forum1.BoardID, DnnUser.ProviderUserKey);
+					}
+					catch (Exception)
+					{
+						// create the user in the YAF DB so profile can ge created...
+						int? userID = RoleMembershipHelper.CreateForumUser(DnnUser, Forum1.BoardID);
+
+						// create profile
+						YafUserProfile userProfile = YafContext.Current.GetProfile(DnnUserInfo.Username);
+						// setup their inital profile information
+						userProfile.RealName = DnnUserInfo.Profile.FullName;
+						userProfile.Location = DnnUserInfo.Profile.Country;
+						userProfile.Homepage = DnnUserInfo.Profile.Website;
+						userProfile.Save();
+
+						yafUserId = UserMembershipHelper.GetUserIDFromProviderUserKey(DnnUser.ProviderUserKey);
+
+						// save the time zone...
+						YAF.Classes.Data.DB.user_save(yafUserId, Forum1.BoardID, null, null, Convert.ToInt32(DnnUserInfo.Profile.TimeZone), null, null, null, null, null);
+					}
+
+					// super admin check...
+					if (DnnUserInfo.IsSuperUser)
+					{
+						// get this user information...
+						DataTable userInfo = DB.user_list(Forum1.BoardID, yafUserId, null, null, null);
+
+						if (userInfo.Rows.Count > 0)
+						{
+							DataRow row = userInfo.Rows[0];
+
+							if (Convert.ToBoolean(row["IsHostAdmin"]) == false)
+							{
+								// fix the ishostadmin flag...
+								UserFlags userFlags = new UserFlags(row["Flags"]);
+								userFlags.IsHostAdmin = true;
+
+								// update...
+								DB.user_adminsave(Forum1.BoardID, yafUserId, row["Name"], row["Email"], userFlags.BitValue, row["RankID"]);
+							}
+						}
+					}
+
+					Session[SessionUserKeyName + "_userSync"] = true;
+				}
+			}
+		}
+
+		private void CreateNewBoard(DotNetNuke.Entities.Users.UserInfo DnnUserInfo, MembershipUser DnnUser)
+		{
+			// Add new admin users to group
+			if (!Roles.IsUserInRole(DnnUserInfo.Username, "Administrators"))
+			{
+				Roles.AddUserToRole(DnnUserInfo.Username, "Administrators");
+			}
+
+			if (DnnUserInfo.IsSuperUser)
+			{
+				//This is HOST and probably the first board.
+				//The install routine already created the first board.
+				//Make sure Module settings are in place
+				DotNetNuke.Entities.Modules.ModuleController objForumSettings = new DotNetNuke.Entities.Modules.ModuleController();
+				objForumSettings.UpdateModuleSetting(ModuleId, "forumboardid", "1");
+				objForumSettings.UpdateModuleSetting(ModuleId, "forumcategoryid", string.Empty);
+				Forum1.BoardID = 1;
+			}
+			else
+			{
+				// This is an admin adding a new forum.
+
+				string newBoardName = "New Forum - Module " + ModuleId.ToString();
+				//Create the board
+				DB.board_create(DnnUserInfo.Username, DnnUser.ProviderUserKey, newBoardName, "DotNetNuke", "DotNetNuke");
+
+				//The newly created board will be the last one in the DB and have the highest BoardID
+				DataTable tbl = DB.board_list(null);
+				int largestBoardID = 0;
+				foreach (DataRow row in tbl.Rows)
+				{
+					if (Convert.ToInt32(row["BoardID"]) > largestBoardID)
+					{
+						largestBoardID = Convert.ToInt32(row["BoardID"]);
+					}
+				}
+				//Assign the new forum to this module
+				DotNetNuke.Entities.Modules.ModuleController objForumSettings = new DotNetNuke.Entities.Modules.ModuleController();
+				objForumSettings.UpdateModuleSetting(ModuleId, "forumboardid", largestBoardID.ToString());
+				objForumSettings.UpdateModuleSetting(ModuleId, "forumcategoryid", string.Empty);
+				Forum1.BoardID = largestBoardID;
+			}
+		}
+
+		void Forum1_PageTitleSet(object sender, YAF.Classes.Base.ForumPageArgs e)
+		{
+			BasePage.Title = e.Title + " - " + BasePage.Title;
+
+		}
+
+		override protected void OnInit(EventArgs e)
+		{
+			Load += new EventHandler(DotNetNukeModule_Load);
+			Forum1.PageTitleSet += new EventHandler<YAF.Classes.Base.ForumPageArgs>(Forum1_PageTitleSet);
+			//Get current BoardID
+			try
+			{
+				createNewBoard = false;
+				// This will create an error if there is no setting for forumboardid
+				Forum1.BoardID = int.Parse(Settings["forumboardid"].ToString());
+
+				string cID = Settings["forumcategoryid"].ToString();
+				if (cID != string.Empty)
+					Forum1.CategoryID = int.Parse(cID);
+			}
+			catch (Exception)
+			{
+				//A forum does not exist for this module
+				//Create a new board
+				createNewBoard = true;
+				//Forum1.BoardID = 1;
+			}
+			//sync roles from DNN to YAF
+
+			base.OnInit(e);
+		}
+
+		public DotNetNuke.Framework.CDefault BasePage
+		{
+			get
+			{
+				return (DotNetNuke.Framework.CDefault)this.Page;
+			}
+		}
+
+		#region IActionable Members
+
+		public ModuleActionCollection ModuleActions
+		{
+			get
+			{
+				ModuleActionCollection actions = new ModuleActionCollection();
+				//Change
+				//actions.Add(GetNextActionID(), "Edit YAF Settings", ModuleActionType.AddContent, String.Empty, String.Empty, EditUrl(), false, DotNetNuke.Security.SecurityAccessLevel.Edit, true, false);
+				actions.Add(GetNextActionID(), "Edit YAF Settings", ModuleActionType.AddContent, String.Empty, String.Empty, EditUrl(), false, DotNetNuke.Security.SecurityAccessLevel.Host, true, false);
+				return actions;
+			}
+		}
+
+		#endregion
+	}
 }
--- yaf_dnn/DotNetNukeUrlBuilder.cs
+++ yaf_dnn/DotNetNukeUrlBuilder.cs
@@ -6,15 +6,15 @@ namespace yaf_dnn
 {
 	public class DotNetNukeUrlBuilder : YAF.Classes.IUrlBuilder
 	{
-		public string BuildUrl( string url )
+		public string BuildUrl(string url)
 		{
 			// escape & to &amp;
-			url = url.Replace( "&", "&amp;" );
+			url = url.Replace("&", "&amp;");
 
 			string scriptname = HttpContext.Current.Request.ServerVariables["SCRIPT_NAME"];
 			string tabid = HttpContext.Current.Request.QueryString["tabid"];
 
-			string builturl = tabid != null ? string.Format( "{0}?tabid={1}&{2}", scriptname, tabid, url ) : string.Format( "{0}?{1}", scriptname, url );
+			string builturl = tabid != null ? string.Format("{0}?tabid={1}&{2}", scriptname, tabid, url) : string.Format("{0}?{1}", scriptname, url);
 
 			return builturl;
 		}
