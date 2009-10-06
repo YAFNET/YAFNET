8fc8df168a61eceae655e79db6674a45554a0ae8
Jaben <jaben@yetanotherforum.net>
2009-10-06 00:28:37 +0000

    Added HttpSimulator to the test project -- added a few UrlBuilder tests.

---
+++ yafsrc/YAF.Classes/YAF.Classes.UnitTests/UrlBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Subtext.TestLibrary;
+using TeamAgile.ApplicationBlocks.Interception.UnitTestExtensions;
+using YAF.Classes.Core;
+using YAF.Classes.Utils;
+
+namespace YAF.Classes.UnitTests
+{
+	[TestFixture]
+	public class UrlBuilderTests : UrlBuilder
+	{
+		[Test]
+		public void TreatBaseUrlTest()
+		{
+			Assert.AreEqual( UrlBuilder.TreatBaseUrl( "http://donkey.com/" ), "http://donkey.com" );
+		}
+
+		[Test]
+		public void TreatPathStrTest()
+		{
+			using ( HttpSimulator simulator = new HttpSimulator( "/", @"c:\inetpub\wwwroot" ).SimulateRequest() )
+			{
+				Assert.AreEqual( UrlBuilder.TreatPathStr( null ), @"/" );
+				Assert.AreEqual( UrlBuilder.TreatPathStr( "~/forum" ), @"/forum/" );
+			}
+		}
+	}
+}
