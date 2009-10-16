11f16053b04aebf96a712ffe4f3c93631ed330ec
mekworld <devnull@localhost>
2009-10-16 16:10:21 +0000

    Wouldn't get very far without this new class... YafBoardFolders - Provides methods for obtaining the locations for Images, THemes, Uploads.

---
+++ yafsrc/YAF.Classes/YAF.Classes.Config/YafBoardFolders.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YAF.Classes.Pattern;
+
+namespace YAF.Classes
+{
+    public class YafBoardFolders
+    {
+
+        public static YafBoardFolders Current
+        {
+            get
+            {
+                return PageSingleton<YafBoardFolders>.Instance;
+            }
+        }
+
+        public string BoardFolder
+        {
+            get
+            {
+                if (Config.MultiBoardFolders)
+                    return String.Format(Config.BoardRoot + "{0}/", Config.BoardID);
+                else
+                {
+                    return Config.BoardRoot;
+                }
+            }
+        }
+
+        public string Uploads
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Uploads");
+            }
+        }
+
+        public string Themes
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Themes");
+            }
+        }
+
+        public string Images
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images");
+            }
+        }
+
+        public string Avatars
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images/Avatars");
+            }
+        }
+
+        public string Categories
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images/Categories");
+            }
+        }
+
+        public string Emoticons
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images/Emoticons");
+            }
+        }
+
+        public string Medals
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images/Medals");
+            }
+        }
+
+        public string Ranks
+        {
+            get
+            {
+                return String.Concat(BoardFolder, "Images/Ranks");
+            }
+        }
+
+
+
+
+
+    }
+}
