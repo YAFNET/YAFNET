3cde205ee38194c2c885dc3cb3b3bba244e33bcb
touradg <devnull@localhost>
2009-10-20 18:22:00 +0000

    Some Cleanup of duplicate references

--- yafsrc/YetAnotherForum.NET/controls/EditUsersProfile.ascx.cs
+++ yafsrc/YetAnotherForum.NET/controls/EditUsersProfile.ascx.cs
@@ -30,7 +30,6 @@ using System.Globalization;
 using System.Collections.Specialized;
 using YAF.Classes.Core;
 using YAF.Classes;
-using YAF.Classes.Core;
 using YAF.Classes.Utils;
 
 namespace YAF.Controls
