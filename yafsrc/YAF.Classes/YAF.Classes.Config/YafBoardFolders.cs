f826ef88a51fd6df861184b649c0a2f5898d9e74
Jaben <jaben@yetanotherforum.net>
2009-10-25 18:32:16 +0000

    Boskone bug in BoardFolders -- code wasn't selecting BoardID from the current settings.

--- yafsrc/YAF.Classes/YAF.Classes.Config/YafBoardFolders.cs
+++ yafsrc/YAF.Classes/YAF.Classes.Config/YafBoardFolders.cs
@@ -6,97 +6,90 @@ using YAF.Classes.Pattern;
 
 namespace YAF.Classes
 {
-    public class YafBoardFolders
-    {
+	public class YafBoardFolders
+	{
 
-        public static YafBoardFolders Current
-        {
-            get
-            {
-                return PageSingleton<YafBoardFolders>.Instance;
-            }
-        }
+		public static YafBoardFolders Current
+		{
+			get
+			{
+				return PageSingleton<YafBoardFolders>.Instance;
+			}
+		}
 
-        public string BoardFolder
-        {
-            get
-            {
-                if (Config.MultiBoardFolders)
-                    return String.Format(Config.BoardRoot + "{0}/", Config.BoardID);
-                else
-                {
-                    return Config.BoardRoot;
-                }
-            }
-        }
+		public string BoardFolder
+		{
+			get
+			{
+				if ( Config.MultiBoardFolders )
+					return String.Format( Config.BoardRoot + "{0}/", YafControlSettings.Current.BoardID );
+				else
+					return Config.BoardRoot;
+			}
+		}
 
-        public string Uploads
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Uploads");
-            }
-        }
+		public string Uploads
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Uploads" );
+			}
+		}
 
-        public string Themes
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Themes");
-            }
-        }
+		public string Themes
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Themes" );
+			}
+		}
 
-        public string Images
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images");
-            }
-        }
+		public string Images
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images" );
+			}
+		}
 
-        public string Avatars
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images/Avatars");
-            }
-        }
+		public string Avatars
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images/Avatars" );
+			}
+		}
 
-        public string Categories
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images/Categories");
-            }
-        }
+		public string Categories
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images/Categories" );
+			}
+		}
 
-        public string Emoticons
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images/Emoticons");
-            }
-        }
+		public string Emoticons
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images/Emoticons" );
+			}
+		}
 
-        public string Medals
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images/Medals");
-            }
-        }
+		public string Medals
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images/Medals" );
+			}
+		}
 
-        public string Ranks
-        {
-            get
-            {
-                return String.Concat(BoardFolder, "Images/Ranks");
-            }
-        }
-
-
-
-
-
-    }
+		public string Ranks
+		{
+			get
+			{
+				return String.Concat( BoardFolder, "Images/Ranks" );
+			}
+		}
+	}
 }
