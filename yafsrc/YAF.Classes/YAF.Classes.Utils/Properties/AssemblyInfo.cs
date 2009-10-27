7cd2dc0a26ed9caac592cc0b9f6099fa7c83c04b
Jaben <jaben@yetanotherforum.net>
2009-10-27 16:26:56 +0000

    Removed weird file version number.

--- yafsrc/YAF.Classes/YAF.Classes.Utils/Properties/AssemblyInfo.cs
+++ yafsrc/YAF.Classes/YAF.Classes.Utils/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Security;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion( "1.9.4.0" )]
-[assembly: AssemblyFileVersion( "1.9.4.*" )]
+[assembly: AssemblyFileVersion( "1.9.4.0" )]
 
 #if (!COMPACT_FRAMEWORK)
 [assembly: AllowPartiallyTrustedCallers]
