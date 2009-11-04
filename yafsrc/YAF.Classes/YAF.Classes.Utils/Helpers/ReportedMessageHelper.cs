23c52c51b59509aeecb05eb492367ab863312c5a
vzrus <vzrus.net@gmail.com>
2009-11-04 10:58:03 +0000

    Enhancement to prev. commit. Simply moved repeating code from 2 files into 1 place.

---
+++ yafsrc/YAF.Classes/YAF.Classes.Utils/Helpers/ReportedMessageHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace YAF.Classes.Utils
+{
+    public static class ReportedMessageHelper
+    {
+        public static DataTable GetReportersList(int flags,int forumid)
+        {
+            System.Data.DataTable dt = YAF.Classes.Data.DB.message_listreported(flags, forumid);
+            dt.Columns.Add("Reporters", typeof(string));
+            dt.AcceptChanges();
+            int i = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                DataTable _reportersList = YAF.Classes.Data.DB.message_listreporters(Convert.ToInt32(dr["MessageID"]));
+                if (_reportersList.Rows.Count > 0)
+                {
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    foreach (DataRow reporter in _reportersList.Rows)
+                    {
+                        string howMany = null;
+                        if (Convert.ToInt32(reporter["ReportedNumber"]) > 1)
+                            howMany = "(" + reporter["ReportedNumber"].ToString() + ")";
+
+                        sb.AppendFormat(@"<a id=""Link{1}{0}"" href=""{3}"" runat=""server"">{2}{4}</a>, ", i, Convert.ToInt32(reporter["UserID"]), reporter["UserName"].ToString(), YafBuildLink.GetLink(ForumPages.profile, "u={0}", Convert.ToInt32(reporter["UserID"])), howMany);
+
+                        i++;
+                    }
+                    dr["Reporters"] = sb.ToString().TrimEnd().TrimEnd(',');
+                }
+
+            }
+            return dt;
+        }
+    }
+}
