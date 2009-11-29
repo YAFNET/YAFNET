b8044b51ffb5576bf041ba34087a2df9e02c0e18
Jaben <jaben@yetanotherforum.net>
2009-11-29 00:11:59 +0000

    Forgot to commit the interfaces.

---
+++ yafsrc/YAF.Classes/YAF.Classes.Config/Interfaces/IStyleTransform.cs
@@ -0,0 +1,79 @@
+﻿namespace YAF.Classes.Interfaces
+{
+  using System.Data;
+
+  /// <summary>
+  /// The i style transform.
+  /// </summary>
+  public interface IStyleTransform
+  {
+    /// <summary>
+    /// The decode style by table.
+    /// </summary>
+    /// <param name="dt">
+    /// The dt.
+    /// </param>
+    void DecodeStyleByTable(ref DataTable dt);
+
+    /// <summary>
+    /// The decode style by table.
+    /// </summary>
+    /// <param name="dt">
+    /// The dt.
+    /// </param>
+    /// <param name="colorOnly">
+    /// The color only.
+    /// </param>
+    void DecodeStyleByTable(ref DataTable dt, bool colorOnly);
+
+    /// <summary>
+    /// The decode style by table.
+    /// </summary>
+    /// <param name="dt">
+    /// The dt.
+    /// </param>
+    /// <param name="colorOnly">
+    /// The color only.
+    /// </param>
+    /// <param name="styleColumns">
+    /// The style Columns.
+    /// </param>
+    /// <param name="colorOnly">
+    /// The styleColumns can contain param array to handle several style columns.
+    /// </param>
+    void DecodeStyleByTable(ref DataTable dt, bool colorOnly, params string[] styleColumns);
+
+    /// <summary>
+    /// The decode style by row.
+    /// </summary>
+    /// <param name="dr">
+    /// The dr.
+    /// </param>
+    void DecodeStyleByRow(ref DataRow dr);
+
+    /// <summary>
+    /// The decode style by row.
+    /// </summary>
+    /// <param name="dr">
+    /// The dr.
+    /// </param>
+    /// <param name="colorOnly">
+    /// The color only.
+    /// </param>
+    void DecodeStyleByRow(ref DataRow dr, bool colorOnly);
+
+    /// <summary>
+    /// The decode style by string.
+    /// </summary>
+    /// <param name="styleStr">
+    /// The style str.
+    /// </param>
+    /// <param name="colorOnly">
+    /// The color only.
+    /// </param>
+    /// <returns>
+    /// The decode style by string.
+    /// </returns>
+    string DecodeStyleByString(string styleStr, bool colorOnly);
+  }
+}
---
+++ yafsrc/YAF.Classes/YAF.Classes.Config/Interfaces/IYafTheme.cs
@@ -0,0 +1,95 @@
+﻿/* Yet Another Forum.NET
+ * Copyright (C) 2003-2005 Bjørnar Henden
+ * Copyright (C) 2006-2009 Jaben Cargman
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+namespace YAF.Classes.Interfaces
+{
+  /// <summary>
+  /// The i yaf theme.
+  /// </summary>
+  public interface IYafTheme
+  {
+    /// <summary>
+    /// Get or Set the current Theme File
+    /// </summary>
+    string ThemeFile
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether LogMissingThemeItem.
+    /// </summary>
+    bool LogMissingThemeItem
+    {
+      get;
+      set;
+    }
+
+    /// <summary>
+    /// Gets ThemeDir.
+    /// </summary>
+    string ThemeDir
+    {
+      get;
+    }
+
+    /// <summary>
+    /// The get item.
+    /// </summary>
+    /// <param name="page">
+    /// The page.
+    /// </param>
+    /// <param name="tag">
+    /// The tag.
+    /// </param>
+    /// <returns>
+    /// The get item.
+    /// </returns>
+    string GetItem(string page, string tag);
+
+    /// <summary>
+    /// The get item.
+    /// </summary>
+    /// <param name="page">
+    /// The page.
+    /// </param>
+    /// <param name="tag">
+    /// The tag.
+    /// </param>
+    /// <param name="defaultValue">
+    /// The default value.
+    /// </param>
+    /// <returns>
+    /// The get item.
+    /// </returns>
+    string GetItem(string page, string tag, string defaultValue);
+
+    /// <summary>
+    /// Gets full path to the given theme file.
+    /// </summary>
+    /// <param name="filename">
+    /// Short name of theme file.
+    /// </param>
+    /// <returns>
+    /// The build theme path.
+    /// </returns>
+    string BuildThemePath(string filename);
+  }
+}
