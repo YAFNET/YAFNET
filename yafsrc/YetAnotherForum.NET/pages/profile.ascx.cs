686fb5dd01c47789ae74690c0a64ffe06f958fab
ederon-net <devnull@localhost>
2010-02-15 23:09:47 +0000

    Fixed View All Posts by User, there was user ID passed instead of username

--- yafsrc/YetAnotherForum.NET/pages/profile.ascx.cs
+++ yafsrc/YetAnotherForum.NET/pages/profile.ascx.cs
@@ -313,7 +313,7 @@ namespace YAF.Pages
       if (LastPosts.Visible)
       {
         LastPosts.DataSource = DB.post_alluser(this.PageContext.PageBoardID, userID, this.PageContext.PageUserID, 10);
-        SearchUser.NavigateUrl = YafBuildLink.GetLinkNotEscaped(ForumPages.search, "postedby={0}", userID);
+		  SearchUser.NavigateUrl = YafBuildLink.GetLinkNotEscaped(ForumPages.search, "postedby={0}", userDisplayName);
       }
 
       this.DataBind();
