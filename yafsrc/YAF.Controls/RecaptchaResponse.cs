ed65cef2781abd6a6b276bbaac70813dc13aeaef
Jaben <jaben@yetanotherforum.net>
2010-02-23 15:57:52 +0000

    Adding Copyright and documentation to new classes.

--- yafsrc/YAF.Controls/RecaptchaResponse.cs
+++ yafsrc/YAF.Controls/RecaptchaResponse.cs
@@ -1,54 +1,136 @@
-﻿namespace YAF.Controls
+﻿/* Yet Another Forum.NET
+ * Copyright (C) 2006-2010 Jaben Cargman
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+namespace YAF.Controls
 {
-    using System;
-    using YAF.Classes.Core;
+  /// <summary>
+  /// The recaptcha response.
+  /// </summary>
+  public class RecaptchaResponse
+  {
+    #region Constants and Fields
 
-    public class RecaptchaResponse
+    /// <summary>
+    /// The invalid solution.
+    /// </summary>
+    public static readonly RecaptchaResponse InvalidSolution = new RecaptchaResponse(false, "incorrect-captcha-sol");
+
+    /// <summary>
+    /// The recaptcha not reachable.
+    /// </summary>
+    public static readonly RecaptchaResponse RecaptchaNotReachable = new RecaptchaResponse(
+      false, "recaptcha-not-reachable");
+
+    /// <summary>
+    /// The valid.
+    /// </summary>
+    public static readonly RecaptchaResponse Valid = new RecaptchaResponse(true, string.Empty);
+
+    /// <summary>
+    /// The error code.
+    /// </summary>
+    private string errorCode;
+
+    /// <summary>
+    /// The is valid.
+    /// </summary>
+    private bool isValid;
+
+    #endregion
+
+    #region Constructors and Destructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RecaptchaResponse"/> class.
+    /// </summary>
+    /// <param name="isValid">
+    /// The is valid.
+    /// </param>
+    /// <param name="errorCode">
+    /// The error code.
+    /// </param>
+    internal RecaptchaResponse(bool isValid, string errorCode)
     {
-        private string errorCode;
-        public static readonly RecaptchaResponse InvalidSolution = new RecaptchaResponse(false, "incorrect-captcha-sol");
-        private bool isValid;
-        public static readonly RecaptchaResponse RecaptchaNotReachable = new RecaptchaResponse(false, "recaptcha-not-reachable");
-        public static readonly RecaptchaResponse Valid = new RecaptchaResponse(true, string.Empty);
-        
-
-
-        internal RecaptchaResponse(bool isValid, string errorCode)
-        {
-            this.isValid = isValid;
-            this.errorCode = errorCode;
-        }
-
-        public override bool Equals(object obj)
-        {
-            RecaptchaResponse response = (RecaptchaResponse) obj;
-            if (response == null)
-            {
-                return false;
-            }
-            return ((response.IsValid == this.IsValid) && (response.ErrorCode == this.ErrorCode));
-        }
-
-        public override int GetHashCode()
-        {
-            return (this.IsValid.GetHashCode() ^ this.ErrorCode.GetHashCode());
-        }
-
-        public string ErrorCode
-        {
-            get
-            {
-                return this.errorCode;
-            }
-        }
-
-        public bool IsValid
-        {
-            get
-            {
-                return this.isValid;
-            }
-        }
+      this.isValid = isValid;
+      this.errorCode = errorCode;
     }
-}
 
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets ErrorCode.
+    /// </summary>
+    public string ErrorCode
+    {
+      get
+      {
+        return this.errorCode;
+      }
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether IsValid.
+    /// </summary>
+    public bool IsValid
+    {
+      get
+      {
+        return this.isValid;
+      }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// The equals.
+    /// </summary>
+    /// <param name="obj">
+    /// The obj.
+    /// </param>
+    /// <returns>
+    /// The equals.
+    /// </returns>
+    public override bool Equals(object obj)
+    {
+      var response = (RecaptchaResponse)obj;
+      if (response == null)
+      {
+        return false;
+      }
+
+      return (response.IsValid == this.IsValid) && (response.ErrorCode == this.ErrorCode);
+    }
+
+    /// <summary>
+    /// The get hash code.
+    /// </summary>
+    /// <returns>
+    /// The get hash code.
+    /// </returns>
+    public override int GetHashCode()
+    {
+      return this.IsValid.GetHashCode() ^ this.ErrorCode.GetHashCode();
+    }
+
+    #endregion
+  }
+}
--- yafsrc/YAF.Controls/RecaptchaValidator.cs
+++ yafsrc/YAF.Controls/RecaptchaValidator.cs
@@ -1,133 +1,241 @@
-﻿namespace YAF.Controls
+﻿/* Yet Another Forum.NET
+ * Copyright (C) 2006-2010 Jaben Cargman
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+namespace YAF.Controls
 {
-    using System;
-    using System.Diagnostics;
-    using System.IO;
-    using System.Net;
-    using System.Net.Sockets;
-    using System.Text;
-    using System.Web;
-    using System.Web.UI;
-    using System.Web.UI.WebControls;
-    using YAF.Classes.Core;
+  #region Using
 
-    public class RecaptchaValidator
+  using System;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Net;
+  using System.Net.Sockets;
+  using System.Text;
+  using System.Web;
+
+  using YAF.Classes.Core;
+  using YAF.Classes.Data;
+
+  #endregion
+
+  /// <summary>
+  /// The recaptcha validator.
+  /// </summary>
+  public class RecaptchaValidator
+  {
+    #region Constants and Fields
+
+    /// <summary>
+    /// The verify url.
+    /// </summary>
+    private const string VerifyUrl = "http://api-verify.recaptcha.net/verify";
+
+    /// <summary>
+    /// The challenge.
+    /// </summary>
+    private string challenge;
+
+    /// <summary>
+    /// The private key.
+    /// </summary>
+    private string privateKey;
+
+    /// <summary>
+    /// The remote ip.
+    /// </summary>
+    private string remoteIp;
+
+    /// <summary>
+    /// The response.
+    /// </summary>
+    private string response;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets Challenge.
+    /// </summary>
+    public string Challenge
     {
-        private string challenge;
-        private string privateKey;
-        private string remoteIp;
-        private string response;
-        private const string VerifyUrl = "http://api-verify.recaptcha.net/verify";
+      get
+      {
+        return this.challenge;
+      }
 
-        private void CheckNotNull(object obj, string name)
-        {
-            if (obj == null)
-            {
-                throw new ArgumentNullException(name);
-            }
-        }
-
-        public RecaptchaResponse Validate()
-        {
-            string[] strArray;
-            this.CheckNotNull(this.PrivateKey, "PrivateKey");
-            this.CheckNotNull(this.RemoteIP, "RemoteIp");
-            this.CheckNotNull(this.Challenge, "Challenge");
-            this.CheckNotNull(this.Response, "Response");
-            if ((this.challenge == string.Empty) || (this.response == string.Empty))
-            {
-                return RecaptchaResponse.InvalidSolution;
-            }
-            HttpWebRequest request = (HttpWebRequest) WebRequest.Create("http://api-verify.recaptcha.net/verify");
-            request.ProtocolVersion = HttpVersion.Version10;
-            request.Timeout = 0x7530;
-            request.Method = "POST";
-            request.UserAgent = "reCAPTCHA/ASP.NET";
-            request.ContentType = "application/x-www-form-urlencoded";
-            string s = string.Format("privatekey={0}&remoteip={1}&challenge={2}&response={3}", new object[] { HttpUtility.UrlEncode(this.PrivateKey), HttpUtility.UrlEncode(this.RemoteIP), HttpUtility.UrlEncode(this.Challenge), HttpUtility.UrlEncode(this.Response)});
-            byte[] bytes = Encoding.ASCII.GetBytes(s);
-            using (Stream stream = request.GetRequestStream())
-            {
-                stream.Write(bytes, 0, bytes.Length);
-            }
-            try
-            {
-                using (WebResponse response = request.GetResponse())
-                {
-                    using (TextReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                    {
-                        strArray = reader.ReadToEnd().Split(new char[0]);
-                    }
-                }
-            }
-            catch (WebException exception)
-            {
-                YAF.Classes.Data.DB.eventlog_create(YafContext.Current.PageUserID, this, exception.Message, EventLogEntryType.Error);
-                return RecaptchaResponse.RecaptchaNotReachable;
-            }
-            switch (strArray[0])
-            {
-                case "true":
-                    return RecaptchaResponse.Valid;
-
-                case "false":
-                    return new RecaptchaResponse(false, strArray[1]);
-            }
-            throw new InvalidProgramException("Unknown status response.");
-        }
-
-        public string Challenge
-        {
-            get
-            {
-                return this.challenge;
-            }
-            set
-            {
-                this.challenge = value;
-            }
-        }
-
-        public string PrivateKey
-        {
-            get
-            {
-                return this.privateKey;
-            }
-            set
-            {
-                this.privateKey = value;
-            }
-        }
-
-        public string RemoteIP
-        {
-            get
-            {
-                return this.remoteIp;
-            }
-            set
-            {
-                IPAddress address = IPAddress.Parse(value);
-                if ((address == null) || ((address.AddressFamily != AddressFamily.InterNetwork) && (address.AddressFamily != AddressFamily.InterNetworkV6)))
-                {                   
-                    throw new ArgumentException("Expecting an IP address, got " + address);
-                }
-                this.remoteIp = address.ToString();
-            }
-        }
-
-        public string Response
-        {
-            get
-            {
-                return this.response;
-            }
-            set
-            {
-                this.response = value;
-            }
-        }
+      set
+      {
+        this.challenge = value;
+      }
     }
-}
 
+    /// <summary>
+    /// Gets or sets PrivateKey.
+    /// </summary>
+    public string PrivateKey
+    {
+      get
+      {
+        return this.privateKey;
+      }
+
+      set
+      {
+        this.privateKey = value;
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets RemoteIP.
+    /// </summary>
+    /// <exception cref="ArgumentException">
+    /// </exception>
+    public string RemoteIP
+    {
+      get
+      {
+        return this.remoteIp;
+      }
+
+      set
+      {
+        IPAddress address = IPAddress.Parse(value);
+        if ((address == null) ||
+            ((address.AddressFamily != AddressFamily.InterNetwork) &&
+             (address.AddressFamily != AddressFamily.InterNetworkV6)))
+        {
+          throw new ArgumentException("Expecting an IP address, got " + address);
+        }
+
+        this.remoteIp = address.ToString();
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets Response.
+    /// </summary>
+    public string Response
+    {
+      get
+      {
+        return this.response;
+      }
+
+      set
+      {
+        this.response = value;
+      }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// The validate.
+    /// </summary>
+    /// <returns>
+    /// </returns>
+    /// <exception cref="InvalidProgramException">
+    /// </exception>
+    public RecaptchaResponse Validate()
+    {
+      string[] strArray;
+      this.CheckNotNull(this.PrivateKey, "PrivateKey");
+      this.CheckNotNull(this.RemoteIP, "RemoteIp");
+      this.CheckNotNull(this.Challenge, "Challenge");
+      this.CheckNotNull(this.Response, "Response");
+      if ((this.challenge == string.Empty) || (this.response == string.Empty))
+      {
+        return RecaptchaResponse.InvalidSolution;
+      }
+
+      var request = (HttpWebRequest)WebRequest.Create("http://api-verify.recaptcha.net/verify");
+      request.ProtocolVersion = HttpVersion.Version10;
+      request.Timeout = 0x7530;
+      request.Method = "POST";
+      request.UserAgent = "reCAPTCHA/ASP.NET";
+      request.ContentType = "application/x-www-form-urlencoded";
+      string s = string.Format(
+        "privatekey={0}&remoteip={1}&challenge={2}&response={3}", 
+        new object[]
+          {
+            HttpUtility.UrlEncode(this.PrivateKey), HttpUtility.UrlEncode(this.RemoteIP), 
+            HttpUtility.UrlEncode(this.Challenge), HttpUtility.UrlEncode(this.Response)
+          });
+      byte[] bytes = Encoding.ASCII.GetBytes(s);
+      using (Stream stream = request.GetRequestStream())
+      {
+        stream.Write(bytes, 0, bytes.Length);
+      }
+
+      try
+      {
+        using (WebResponse response = request.GetResponse())
+        {
+          using (TextReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+          {
+            strArray = reader.ReadToEnd().Split(new char[0]);
+          }
+        }
+      }
+      catch (WebException exception)
+      {
+        DB.eventlog_create(YafContext.Current.PageUserID, this, exception.Message, EventLogEntryType.Error);
+        return RecaptchaResponse.RecaptchaNotReachable;
+      }
+
+      switch (strArray[0])
+      {
+        case "true":
+          return RecaptchaResponse.Valid;
+
+        case "false":
+          return new RecaptchaResponse(false, strArray[1]);
+      }
+
+      throw new InvalidProgramException("Unknown status response.");
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// The check not null.
+    /// </summary>
+    /// <param name="obj">
+    /// The obj.
+    /// </param>
+    /// <param name="name">
+    /// The name.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// </exception>
+    private void CheckNotNull(object obj, string name)
+    {
+      if (obj == null)
+      {
+        throw new ArgumentNullException(name);
+      }
+    }
+
+    #endregion
+  }
+}
