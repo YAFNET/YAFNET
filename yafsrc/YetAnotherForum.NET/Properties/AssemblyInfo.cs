818e98707a6b142186325fa02eab419d8bb3fca5
Jaben <jaben@yetanotherforum.net>
2010-05-28 08:25:16 +0000

    Most recent compile of v1.9.4. Properties setup correctly on YAF.dll. Resource handler is now compiled -- AND SO MUCH FASTER.

---
+++ yafsrc/YetAnotherForum.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("YAF")]
+[assembly: AssemblyDescription("Yet Another Forum.NET Main WAP DLL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tiny Gecko")]
+[assembly: AssemblyProduct("YAF")]
+[assembly: AssemblyCopyright("Copyright © 2006-2010 Yet Another Forum.NET")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7d89905-0836-48ac-b0f7-afaf15d0f91d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.9.4.0")]
+[assembly: AssemblyFileVersion("1.9.4.0")]
+
+#if (!COMPACT_FRAMEWORK)
+
+[assembly: AllowPartiallyTrustedCallers]
+#endif
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("..\\YetAnotherForum.NET.snk")]
