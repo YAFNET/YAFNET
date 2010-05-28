93bd9e4d2eacdaf23f5c7580f95a4cadc83c72bb
Jaben <jaben@yetanotherforum.net>
2010-05-28 17:14:32 +0000

    new dynamic features...

--- yafsrc/YetAnotherForum.NET/themes/cleanSlate/cleanslate.js
+++ yafsrc/YetAnotherForum.NET/themes/cleanSlate/cleanslate.js
@@ -20,5 +20,13 @@
         $(this).stop().fadeTo("fast", 1.0);
     });
 
+    $(".avatarimage").stop().fadeTo("fast", 0.5);
+
+    $(".yafnet .postContainer, .yafnet .postContainer_Alt").hover(function () {
+        $(this).find(".avatarimage").stop().fadeTo("fast", 1.0);
+    }, function () {
+        $(this).find(".avatarimage").stop().fadeTo("fast", 0.5);
+    });
+
 
 });
