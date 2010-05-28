0c7f0488e97d542f0b844cd9a664e652b11da3f6
Jaben <jaben@yetanotherforum.net>
2010-05-28 07:43:44 +0000

    yay! more jQuery animated stuff.

---
+++ yafsrc/YetAnotherForum.NET/themes/cleanSlate/cleanslate.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $(".yafnet .forumRow").hover(function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .forumRow_Alt").hover(function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .topicRow").hover(function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .topicRow_Alt").hover(function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+
+
+});
