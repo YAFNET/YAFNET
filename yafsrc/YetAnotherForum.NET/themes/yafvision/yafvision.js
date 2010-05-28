1de07108e593841a6082d844422d20405813b8d9
Jaben <jaben@yetanotherforum.net>
2010-05-28 07:29:40 +0000

    Added yafvision specific javascript...

---
+++ yafsrc/YetAnotherForum.NET/themes/yafvision/yafvision.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $(".yafnet .yafcssbigbutton").fadeTo("fast", 0.6);
+    $(".yafnet .yaflittlebutton").fadeTo("fast", 0.6);
+    $(".yafnet .yafcssbigbutton").hover(function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    }, function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    });
+    $(".yafnet .yaflittlebutton").hover(function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    }, function () {
+        $(this).stop().fadeTo("fast", 0.5);
+    });
+    $(".yafnet .forumRow").hover(function () {
+        $(this).stop().fadeTo("fast", 0.6);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .forumRow_Alt").hover(function () {
+        $(this).stop().fadeTo("fast", 0.6);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .topicRow").hover(function () {
+        $(this).stop().fadeTo("fast", 0.6);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+    $(".yafnet .topicRow_Alt").hover(function () {
+        $(this).stop().fadeTo("fast", 0.6);
+    }, function () {
+        $(this).stop().fadeTo("fast", 1.0);
+    });
+
+
+});
