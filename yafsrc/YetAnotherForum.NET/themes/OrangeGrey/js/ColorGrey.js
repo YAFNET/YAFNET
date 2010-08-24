6e96e4690af317337fc8ed1215cd9aa559b09056
w8tcha <thewatcher@watchersnet.de>
2010-08-24 10:24:10 +0000

    Added my ColorGrey Themes (Currently Contains 6 Different Colors -  Black, Blue, Green, Grey, Orange, Red)

---
+++ yafsrc/YetAnotherForum.NET/themes/OrangeGrey/js/ColorGrey.js
@@ -0,0 +1,19 @@
+﻿
+function pageLoad(){
+
+  // TipTip
+  if(!jQuery.browser.msie)
+  {
+    jQuery("a, img, input").tipTip();
+  }
+  else
+  {
+	  // jQuery Corner Test
+	  /*jQuery('.section').corner();
+      jQuery('.yaflittlebutton').corner();
+	  jQuery('.yafcssimagebutton').corner();
+      jQuery('.yafcssbigbutton').corner();
+	  jQuery('.pbutton').corner();
+	  jQuery('.yafPageLink').corner();*/
+  }
+}
