02c3a35d3d52b73989d1979e89f13089dee9f82e
w8tcha <thewatcher@watchersnet.de>
2010-09-01 10:16:04 +0000

    Updated TinyMCE Editor to Version 3.3.8

--- yafsrc/YetAnotherForum.NET/editors/tiny_mce/plugins/autoresize/editor_plugin_src.js
+++ yafsrc/YetAnotherForum.NET/editors/tiny_mce/plugins/autoresize/editor_plugin_src.js
@@ -1,29 +1,35 @@
 /**
-* $Id: editor_plugin_src.js 539 2008-01-14 19:08:58Z spocke $
-*
-* @author Moxiecode
-* @copyright Copyright © 2004-2008, Moxiecode Systems AB, All rights reserved.
-*/
+ * editor_plugin_src.js
+ *
+ * Copyright 2009, Moxiecode Systems AB
+ * Released under LGPL License.
+ *
+ * License: http://tinymce.moxiecode.com/license
+ * Contributing: http://tinymce.moxiecode.com/contributing
+ */
 
 (function() {
 	/**
-	* Auto Resize
-	* 
-	* This plugin automatically resizes the content area to fit its content height.
-	* It will retain a minimum height, which is the height of the content area when
-	* it's initialized.
-	*/
+	 * Auto Resize
+	 * 
+	 * This plugin automatically resizes the content area to fit its content height.
+	 * It will retain a minimum height, which is the height of the content area when
+	 * it's initialized.
+	 */
 	tinymce.create('tinymce.plugins.AutoResizePlugin', {
 		/**
-		* Initializes the plugin, this will be executed after the plugin has been created.
-		* This call is done before the editor instance has finished it's initialization so use the onInit event
-		* of the editor instance to intercept that event.
-		*
-		* @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
-		* @param {string} url Absolute URL to where the plugin is located.
-		*/
+		 * Initializes the plugin, this will be executed after the plugin has been created.
+		 * This call is done before the editor instance has finished it's initialization so use the onInit event
+		 * of the editor instance to intercept that event.
+		 *
+		 * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+		 * @param {string} url Absolute URL to where the plugin is located.
+		 */
 		init : function(ed, url) {
 			var t = this;
+
+			if (ed.getParam('fullscreen_is_enabled'))
+				return;
 
 			/**
 			 * This method gets executed each time the editor needs to resize.
@@ -53,16 +59,6 @@
 			// Define minimum height
 			t.autoresize_min_height = ed.getElement().offsetHeight;
 
-			// Things to do when the editor is ready
-			ed.onInit.add(function(ed, l) {
-				// Show throbber until content area is resized properly
-				ed.setProgressState(true);
-				t.throbbing = true;
-
-				// Hide scrollbars
-				ed.getBody().style.overflowY = "hidden";
-			});
-
 			// Add appropriate listeners for resizing content area
 			ed.onChange.add(resize);
 			ed.onSetContent.add(resize);
@@ -70,31 +66,43 @@
 			ed.onKeyUp.add(resize);
 			ed.onPostRender.add(resize);
 
-			ed.onLoadContent.add(function(ed, l) {
-				resize();
+			if (ed.getParam('autoresize_on_init', true)) {
+				// Things to do when the editor is ready
+				ed.onInit.add(function(ed, l) {
+					// Show throbber until content area is resized properly
+					ed.setProgressState(true);
+					t.throbbing = true;
 
-				// Because the content area resizes when its content CSS loads,
-				// and we can't easily add a listener to its onload event,
-				// we'll just trigger a resize after a short loading period
-				setTimeout(function() {
+					// Hide scrollbars
+					ed.getBody().style.overflowY = "hidden";
+				});
+
+				ed.onLoadContent.add(function(ed, l) {
 					resize();
 
-					// Disable throbber
-					ed.setProgressState(false);
-					t.throbbing = false;
-				}, 1250);
-			});
+					// Because the content area resizes when its content CSS loads,
+					// and we can't easily add a listener to its onload event,
+					// we'll just trigger a resize after a short loading period
+					setTimeout(function() {
+						resize();
+
+						// Disable throbber
+						ed.setProgressState(false);
+						t.throbbing = false;
+					}, 1250);
+				});
+			}
 
 			// Register the command so that it can be invoked by using tinyMCE.activeEditor.execCommand('mceExample');
 			ed.addCommand('mceAutoResize', resize);
 		},
 
 		/**
-		* Returns information about the plugin as a name/value array.
-		* The current keys are longname, author, authorurl, infourl and version.
-		*
-		* @return {Object} Name/value array containing information about the plugin.
-		*/
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
 		getInfo : function() {
 			return {
 				longname : 'Auto Resize',
