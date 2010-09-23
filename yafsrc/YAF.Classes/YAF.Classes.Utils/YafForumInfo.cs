8385e6eb1a4500e119ca1e797312bc6e83ee5a6b
Jaben <jaben@yetanotherforum.net>
2010-09-23 18:13:18 +0000

    v1.9.4.1

--- yafsrc/YAF.Classes/YAF.Classes.Utils/YafForumInfo.cs
+++ yafsrc/YAF.Classes/YAF.Classes.Utils/YafForumInfo.cs
@@ -128,7 +128,7 @@ namespace YAF.Classes.Utils
     {
       get
       {
-        return 0x01094120;
+        return 0x01094100;
       }
     }
 
@@ -139,7 +139,7 @@ namespace YAF.Classes.Utils
     {
       get
       {
-        return new DateTime(2010, 9, 1);
+        return new DateTime(2010, 9, 23);
       }
     }
 
