d6fefb9cdbc64d91d6fd136bbcd624b90770ab29
w8tcha <thewatcher@watchersnet.de>
2010-09-24 13:17:50 +0000

    Updates for my Themes td->th changes, ie9 buttons fixed, rounded avatars.

--- yafsrc/YetAnotherForum.NET/themes/BlackGrey/js/ColorGrey.js
+++ yafsrc/YetAnotherForum.NET/themes/BlackGrey/js/ColorGrey.js
@@ -16,4 +16,6 @@ function pageLoad(){
 	  jQuery('.pbutton').corner();
 	  jQuery('.yafPageLink').corner();*/
   }
+  jQuery("img.avatarimage").load(function() {jQuery(this).wrap('<span class="' + jQuery(this).attr('class') + '" style="background:url(' + jQuery(this).attr('src') + ') no-repeat ;background-size:100%; width: ' + jQuery(this).width() + 'px; height: ' + jQuery(this).height() + 'px;" />');jQuery(this).css("opacity","0");});
+  
 }
