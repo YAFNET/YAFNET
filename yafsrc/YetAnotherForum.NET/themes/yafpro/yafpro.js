cf175a35215556bacace61272e878e898fe163ea
Jaben <jaben@yetanotherforum.net>
2010-10-03 01:57:21 +0000

    Updating and cleaning out old themes that just aren't good anymore.

---
+++ yafsrc/YetAnotherForum.NET/themes/yafpro/yafpro.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(".yafnet .forumRow, .yafnet .forumRow_Alt, .yafnet .topicRow, .yafnet .topicRow_Alt").hover(function () {
+        $(this).find("img").stop().fadeTo("fast", 0.5);
+    }, function () {
+        $(this).find("img").stop().fadeTo("fast", 1.0);
+    });
+
+    $(".avatarimage").stop().fadeTo("fast", 0.5);
+
+    $(".yafnet .postContainer, .yafnet .postContainer_Alt").hover(function () {
+        $(this).find(".avatarimage").stop().fadeTo("fast", 1.0);
+    }, function () {
+        $(this).find(".avatarimage").stop().fadeTo("fast", 0.5);
+    });
+});
