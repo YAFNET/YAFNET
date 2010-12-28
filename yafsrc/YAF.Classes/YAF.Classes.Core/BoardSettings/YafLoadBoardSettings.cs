7b4eee6fd537a74b514fffa185c9fcbfc6349223
Jaben <jaben@yetanotherforum.net>
2010-12-28 04:19:58 +0000

    Updates before move.

--- yafsrc/YAF.Classes/YAF.Classes.Core/BoardSettings/YafLoadBoardSettings.cs
+++ yafsrc/YAF.Classes/YAF.Classes.Core/BoardSettings/YafLoadBoardSettings.cs
@@ -16,16 +16,20 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-namespace YAF.Classes.Core
+namespace YAF.Core
 {
   #region Using
 
   using System;
   using System.Data;
 
+  using YAF.Classes;
   using YAF.Classes.Data;
-  using YAF.Classes.Pattern;
-  using YAF.Classes.Utils;
+  using YAF.Utils;
+  using YAF.Utils.Helpers.StringUtils;
+  using YAF.Core.Services;
+  using YAF.Types;
+  using YAF.Types.Interfaces;
 
   #endregion
 
