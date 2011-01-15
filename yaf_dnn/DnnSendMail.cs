04cbdb067d08f68a16c37e4b79f5eec389a8da1c
w8tcha <thewatcher@watchersnet.de>
2011-01-15 19:05:02 +0000

    Added Class for DNN SendMail.

---
+++ yaf_dnn/DnnSendMail.cs
@@ -0,0 +1,143 @@
+﻿/* Yet Another Forum.net
+ * Copyright (C) 2006-2010 Jaben Cargman
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace YAF.DotNetNuke
+{
+    #region Using
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Mail;
+    using System.Threading;
+    using global::DotNetNuke.Entities.Portals;
+    using global::DotNetNuke.Services.Mail;
+
+    using YAF.Core.Services;
+    using YAF.Types;
+    using YAF.Types.Interfaces;
+
+    using MailPriority = global::DotNetNuke.Services.Mail.MailPriority;
+
+    #endregion
+
+    /// <summary>
+    /// Functions to send email via SMTP
+    /// </summary>
+    public class DnnSendMail : ISendMail
+    {
+
+        /// <summary>
+        /// Gets CurrentPortalSettings.
+        /// </summary>
+        private PortalSettings CurrentPortalSettings
+        {
+            get
+            {
+                return PortalController.GetCurrentPortalSettings();
+            }
+        }
+
+        #region Public Methods
+
+        /// <summary>
+        /// Sends all MailMessages via the SmtpClient. Doesn't handle any exceptions.
+        /// </summary>
+        /// <param name="messages">
+        /// The messages.
+        /// </param>
+        public void SendAll([NotNull] IEnumerable<MailMessage> messages)
+        {
+            CodeContracts.ArgumentNotNull(messages, "messages");
+
+            foreach (var mailMessage in messages)
+            {
+                this.Send(mailMessage);
+            }
+
+          /*  messages.ToList().ForEach(
+                m => m.Send());*/
+        }
+
+        /// <summary>
+        /// The send all isolated.
+        /// </summary>
+        /// <param name="messages">
+        /// The messages.
+        /// </param>
+        /// <param name="handleExceptionAction">
+        /// The handle exception action.
+        /// </param>
+        public  void SendAllIsolated([NotNull] IEnumerable<MailMessage> messages, [CanBeNull] Action<MailMessage, Exception> handleExceptionAction)
+        {
+            CodeContracts.ArgumentNotNull(messages, "messages");
+
+            foreach (var mailMessage in messages.ToList())
+            {
+                try
+                {
+                    // send the message...
+                    //message.Send();
+
+                    this.Send(mailMessage);
+
+
+                    // sleep for a 1/20 of a second...
+                    Thread.Sleep(50);
+                }
+                catch (Exception ex)
+                {
+                    if (handleExceptionAction != null)
+                    {
+                        handleExceptionAction(mailMessage, ex);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a SmtpClient and sends a MailMessage.
+        /// </summary>
+        /// <param name="mailMessage">
+        /// The message.
+        /// </param>
+        public void Send([NotNull] MailMessage mailMessage)
+        {
+            CodeContracts.ArgumentNotNull(mailMessage, "mailMessage");
+
+            Mail.SendMail(
+                     mailMessage.From.ToString(),
+                     mailMessage.To.ToString(),
+                     string.Empty,
+                     string.Empty,
+                     MailPriority.Normal,
+                     mailMessage.Subject,
+                     mailMessage.IsBodyHtml ? MailFormat.Html : MailFormat.Text,
+                     mailMessage.BodyEncoding,
+                     mailMessage.Body,
+                     string.Empty,
+                     Convert.ToString(this.CurrentPortalSettings.HostSettings["SMTPServer"]),
+                     Convert.ToString(this.CurrentPortalSettings.HostSettings["SMTPAuthentication"]),
+                     Convert.ToString(this.CurrentPortalSettings.HostSettings["SMTPUsername"]),
+                     Convert.ToString(this.CurrentPortalSettings.HostSettings["SMTPPassword"]));
+        }
+
+        #endregion
+    }
+}
