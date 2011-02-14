808f15e0f0938df5a7de93588d6b979264cbe901
w8tcha <thewatcher@watchersnet.de>
2011-02-14 11:05:39 +0000

    Fixed display of avatar images on the View Profile Page.

--- yafsrc/YetAnotherForum.NET/themes/OrangeGrey/js/ColorGrey.js
+++ yafsrc/YetAnotherForum.NET/themes/OrangeGrey/js/ColorGrey.js
@@ -6,7 +6,7 @@
   if(!jQuery.browser.msie)
   {
 	 jQuery("img.avatarimage").load(function() {
-		 jQuery(this).wrap('<span class="' + jQuery(this).attr('class') + '" style="background:url(' + jQuery(this).attr('src') + ') no-repeat ;background-size:100%; width: ' + jQuery(this).width() + 'px; height: ' + jQuery(this).height() + 'px;" />');jQuery(this).css("opacity","0");
+		 jQuery(this).wrap('<span class="' + jQuery(this).attr('class') + '" style="background:url(' + jQuery(this).attr('src') + ') no-repeat ;background-size:100%; width: auto; height: auto;" />');jQuery(this).css("opacity","0");
 	 });
   }
   else
@@ -14,7 +14,7 @@
 	 if(jQuery.browser.version >= 9)
   {
 	   jQuery("img.avatarimage").load(function() {
-		 jQuery(this).wrap('<span class="' + jQuery(this).attr('class') + '" style="background:url(' + jQuery(this).attr('src') + ') no-repeat ;background-size:100%; width: ' + jQuery(this).width() + 'px; height: ' + jQuery(this).height() + 'px;" />');jQuery(this).css("opacity","0");
+		 jQuery(this).wrap('<span class="' + jQuery(this).attr('class') + '" style="background:url(' + jQuery(this).attr('src') + ') no-repeat ;background-size:100%; width:auto; height: auto;" />');jQuery(this).css("opacity","0");
 	 });
   }
   }
