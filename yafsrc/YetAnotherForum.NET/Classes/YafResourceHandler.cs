c31cb26641acd4823c4027ea1833e5080bed453a
w8tcha <thewatcher@watchersnet.de>
2011-02-16 11:13:16 +0000

    was causing some problems

--- yafsrc/YetAnotherForum.NET/Classes/YafResourceHandler.cs
+++ yafsrc/YetAnotherForum.NET/Classes/YafResourceHandler.cs
@@ -82,7 +82,7 @@ namespace YAF
         // resource request
         GetResource(context);
       }
-      else if (context.Request.UrlReferrer.AbsoluteUri.Contains(BaseUrlBuilder.BaseUrl) || context.Session["lastvisit"] != null)
+      else if (context.Session["lastvisit"] != null || context.Request.UrlReferrer.AbsoluteUri.Contains(BaseUrlBuilder.BaseUrl))
       {
         if (context.Request.QueryString.GetFirstOrDefault("u") != null)
         {
