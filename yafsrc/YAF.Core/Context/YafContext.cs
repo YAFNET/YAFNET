c90fd2dc2723eb0d9efb5f6e51a40a263ac8e762
Jaben <jaben@yetanotherforum.net>
2011-10-17 23:26:40 -0400

    Finally cleaned up the terrible page loading a bit. Still plenty more to do.

--- yafsrc/YAF.Core/Context/YafContext.cs
+++ yafsrc/YAF.Core/Context/YafContext.cs
@@ -18,409 +18,454 @@
  */
 namespace YAF.Core
 {
-  #region Using
+	#region Using
 
-  using System;
-  using System.Web;
-  using System.Web.Profile;
-  using System.Web.Security;
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Web;
+	using System.Web.Profile;
+	using System.Web.Security;
 
-  using Autofac;
+	using Autofac;
 
-  using YAF.Classes;
-  using YAF.Classes.Pattern;
-  using YAF.Core.Services;
-  using YAF.Types;
-  using YAF.Types.Constants;
-  using YAF.Types.Interfaces;
-  using YAF.Utils;
-  using YAF.Utils.Helpers;
+	using YAF.Classes;
+	using YAF.Classes.Data;
+	using YAF.Classes.Pattern;
+	using YAF.Core.Services;
+	using YAF.Types;
+	using YAF.Types.Constants;
+	using YAF.Types.EventProxies;
+	using YAF.Types.Interfaces;
+	using YAF.Utils;
+	using YAF.Utils.Helpers;
 
-  #endregion
+	#endregion
 
-  /// <summary>
-  /// Context class that accessible with the same instance from all locations
-  /// </summary>
-  public class 
-    YafContext : UserPageBase, IDisposable, IHaveServiceLocator
-  {
-    #region Constants and Fields
+	/// <summary>
+	/// Context class that accessible with the same instance from all locations
+	/// </summary>
+	public class YafContext : UserPageBase, IDisposable, IHaveServiceLocator
+	{
+		#region Constants and Fields
 
-    /// <summary>
-    /// The _context lifetime container.
-    /// </summary>
-    protected ILifetimeScope _contextLifetimeContainer;
+		/// <summary>
+		/// The _context lifetime container.
+		/// </summary>
+		protected ILifetimeScope _contextLifetimeContainer;
 
-    /// <summary>
-    ///   The _current forum page.
-    /// </summary>
-    protected ForumPage _currentForumPage;
+		/// <summary>
+		///   The _current forum page.
+		/// </summary>
+		protected ForumPage _currentForumPage;
 
-    /// <summary>
-    ///   The _repository.
-    /// </summary>
-    protected ContextVariableRepository _repository;
+		/// <summary>
+		///   The _repository.
+		/// </summary>
+		protected ContextVariableRepository _repository;
 
-    /// <summary>
-    ///   The _single instance factory.
-    /// </summary>
-    protected SingleClassInstanceFactory _singleInstanceFactory = new SingleClassInstanceFactory();
+		/// <summary>
+		///   The _single instance factory.
+		/// </summary>
+		protected SingleClassInstanceFactory _singleInstanceFactory = new SingleClassInstanceFactory();
 
-    /// <summary>
-    ///   The _variables.
-    /// </summary>
-    protected TypeDictionary _variables = new TypeDictionary();
+		/// <summary>
+		///   The _variables.
+		/// </summary>
+		protected TypeDictionary _variables = new TypeDictionary();
 
-    /// <summary>
-    ///   The _user.
-    /// </summary>
-    private MembershipUser _user;
+		/// <summary>
+		///   The _user.
+		/// </summary>
+		protected MembershipUser _user = null;
 
-    #endregion
+		#endregion
 
-    #region Constructors and Destructors
+		#region Constructors and Destructors
 
-    /// <summary>
-    ///   Initializes a new instance of the <see cref = "YafContext" /> class. 
-    ///   YafContext Constructor
-    /// </summary>
-    public YafContext()
-    {
-      this._contextLifetimeContainer = GlobalContainer.Container.BeginLifetimeScope(YafLifetimeScope.Context);
+		/// <summary>
+		///   Initializes a new instance of the <see cref = "YafContext" /> class. 
+		///   YafContext Constructor
+		/// </summary>
+		public YafContext()
+		{
+			this._contextLifetimeContainer = GlobalContainer.Container.BeginLifetimeScope(YafLifetimeScope.Context);
 
-      // init the respository
-      this._repository = new ContextVariableRepository(this._variables);
+			// init the respository
+			this._repository = new ContextVariableRepository(this._variables);
 
-      // init context...
-      if (this.Init != null)
-      {
-        this.Init(this, new EventArgs());
-      }
-    }
+			// init context...
+			if (this.Init != null)
+			{
+				this.Init(this, new EventArgs());
+			}
+		}
 
-    #endregion
+		#endregion
 
-    #region Events
+		#region Events
 
-    /// <summary>
-    ///   On YafContext Constructor Call
-    /// </summary>
-    public event EventHandler<EventArgs> Init;
+		/// <summary>
+		///   On YafContext Constructor Call
+		/// </summary>
+		public event EventHandler<EventArgs> Init;
 
-    /// <summary>
-    ///   On YafContext Unload Call
-    /// </summary>
-    public event EventHandler<EventArgs> Unload;
+		/// <summary>
+		///   On YafContext Unload Call
+		/// </summary>
+		public event EventHandler<EventArgs> Unload;
 
-    #endregion
+		#endregion
 
-    #region Properties
+		#region Properties
 
-    /// <summary>
-    ///   Get the instance of the Forum Context
-    /// </summary>
-    public static YafContext Current
-    {
-      get
-      {
-        return PageSingleton<YafContext>.Instance;
-      }
-    }
+		/// <summary>
+		///   Get the instance of the Forum Context
+		/// </summary>
+		public static YafContext Current
+		{
+			get
+			{
+				return PageSingleton<YafContext>.Instance;
+			}
+		}
 
-    /// <summary>
-    /// Returns the current application.
-    /// </summary>
-    [Obsolete("Use Service Location: this.Get<HttpApplicationStateBase>()")]
-    public static HttpApplicationStateBase Application
-    {
-      get
-      {
-        return GlobalContainer.Container.Resolve<HttpApplicationStateBase>();
-      }
-    }
+		/// <summary>
+		/// Returns the current application.
+		/// </summary>
+		[Obsolete("Use Service Location: this.Get<HttpApplicationStateBase>()")]
+		public static HttpApplicationStateBase Application
+		{
+			get
+			{
+				return GlobalContainer.Container.Resolve<HttpApplicationStateBase>();
+			}
+		}
 
-    /// <summary>
-    ///   Current Board Settings
-    /// </summary>
-    [Obsolete("Use ServiceLocation or DI directly: this.Get<YafBoardSettings>().")]
-    public virtual YafBoardSettings BoardSettings
-    {
-      get
-      {
-        return this.Get<YafBoardSettings>();
-      }
+		/// <summary>
+		///   Current Board Settings
+		/// </summary>
+		[Obsolete("Use ServiceLocation or DI directly: this.Get<YafBoardSettings>().")]
+		public virtual YafBoardSettings BoardSettings
+		{
+			get
+			{
+				return this.Get<YafBoardSettings>();
+			}
 
-      set
-      {
-        this.Get<CurrentBoardSettings>().Instance = value;
-      }
-    }
+			set
+			{
+				this.Get<CurrentBoardSettings>().Instance = value;
+			}
+		}
 
-    /// <summary>
-    ///   Forum page instance of the current forum page.
-    ///   May not be valid until everything is initialized.
-    /// </summary>
-    public ForumPage CurrentForumPage
-    {
-      get
-      {
-        return this._currentForumPage;
-      }
+		/// <summary>
+		///   Forum page instance of the current forum page.
+		///   May not be valid until everything is initialized.
+		/// </summary>
+		public ForumPage CurrentForumPage
+		{
+			get
+			{
+				return this._currentForumPage;
+			}
 
-      set
-      {
-        this._currentForumPage = value;
-      }
-    }
+			set
+			{
+				this._currentForumPage = value;
+			}
+		}
 
-    /// <summary>
-    ///   Instance of the Combined UserData for the current user.
-    /// </summary>
-    public IUserData CurrentUserData
-    {
-      get
-      {
-        return this._singleInstanceFactory.GetInstance<CombinedUserDataHelper>();
-      }
-    }
+		/// <summary>
+		///   Instance of the Combined UserData for the current user.
+		/// </summary>
+		public IUserData CurrentUserData
+		{
+			get
+			{
+				return this._singleInstanceFactory.GetInstance<CombinedUserDataHelper>();
+			}
+		}
 
-    /// <summary>
-    ///   Current Page Instance of the Module Manager
-    /// </summary>
-    [Obsolete("Use Service Location or Dependency Injection to get interface: IModuleManager<ForumEditor>")]
-    public IModuleManager<ForumEditor> EditorModuleManager
-    {
-      get
-      {
-        return this.Get<IModuleManager<ForumEditor>>();
-      }
-    }
+		/// <summary>
+		///   Current Page Instance of the Module Manager
+		/// </summary>
+		[Obsolete("Use Service Location or Dependency Injection to get interface: IModuleManager<ForumEditor>")]
+		public IModuleManager<ForumEditor> EditorModuleManager
+		{
+			get
+			{
+				return this.Get<IModuleManager<ForumEditor>>();
+			}
+		}
 
-    /// <summary>
-    ///   Get the current page as the forumPage Enum (for comparison)
-    /// </summary>
-    public ForumPages ForumPageType
-    {
-      get
-      {
-        if (this.Get<HttpRequestBase>().QueryString.GetFirstOrDefault("g").IsNotSet())
-        {
-          return ForumPages.forum;
-        }
+		/// <summary>
+		///   Get the current page as the forumPage Enum (for comparison)
+		/// </summary>
+		public ForumPages ForumPageType
+		{
+			get
+			{
+				if (this.Get<HttpRequestBase>().QueryString.GetFirstOrDefault("g").IsNotSet())
+				{
+					return ForumPages.forum;
+				}
 
-        try
-        {
-          return this.Get<HttpRequestBase>().QueryString.GetFirstOrDefault("g").ToEnum<ForumPages>(true);
-        }
-        catch (Exception)
-        {
-          return ForumPages.forum;
-        }
-      }
-    }
+				try
+				{
+					return this.Get<HttpRequestBase>().QueryString.GetFirstOrDefault("g").ToEnum<ForumPages>(true);
+				}
+				catch (Exception)
+				{
+					return ForumPages.forum;
+				}
+			}
+		}
 
-    /// <summary>
-    ///   Access to the Context Global Variable Repository Class
-    ///   which is a helper class that accesses YafContext.Vars with strongly
-    ///   typed properties for primary variables.
-    /// </summary>
-    public ContextVariableRepository Globals
-    {
-      get
-      {
-        return this._repository;
-      }
-    }
+		/// <summary>
+		///   Access to the Context Global Variable Repository Class
+		///   which is a helper class that accesses YafContext.Vars with strongly
+		///   typed properties for primary variables.
+		/// </summary>
+		public ContextVariableRepository Globals
+		{
+			get
+			{
+				return this._repository;
+			}
+		}
 
-    /// <summary>
-    ///   Current Page Load Message
-    /// </summary>
-    public LoadMessage LoadMessage
-    {
-      get
-      {
-        return this._singleInstanceFactory.GetInstance<LoadMessage>();
-      }
-    }
+		/// <summary>
+		///   Current Page Load Message
+		/// </summary>
+		public LoadMessage LoadMessage
+		{
+			get
+			{
+				return this._singleInstanceFactory.GetInstance<LoadMessage>();
+			}
+		}
 
-    /// <summary>
-    ///   Current Page Elements
-    /// </summary>
-    public PageElementRegister PageElements
-    {
-      get
-      {
-        return this._singleInstanceFactory.GetInstance<PageElementRegister>();
-      }
-    }
+		/// <summary>
+		///   Current Page Elements
+		/// </summary>
+		public PageElementRegister PageElements
+		{
+			get
+			{
+				return this._singleInstanceFactory.GetInstance<PageElementRegister>();
+			}
+		}
 
-    /// <summary>
-    ///   Current Page User Profile
-    /// </summary>
-    public YafUserProfile Profile
-    {
-      get
-      {
-        return (YafUserProfile)HttpContext.Current.Profile;
-      }
-    }
+		/// <summary>
+		///   Current Page User Profile
+		/// </summary>
+		public YafUserProfile Profile
+		{
+			get
+			{
+				return (YafUserProfile)HttpContext.Current.Profile;
+			}
+		}
 
-    /// <summary>
-    ///   Current Page Query ID Helper
-    /// </summary>
-    public QueryStringIDHelper QueryIDs
-    {
-      get
-      {
-        return this._singleInstanceFactory.GetInstance<QueryStringIDHelper>();
-      }
+		/// <summary>
+		///   Current Page Query ID Helper
+		/// </summary>
+		public QueryStringIDHelper QueryIDs
+		{
+			get
+			{
+				return this._singleInstanceFactory.GetInstance<QueryStringIDHelper>();
+			}
 
-      set
-      {
-        this._singleInstanceFactory.SetInstance(value);
-      }
-    }
+			set
+			{
+				this._singleInstanceFactory.SetInstance(value);
+			}
+		}
 
-    /// <summary>
-    ///   Provides access to the Service Locatorer
-    /// </summary>
-    public IServiceLocator ServiceLocator
-    {
-      get
-      {
-        return this._contextLifetimeContainer.Resolve<IServiceLocator>();
-      }
-    }
+		/// <summary>
+		///   Provides access to the Service Locatorer
+		/// </summary>
+		public IServiceLocator ServiceLocator
+		{
+			get
+			{
+				return this._contextLifetimeContainer.Resolve<IServiceLocator>();
+			}
+		}
 
-    /// <summary>
-    ///   Current Page Control Settings from Forum Control
-    /// </summary>
-    public YafControlSettings Settings
-    {
-      get
-      {
-        return YafControlSettings.Current;
-      }
-    }
+		/// <summary>
+		///   Current Page Control Settings from Forum Control
+		/// </summary>
+		public YafControlSettings Settings
+		{
+			get
+			{
+				return YafControlSettings.Current;
+			}
+		}
 
-    /// <summary>
-    /// Gets Theme.
-    /// </summary>
-    [Obsolete("Use Service Location or Dependency Injection to get interface: ITheme")]
-    public ITheme Theme
-    {
-      get
-      {
-        return this.Get<ITheme>();
-      }
-    }
+		/// <summary>
+		/// Gets Theme.
+		/// </summary>
+		[Obsolete("Use Service Location or Dependency Injection to get interface: ITheme")]
+		public ITheme Theme
+		{
+			get
+			{
+				return this.Get<ITheme>();
+			}
+		}
 
-    /// <summary>
-    ///   Gets the UrlBuilder
-    /// </summary>
-    public IUrlBuilder UrlBuilder
-    {
-      get
-      {
-        return YafFactoryProvider.UrlBuilder;
-      }
-    }
+		/// <summary>
+		///   Gets the UrlBuilder
+		/// </summary>
+		public IUrlBuilder UrlBuilder
+		{
+			get
+			{
+				return YafFactoryProvider.UrlBuilder;
+			}
+		}
 
-    /// <summary>
-    ///   Current Membership User
-    /// </summary>
-    public MembershipUser User
-    {
-      get
-      {
-        if (this._user == null)
-        {
-          this._user = UserMembershipHelper.GetUser();
-        }
+		/// <summary>
+		///   Current Membership User
+		/// </summary>
+		public MembershipUser User
+		{
+			get
+			{
+				if (this._user == null)
+				{
+					this._user = UserMembershipHelper.GetUser(true);
+				}
 
-        return this._user;
-      }
+				return this._user;
+			}
 
-      set
-      {
-        this._user = value;
-      }
-    }
+			set
+			{
+				this._user = value;
+			}
+		}
 
-    /// <summary>
-    ///   YafContext Global Instance Variables
-    ///   Use for plugins or other situations where a value is needed per instance.
-    /// </summary>
-    public TypeDictionary Vars
-    {
-      get
-      {
-        return this._variables;
-      }
-    }
+		/// <summary>
+		///   YafContext Global Instance Variables
+		///   Use for plugins or other situations where a value is needed per instance.
+		/// </summary>
+		public TypeDictionary Vars
+		{
+			get
+			{
+				return this._variables;
+			}
+		}
 
-    #endregion
+		#endregion
 
-    #region Indexers
+		#region Indexers
 
-    /// <summary>
-    ///   Returns a value from the YafContext Global Instance Variables (Vars) collection.
-    /// </summary>
-    /// <param name = "varName"></param>
-    /// <returns>Value if it's found, null if it doesn't exist.</returns>
-    public object this[[NotNull] string varName]
-    {
-      get
-      {
-        if (this._variables.ContainsKey(varName))
-        {
-          return this._variables[varName];
-        }
+		/// <summary>
+		///   Returns a value from the YafContext Global Instance Variables (Vars) collection.
+		/// </summary>
+		/// <param name = "varName"></param>
+		/// <returns>Value if it's found, null if it doesn't exist.</returns>
+		public object this[[NotNull] string varName]
+		{
+			get
+			{
+				if (this._variables.ContainsKey(varName))
+				{
+					return this._variables[varName];
+				}
 
-        return null;
-      }
+				return null;
+			}
 
-      set
-      {
-        this._variables[varName] = value;
-      }
-    }
+			set
+			{
+				this._variables[varName] = value;
+			}
+		}
 
-    #endregion
+		#region Events
 
-    #region Public Methods
+		/// <summary>
+		///   The after init.
+		/// </summary>
+		public event EventHandler<EventArgs> AfterInit;
 
-    /// <summary>
-    /// Helper Function that adds a "load message" to the load message class.
-    /// </summary>
-    /// <param name="loadMessage">
-    /// </param>
-    public void AddLoadMessage([NotNull] string loadMessage)
-    {
-      this.LoadMessage.Add(loadMessage);
-    }
+		/// <summary>
+		///   The before init.
+		/// </summary>
+		public event EventHandler<EventArgs> BeforeInit;
 
-    #endregion
+		#endregion
 
-    #region Implemented Interfaces
+		protected override void InitUserAndPage()
+		{
+			if (!this._initUserPage)
+			{
+				if (this.BeforeInit != null)
+				{
+					this.BeforeInit(this, new EventArgs());
+				}
 
-    #region IDisposable
+				if (this.User != null && this.Get<HttpSessionStateBase>()["UserUpdated"] == null)
+				{
+					RoleMembershipHelper.UpdateForumUser(this.User, this.PageBoardID);
+					this.Get<HttpSessionStateBase>()["UserUpdated"] = true;
+				}
 
-    /// <summary>
-    /// The dispose.
-    /// </summary>
-    public void Dispose()
-    {
-      if (this.Unload != null)
-      {
-        this.Unload(this, new EventArgs());
-      }
+				var pageLoadEvent = new InitPageLoadEvent();
 
-      this._contextLifetimeContainer.Dispose();
-    }
+				this.Get<IRaiseEvent>().Raise(pageLoadEvent);
 
-    #endregion
+				this.Page = pageLoadEvent.DataDictionary;
 
-    #endregion
-  }
+				if (this.AfterInit != null)
+				{
+					this.AfterInit(this, new EventArgs());
+				}
+			}
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Helper Function that adds a "load message" to the load message class.
+		/// </summary>
+		/// <param name="loadMessage">
+		/// </param>
+		public void AddLoadMessage([NotNull] string loadMessage)
+		{
+			this.LoadMessage.Add(loadMessage);
+		}
+
+		#endregion
+
+		#region Implemented Interfaces
+
+		#region IDisposable
+
+		/// <summary>
+		/// The dispose.
+		/// </summary>
+		public void Dispose()
+		{
+			if (this.Unload != null)
+			{
+				this.Unload(this, new EventArgs());
+			}
+
+			this._contextLifetimeContainer.Dispose();
+		}
+
+		#endregion
+
+		#endregion
+	}
 }
