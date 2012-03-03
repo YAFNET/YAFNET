9fda1898eefb19843052f6730e357d11cbf9e9eb
Watcher <Watcher@Watcher-One>
2012-03-03 12:52:24 +0100

    Updated TinyMce to Version 3.4.9

--- yafsrc/YetAnotherForum.NET/editors/tiny_mce/tiny_mce_init.js
+++ yafsrc/YetAnotherForum.NET/editors/tiny_mce/tiny_mce_init.js
@@ -8,7 +8,7 @@ tinyMCE.init({
 	theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
 	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
 	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,scayt",
+	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_statusbar_location : "bottom",
@@ -19,6 +19,10 @@ tinyMCE.init({
 		media_external_list_url: "lists/media_list.js",
         convert_urls : false,
 		language: editorLanguage,
+ // turn on/off SCAYT autostartup  
+                scayt_auto_startup: true,  
+
+ scayt_slang: "en_US", 
 
 // Replace values for the template plugin
 		template_replace_values : {
