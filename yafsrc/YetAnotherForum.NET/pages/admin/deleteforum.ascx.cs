f063fd842326dbf620b47d6fd8a484e5f5e7779f
Watcher <Watcher@Watcher-One>
2012-03-19 12:17:20 +0100

    Last Change set wasnt compiling fixed

--- yafsrc/YetAnotherForum.NET/pages/admin/deleteforum.ascx.cs
+++ yafsrc/YetAnotherForum.NET/pages/admin/deleteforum.ascx.cs
@@ -20,281 +20,265 @@
 
 namespace YAF.Pages.Admin
 {
-	#region Using
+    #region Using
 
-	using System;
-	using System.Data;
+    using System;
+    using System.Data;
 
-	using YAF.Classes;
-	using YAF.Classes.Data;
-	using YAF.Controls;
-	using YAF.Core;
-	using YAF.Core.Tasks;
-	using YAF.Types;
-	using YAF.Types.Constants;
-	using YAF.Types.Interfaces;
-	using YAF.Utilities;
-	using YAF.Utils;
+    using YAF.Classes;
+    using YAF.Classes.Data;
+    using YAF.Core;
+    using YAF.Core.Tasks;
+    using YAF.Types;
+    using YAF.Types.Constants;
+    using YAF.Types.Interfaces;
+    using YAF.Utilities;
+    using YAF.Utils;
 
-	#endregion
+    #endregion
 
-	/// <summary>
-	/// Administrative Page for the deleting of forum properties.
-	/// </summary>
-	public partial class deleteforum : AdminPage
-	{
-		#region Methods
+    /// <summary>
+    /// Administrative Page for the deleting of forum properties.
+    /// </summary>
+    public partial class deleteforum : AdminPage
+    {
+        #region Methods
 
-		/// <summary>
-		/// Get query string as int.
-		/// </summary>
-		/// <param name="name">
-		/// The name.
-		/// </param>
-		/// <returns>
-		/// The get query string as int.
-		/// </returns>
-		protected int? GetQueryStringAsInt([NotNull] string name)
-		{
-			int value;
+        /// <summary>
+        /// Get query string as int.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The get query string as int.
+        /// </returns>
+        protected int? GetQueryStringAsInt([NotNull] string name)
+        {
+            int value;
 
-			if (this.Request.QueryString.GetFirstOrDefault(name) != null &&
-					int.TryParse(this.Request.QueryString.GetFirstOrDefault(name), out value))
-			{
-				return value;
-			}
+            if (this.Request.QueryString.GetFirstOrDefault(name) != null
+                && int.TryParse(this.Request.QueryString.GetFirstOrDefault(name), out value))
+            {
+                return value;
+            }
 
-			return null;
-		}
+            return null;
+        }
 
-		/// <summary>
-		/// The on init.
-		/// </summary>
-		/// <param name="e">
-		/// The e.
-		/// </param>
-		protected override void OnInit([NotNull] EventArgs e)
-		{
-			this.MoveTopics.CheckedChanged += this.MoveTopics_CheckedChanged;
-			this.Delete.Click += this.Save_Click;
-			this.Cancel.Click += this.Cancel_Click;
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init"/> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs"/> object that contains the event data.</param>
+        protected override void OnInit([NotNull] EventArgs e)
+        {
+            this.MoveTopics.CheckedChanged += this.MoveTopics_CheckedChanged;
+            this.Delete.Click += this.Save_Click;
+            this.Cancel.Click += this.Cancel_Click;
 
-			base.OnInit(e);
-		}
+            base.OnInit(e);
+        }
 
-		/// <summary>
-		/// The page_ load.
-		/// </summary>
-		/// <param name="sender">
-		/// The sender.
-		/// </param>
-		/// <param name="e">
-		/// The e.
-		/// </param>
-		protected void Page_Load([NotNull] object sender, [NotNull] EventArgs e)
-		{
-			this.PageContext.PageElements.RegisterJQuery();
-			this.PageContext.PageElements.RegisterJsResourceInclude("blockUIJs", "js/jquery.blockUI.js");
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void Page_Load([NotNull] object sender, [NotNull] EventArgs e)
+        {
+            this.PageContext.PageElements.RegisterJQuery();
+            this.PageContext.PageElements.RegisterJsResourceInclude("blockUIJs", "js/jquery.blockUI.js");
 
-			if (this.IsPostBack)
-			{
-				return;
-			}
+            if (this.IsPostBack)
+            {
+                return;
+            }
 
-			this.LoadingImage.ImageUrl = YafForumInfo.GetURLToResource("images/loader.gif");
+            this.LoadingImage.ImageUrl = YafForumInfo.GetURLToResource("images/loader.gif");
 
-			this.PageLinks.AddLink(this.Get<YafBoardSettings>().Name, YafBuildLink.GetLink(ForumPages.forum));
-			this.PageLinks.AddLink(
-					this.GetText("ADMIN_ADMIN", "Administration"), YafBuildLink.GetLink(ForumPages.admin_admin));
+            this.PageLinks.AddLink(this.Get<YafBoardSettings>().Name, YafBuildLink.GetLink(ForumPages.forum));
+            this.PageLinks.AddLink(
+                this.GetText("ADMIN_ADMIN", "Administration"), YafBuildLink.GetLink(ForumPages.admin_admin));
 
-			this.PageLinks.AddLink(this.GetText("TEAM", "FORUMS"), YafBuildLink.GetLink(ForumPages.admin_forums));
-			this.PageLinks.AddLink(this.GetText("ADMIN_DELETEFORUM", "TITLE"), string.Empty);
+            this.PageLinks.AddLink(this.GetText("TEAM", "FORUMS"), YafBuildLink.GetLink(ForumPages.admin_forums));
+            this.PageLinks.AddLink(this.GetText("ADMIN_DELETEFORUM", "TITLE"), string.Empty);
 
-			this.Page.Header.Title = "{0} - {1} - {2}".FormatWith(
-					this.GetText("ADMIN_ADMIN", "Administration"),
-					this.GetText("TEAM", "FORUMS"),
-					this.GetText("ADMIN_DELETEFORUM", "TITLE"));
+            this.Page.Header.Title = "{0} - {1} - {2}".FormatWith(
+                this.GetText("ADMIN_ADMIN", "Administration"),
+                this.GetText("TEAM", "FORUMS"),
+                this.GetText("ADMIN_DELETEFORUM", "TITLE"));
 
-			this.Delete.Text = this.GetText("ADMIN_DELETEFORUM", "DELETE_FORUM");
-			this.Cancel.Text = this.GetText("CANCEL");
+            this.Delete.Text = this.GetText("ADMIN_DELETEFORUM", "DELETE_FORUM");
+            this.Cancel.Text = this.GetText("CANCEL");
 
-			this.Delete.Attributes["onclick"] =
-					"return (confirm('{0}') && confirm('{1}'));".FormatWith(
-							this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE"),
-							this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE_POSITIVE"));
+            this.Delete.Attributes["onclick"] =
+                "return (confirm('{0}') && confirm('{1}'));".FormatWith(
+                    this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE"),
+                    this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE_POSITIVE"));
 
-			this.BindData();
+            this.BindData();
 
-			var forumId = this.GetQueryStringAsInt("f");
+            var forumId = this.GetQueryStringAsInt("f");
 
-			using (DataTable dt = LegacyDb.forum_list(this.PageContext.PageBoardID, forumId.Value))
-			{
-				DataRow row = dt.Rows[0];
+            using (DataTable dt = LegacyDb.forum_list(this.PageContext.PageBoardID, forumId.Value))
+            {
+                DataRow row = dt.Rows[0];
 
-				this.ForumNameTitle.Text = (string)row["Name"];
+                this.ForumNameTitle.Text = (string)row["Name"];
 
-				// populate parent forums list with forums according to selected category
-				this.BindParentList();
-			}
-		}
+                // populate parent forums list with forums according to selected category
+                this.BindParentList();
+            }
+        }
 
-		/// <summary>
-		/// The update status timer_ tick.
-		/// </summary>
-		/// <param name="sender">
-		/// The sender.
-		/// </param>
-		/// <param name="e">
-		/// The e.
-		/// </param>
-		protected void UpdateStatusTimer_Tick([NotNull] object sender, [NotNull] EventArgs e)
-		{
-			IBackgroundTask task;
+        /// <summary>
+        /// The update status timer_ tick.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected void UpdateStatusTimer_Tick([NotNull] object sender, [NotNull] EventArgs e)
+        {
+            IBackgroundTask task;
 
-			// see if the migration is done....
-			if (this.Get<ITaskModuleManager>().TryGetTask(ForumDeleteTask.TaskName, out task) &&
-						task.IsRunning)
-			{
-				// continue...
-				return;
-			}
+            // see if the migration is done....
+            if (this.Get<ITaskModuleManager>().TryGetTask(ForumDeleteTask.TaskName, out task) && task.IsRunning)
+            {
+                // continue...
+                return;
+            }
 
-			this.UpdateStatusTimer.Enabled = false;
+            this.UpdateStatusTimer.Enabled = false;
 
-			// rebind...
-			this.BindData();
+            // rebind...
+            this.BindData();
 
-			// clear caches...
-			this.ClearCaches();
+            // clear caches...
+            this.ClearCaches();
 
-			YafBuildLink.Redirect(ForumPages.admin_forums);
-		}
+            YafBuildLink.Redirect(ForumPages.admin_forums);
+        }
 
-		/// <summary>
-		/// The bind data.
-		/// </summary>
-		private void BindData()
-		{
-			// Load forum's combo
-			this.BindParentList();
-		}
+        /// <summary>
+        /// Binds the data.
+        /// </summary>
+        private void BindData()
+        {
+            // Load forum's combo
+            this.BindParentList();
+        }
 
-		/// <summary>
-		/// The bind parent list.
-		/// </summary>
-		private void BindParentList()
-		{
-			this.ForumList.DataSource = LegacyDb.forum_listall(
-					this.PageContext.PageBoardID, this.PageContext.PageUserID);
+        /// <summary>
+        /// Binds the parent list.
+        /// </summary>
+        private void BindParentList()
+        {
+            this.ForumList.DataSource = LegacyDb.forum_listall(
+                this.PageContext.PageBoardID, this.PageContext.PageUserID);
 
-			this.ForumList.DataValueField = "ForumID";
-			this.ForumList.DataTextField = "Forum";
-			this.ForumList.DataBind();
-		}
+            this.ForumList.DataValueField = "ForumID";
+            this.ForumList.DataTextField = "Forum";
+            this.ForumList.DataBind();
+        }
 
-		/// <summary>
-		/// Handles the CheckedChanged event of the MoveTopics control.
-		/// </summary>
-		/// <param name="sender">The source of the event.</param>
-		/// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
-		private void MoveTopics_CheckedChanged(object sender, EventArgs e)
-		{
-			if (this.MoveTopics.Checked)
-			{
-				this.Delete.Attributes.Remove("onclick");
-			}
-			else
-			{
-				this.Delete.Attributes["onclick"] =
-						"return (confirm('{0}') && confirm('{1}'));".FormatWith(
-								this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE"),
-								this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE_POSITIVE"));
-			}
+        /// <summary>
+        /// Handles the CheckedChanged event of the MoveTopics control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void MoveTopics_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.MoveTopics.Checked)
+            {
+                this.Delete.Attributes.Remove("onclick");
+            }
+            else
+            {
+                this.Delete.Attributes["onclick"] =
+                    "return (confirm('{0}') && confirm('{1}'));".FormatWith(
+                        this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE"),
+                        this.GetText("ADMIN_FORUMS", "CONFIRM_DELETE_POSITIVE"));
+            }
 
-			this.ForumList.Enabled = this.MoveTopics.Checked;
-		}
+            this.ForumList.Enabled = this.MoveTopics.Checked;
+        }
 
-		/// <summary>
-		/// The cancel_ click.
-		/// </summary>
-		/// <param name="sender">
-		/// The sender.
-		/// </param>
-		/// <param name="e">
-		/// The e.
-		/// </param>
-		private void Cancel_Click([NotNull] object sender, [NotNull] EventArgs e)
-		{
-			YafBuildLink.Redirect(ForumPages.admin_forums);
-		}
+        /// <summary>
+        /// Cancel Deleting and Redirecting back to The Admin Forums Page.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void Cancel_Click([NotNull] object sender, [NotNull] EventArgs e)
+        {
+            YafBuildLink.Redirect(ForumPages.admin_forums);
+        }
 
-		/// <summary>
-		/// The clear caches.
-		/// </summary>
-		private void ClearCaches()
-		{
-			// clear moderatorss cache
-			this.Get<IDataCache>().Remove(Constants.Cache.ForumModerators);
+        /// <summary>
+        /// Clears the caches.
+        /// </summary>
+        private void ClearCaches()
+        {
+            // clear moderatorss cache
+            this.Get<IDataCache>().Remove(Constants.Cache.ForumModerators);
 
-			// clear category cache...
-			this.Get<IDataCache>().Remove(Constants.Cache.ForumCategory);
+            // clear category cache...
+            this.Get<IDataCache>().Remove(Constants.Cache.ForumCategory);
 
-			// clear active discussions cache..
-			this.Get<IDataCache>().Remove(Constants.Cache.ForumActiveDiscussions);
-		}
+            // clear active discussions cache..
+            this.Get<IDataCache>().Remove(Constants.Cache.ForumActiveDiscussions);
+        }
 
-		/// <summary>
-		/// Delete The Forum
-		/// </summary>
-		/// <param name="sender">
-		/// The sender.
-		/// </param>
-		/// <param name="e">
-		/// The e.
-		/// </param>
-		private void Save_Click([NotNull] object sender, [NotNull] EventArgs e)
-		{
-			if (this.MoveTopics.Checked)
-			{
-				// Simply Delete the Forum with all of its Content
-				var forumId = this.GetQueryStringAsInt("f");
+        /// <summary>
+        /// Delete The Forum
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void Save_Click([NotNull] object sender, [NotNull] EventArgs e)
+        {
+            var errorMessage = string.Empty;
 
-				// schedule...
-				ForumDeleteTask.Start(
-						this.PageContext.PageBoardID, forumId.Value, this.ForumList.SelectedValue.ToType<int>());
+            if (this.MoveTopics.Checked)
+            {
+                // Simply Delete the Forum with all of its Content
+                var forumId = this.GetQueryStringAsInt("f");
 
-				// enable timer...
-				this.UpdateStatusTimer.Enabled = true;
+                // schedule...
+                ForumDeleteTask.Start(
+                    this.PageContext.PageBoardID, forumId.Value, this.ForumList.SelectedValue.ToType<int>(), out errorMessage);
 
-				this.LocalizedLabel6.LocalizedTag = "DELETE_MOVE_TITLE";
+                // enable timer...
+                this.UpdateStatusTimer.Enabled = true;
 
-				// show blocking ui...
-				this.PageContext.PageElements.RegisterJsBlockStartup(
-						"BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
-			}
-			else
-			{
-				// Simply Delete the Forum with all of its Content
-				var forumId = this.GetQueryStringAsInt("f");
+                this.LocalizedLabel6.LocalizedTag = "DELETE_MOVE_TITLE";
 
-				// schedule...
-				ForumDeleteTask.Start(this.PageContext.PageBoardID, forumId.Value);
+                // show blocking ui...
+                this.PageContext.PageElements.RegisterJsBlockStartup(
+                    "BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
+            }
+            else
+            {
+                // Simply Delete the Forum with all of its Content
+                var forumId = this.GetQueryStringAsInt("f");
 
-				// enable timer...
-				this.UpdateStatusTimer.Enabled = true;
+                // schedule...
+                ForumDeleteTask.Start(this.PageContext.PageBoardID, forumId.Value, out errorMessage);
 
-				this.LocalizedLabel6.LocalizedTag = "DELETE_TITLE";
+                // enable timer...
+                this.UpdateStatusTimer.Enabled = true;
 
-				// show blocking ui...
-				this.PageContext.PageElements.RegisterJsBlockStartup(
-						"BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
-			}
+                this.LocalizedLabel6.LocalizedTag = "DELETE_TITLE";
 
-			// show blocking ui...
-			this.PageContext.PageElements.RegisterJsBlockStartup(
-					"BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
-		}
+                // show blocking ui...
+                this.PageContext.PageElements.RegisterJsBlockStartup(
+                    "BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
+            }
 
-		#endregion
-	}
+            // show blocking ui...
+            this.PageContext.PageElements.RegisterJsBlockStartup(
+                "BlockUIExecuteJs", JavaScriptBlocks.BlockUIExecuteJs("DeleteForumMessage"));
+
+            // TODO : Handle Error Message?!
+        }
+
+        #endregion
+    }
 }
