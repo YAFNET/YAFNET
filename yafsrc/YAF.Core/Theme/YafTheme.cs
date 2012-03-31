5349da629173bec7832312ada928da42d7a0f8f6
Watcher <Watcher@Watcher-One>
2012-03-31 12:25:32 +0200

    Default Value was not Correctly loaded when theme Item is not found

--- yafsrc/YAF.Core/Theme/YafTheme.cs
+++ yafsrc/YAF.Core/Theme/YafTheme.cs
@@ -20,286 +20,272 @@
 
 namespace YAF.Core
 {
-	#region Using
+    #region Using
 
-	using System;
-	using System.Collections.Generic;
-	using System.IO;
-	using System.Linq;
-	using System.Web;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
 
-	using YAF.Classes;
-	using YAF.Classes.Data;
-	using YAF.Types;
-	using YAF.Types.Constants;
-	using YAF.Types.Interfaces;
-	using YAF.Utils;
+    using YAF.Classes;
+    using YAF.Classes.Data;
+    using YAF.Types;
+    using YAF.Types.Constants;
+    using YAF.Types.Interfaces;
+    using YAF.Utils;
 
-	#endregion
+    #endregion
 
-	/// <summary>
-	/// The yaf theme.
-	/// </summary>
-	public class YafTheme : ITheme
-	{
-		#region Constants and Fields
+    /// <summary>
+    /// The yaf theme.
+    /// </summary>
+    public class YafTheme : ITheme
+    {
+        #region Constants and Fields
 
-		/// <summary>
-		///   The _theme file.
-		/// </summary>
-		private string _themeFile;
+        /// <summary>
+        ///   The _theme file.
+        /// </summary>
+        private string _themeFile;
 
-		/// <summary>
-		/// The _theme resources.
-		/// </summary>
-		private ThemeResources _themeResources;
+        /// <summary>
+        /// The _theme resources.
+        /// </summary>
+        private ThemeResources _themeResources;
 
-		#endregion
+        #endregion
 
-		#region Constructors and Destructors
+        #region Constructors and Destructors
 
-		/// <summary>
-		///   Initializes a new instance of the <see cref="YafTheme" /> class.
-		/// </summary>
-		public YafTheme()
-		{
-		}
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="YafTheme" /> class.
+        /// </summary>
+        public YafTheme()
+        {
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="YafTheme"/> class.
-		/// </summary>
-		/// <param name="newThemeFile">
-		/// The new theme file. 
-		/// </param>
-		public YafTheme([NotNull] string newThemeFile)
-		{
-			CodeContracts.ArgumentNotNull(newThemeFile, "newThemeFile");
+        /// <summary>
+        /// Initializes a new instance of the <see cref="YafTheme"/> class.
+        /// </summary>
+        /// <param name="newThemeFile">
+        /// The new theme file. 
+        /// </param>
+        public YafTheme([NotNull] string newThemeFile)
+        {
+            CodeContracts.ArgumentNotNull(newThemeFile, "newThemeFile");
 
-			this.ThemeFile = newThemeFile;
-		}
+            this.ThemeFile = newThemeFile;
+        }
 
-		#endregion
+        #endregion
 
-		#region Public Properties
+        #region Public Properties
 
-		/// <summary>
-		///   Gets or sets a value indicating whether LogMissingThemeItem.
-		/// </summary>
-		public bool LogMissingThemeItem { get; set; }
+        /// <summary>
+        ///   Gets or sets a value indicating whether LogMissingThemeItem.
+        /// </summary>
+        public bool LogMissingThemeItem { get; set; }
 
-		/// <summary>
-		///   Gets ThemeDir.
-		/// </summary>
-		public string ThemeDir
-		{
-			get
-			{
-				this.LoadThemeFile();
-				return "{0}{1}/{2}/".FormatWith(
-					YafForumInfo.ForumClientFileRoot, YafBoardFolders.Current.Themes, this._themeResources.dir);
-			}
-		}
+        /// <summary>
+        ///   Gets ThemeDir.
+        /// </summary>
+        public string ThemeDir
+        {
+            get
+            {
+                this.LoadThemeFile();
+                return "{0}{1}/{2}/".FormatWith(
+                    YafForumInfo.ForumClientFileRoot, YafBoardFolders.Current.Themes, this._themeResources.dir);
+            }
+        }
 
-		/// <summary>
-		///   Get or Set the current Theme File
-		/// </summary>
-		public string ThemeFile
-		{
-			get
-			{
-				return this._themeFile;
-			}
+        /// <summary>
+        ///   Gets or sets the current Theme File
+        /// </summary>
+        public string ThemeFile
+        {
+            get
+            {
+                return this._themeFile;
+            }
 
-			set
-			{
-				if (this._themeFile != value)
-				{
-					if (IsValidTheme(value))
-					{
-						this._themeFile = value;
-						this._themeResources = null;
-					}
-				}
-			}
-		}
+            set
+            {
+                if (this._themeFile == value)
+                {
+                    return;
+                }
 
-		#endregion
+                if (!IsValidTheme(value))
+                {
+                    return;
+                }
 
-		#region Public Methods and Operators
+                this._themeFile = value;
+                this._themeResources = null;
+            }
+        }
 
-		/// <summary>
-		/// Basic testing of the theme's validity...
-		/// </summary>
-		/// <param name="themeFile">
-		/// </param>
-		/// <returns>
-		/// The is valid theme. 
-		/// </returns>
-		public static bool IsValidTheme([NotNull] string themeFile)
-		{
-			CodeContracts.ArgumentNotNull(themeFile, "themeFile");
+        #endregion
 
-			if (themeFile.IsNotSet())
-			{
-				return false;
-			}
+        #region Public Methods and Operators
 
-			themeFile = themeFile.Trim().ToLower();
+        /// <summary>
+        /// Basic testing of the theme's validity...
+        /// </summary>
+        /// <param name="themeFile">The theme file.</param>
+        /// <returns>
+        /// The is valid theme.
+        /// </returns>
+        public static bool IsValidTheme([NotNull] string themeFile)
+        {
+            CodeContracts.ArgumentNotNull(themeFile, "themeFile");
 
-			if (themeFile.Length == 0)
-			{
-				return false;
-			}
+            if (themeFile.IsNotSet())
+            {
+                return false;
+            }
 
-			if (!themeFile.EndsWith(".xml"))
-			{
-				return false;
-			}
+            themeFile = themeFile.Trim().ToLower();
 
-			return File.Exists(GetMappedThemeFile(themeFile));
-		}
+            return themeFile.Length != 0 && (themeFile.EndsWith(".xml") && File.Exists(GetMappedThemeFile(themeFile)));
+        }
 
-		/// <summary>
-		/// Gets full path to the given theme file.
-		/// </summary>
-		/// <param name="filename">
-		/// Short name of theme file. 
-		/// </param>
-		/// <returns>
-		/// The build theme path. 
-		/// </returns>
-		public string BuildThemePath([NotNull] string filename)
-		{
-			CodeContracts.ArgumentNotNull(filename, "filename");
+        /// <summary>
+        /// Gets full path to the given theme file.
+        /// </summary>
+        /// <param name="filename">
+        /// Short name of theme file. 
+        /// </param>
+        /// <returns>
+        /// The build theme path. 
+        /// </returns>
+        public string BuildThemePath([NotNull] string filename)
+        {
+            CodeContracts.ArgumentNotNull(filename, "filename");
 
-			return this.ThemeDir + filename;
-		}
+            return this.ThemeDir + filename;
+        }
 
-		/// <summary>
-		/// The get item.
-		/// </summary>
-		/// <param name="page">
-		/// The page. 
-		/// </param>
-		/// <param name="tag">
-		/// The tag. 
-		/// </param>
-		/// <returns>
-		/// The get item. 
-		/// </returns>
-		public string GetItem([NotNull] string page, [NotNull] string tag)
-		{
-			CodeContracts.ArgumentNotNull(page, "page");
-			CodeContracts.ArgumentNotNull(tag, "tag");
+        /// <summary>
+        /// Get the Item.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="tag">The tag.</param>
+        /// <returns>
+        /// The item.
+        /// </returns>
+        public string GetItem([NotNull] string page, [NotNull] string tag)
+        {
+            CodeContracts.ArgumentNotNull(page, "page");
+            CodeContracts.ArgumentNotNull(tag, "tag");
 
-			return this.GetItem(page, tag, "[{0}.{1}]".FormatWith(page.ToUpper(), tag.ToUpper()));
-		}
+            return this.GetItem(page, tag, "[{0}.{1}]".FormatWith(page.ToUpper(), tag.ToUpper()));
+        }
 
-		/// <summary>
-		/// The get item.
-		/// </summary>
-		/// <param name="page">
-		/// The page. 
-		/// </param>
-		/// <param name="tag">
-		/// The tag. 
-		/// </param>
-		/// <param name="defaultValue">
-		/// The default value. 
-		/// </param>
-		/// <returns>
-		/// The get item. 
-		/// </returns>
-		public string GetItem([NotNull] string page, [NotNull] string tag, [CanBeNull] string defaultValue)
-		{
-			CodeContracts.ArgumentNotNull(page, "page");
-			CodeContracts.ArgumentNotNull(tag, "tag");
+        /// <summary>
+        /// Get the Item.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="tag">The tag.</param>
+        /// <param name="defaultValue">The default value.</param>
+        /// <returns>
+        /// The item.
+        /// </returns>
+        public string GetItem([NotNull] string page, [NotNull] string tag, [CanBeNull] string defaultValue)
+        {
+            CodeContracts.ArgumentNotNull(page, "page");
+            CodeContracts.ArgumentNotNull(tag, "tag");
 
-			string item = string.Empty;
+            string item = string.Empty;
 
-			this.LoadThemeFile();
+            this.LoadThemeFile();
 
-			if (this._themeResources != null)
-			{
-				string langCode = YafContext.Current.Get<ILocalization>().LanguageCode.ToUpper();
-				var selectedPage = this._themeResources.page.FirstOrDefault(x => x.name == page.ToUpper());
+            if (this._themeResources != null)
+            {
+                string langCode = YafContext.Current.Get<ILocalization>().LanguageCode.ToUpper();
+                var selectedPage = this._themeResources.page.FirstOrDefault(x => x.name == page.ToUpper());
 
-				if (selectedPage != null)
-				{
-					var selectedItem =
-						selectedPage.Resource.FirstOrDefault(
-							x =>
-							x.tag == tag.ToUpper() && (x.language.IsNotSet() || (x.language.IsSet() && x.language == langCode.ToUpper())));
+                if (selectedPage != null)
+                {
+                    var selectedItem =
+                        selectedPage.Resource.FirstOrDefault(
+                            x =>
+                            x.tag == tag.ToUpper()
+                            && (x.language.IsNotSet() || (x.language.IsSet() && x.language == langCode.ToUpper())));
 
-					if (selectedItem == null)
-					{
-						if (this.LogMissingThemeItem)
-						{
-							LegacyDb.eventlog_create(
-								YafContext.Current.PageUserID, 
-								page.ToLower() + ".ascx", 
-								"Missing Theme Item: {0}.{1}".FormatWith(page.ToUpper(), tag.ToUpper()), 
-								EventLogTypes.Error);
-						}
+                    if (selectedItem == null)
+                    {
+                        if (this.LogMissingThemeItem)
+                        {
+                            LegacyDb.eventlog_create(
+                                YafContext.Current.PageUserID,
+                                "{0}.ascx".FormatWith(page.ToLower()),
+                                "Missing Theme Item: {0}.{1}".FormatWith(page.ToUpper(), tag.ToUpper()),
+                                EventLogTypes.Error);
+                        }
 
-						return defaultValue;
-					}
+                        return defaultValue;
+                    }
 
-					if (selectedItem.Value.IsSet())
-					{
-						item = selectedItem.Value.Replace(
-							"~",
-							"{0}{1}/{2}".FormatWith(
-								YafForumInfo.ForumServerFileRoot, YafBoardFolders.Current.Themes, this._themeResources.dir));
-					}
-				}
-			}
+                    return selectedItem.Value.IsSet()
+                               ? selectedItem.Value.Replace(
+                                   "~",
+                                   "{0}{1}/{2}".FormatWith(
+                                       YafForumInfo.ForumServerFileRoot,
+                                       YafBoardFolders.Current.Themes,
+                                       this._themeResources.dir))
+                               : defaultValue;
+                }
 
-			return item;
-		}
+                return defaultValue;
+            }
 
-		#endregion
+            return item;
+        }
 
-		#region Methods
+        #endregion
 
-		/// <summary>
-		/// The get mapped theme file.
-		/// </summary>
-		/// <param name="themeFile">
-		/// The theme file.
-		/// </param>
-		/// <returns>
-		/// The get mapped theme file.
-		/// </returns>
-		private static string GetMappedThemeFile([NotNull] string themeFile)
-		{
-			CodeContracts.ArgumentNotNull(themeFile, "themeFile");
+        #region Methods
 
-			return
-				HttpContext.Current.Server.MapPath(
-					string.Concat(YafForumInfo.ForumServerFileRoot, YafBoardFolders.Current.Themes, "/", themeFile.Trim()));
-		}
+        /// <summary>
+        /// Gets the mapped theme file.
+        /// </summary>
+        /// <param name="themeFile">The theme file.</param>
+        /// <returns>
+        /// The get mapped theme file.
+        /// </returns>
+        private static string GetMappedThemeFile([NotNull] string themeFile)
+        {
+            CodeContracts.ArgumentNotNull(themeFile, "themeFile");
 
-		/// <summary>
-		/// The load theme file.
-		/// </summary>
-		private void LoadThemeFile()
-		{
-			if (this.ThemeFile != null && this._themeResources == null)
-			{
-				this._themeResources = new LoadSerializedXmlFile<ThemeResources>().FromFile(
-					GetMappedThemeFile(this.ThemeFile), 
-					"THEMEFILE{0}".FormatWith(this.ThemeFile), 
-					(r) =>
-						{
-							// transform the page and tag name ToUpper...
-							r.page.ForEach(p => p.name = p.name.ToUpper());
-							r.page.Where(p => p.Resource == null).ForEach(p => p.Resource = new ResourcesPageResource[0]);
-							r.page.ForEach(p => p.Resource.ForEach(i => i.tag = i.tag.ToUpper()));
-						});
-			}
-		}
+            return
+                HttpContext.Current.Server.MapPath(
+                    string.Concat(
+                        YafForumInfo.ForumServerFileRoot, YafBoardFolders.Current.Themes, "/", themeFile.Trim()));
+        }
 
-		#endregion
-	}
+        /// <summary>
+        /// Loads the theme file.
+        /// </summary>
+        private void LoadThemeFile()
+        {
+            if (this.ThemeFile != null && this._themeResources == null)
+            {
+                this._themeResources =
+                    new LoadSerializedXmlFile<ThemeResources>().FromFile(
+                        GetMappedThemeFile(this.ThemeFile),
+                        "THEMEFILE{0}".FormatWith(this.ThemeFile),
+                        r =>
+                            {
+                                // transform the page and tag name ToUpper...
+                                r.page.ForEach(p => p.name = p.name.ToUpper());
+                                r.page.Where(p => p.Resource == null).ForEach(
+                                    p => p.Resource = new ResourcesPageResource[0]);
+                                r.page.ForEach(p => p.Resource.ForEach(i => i.tag = i.tag.ToUpper()));
+                            });
+            }
+        }
+
+        #endregion
+    }
 }
