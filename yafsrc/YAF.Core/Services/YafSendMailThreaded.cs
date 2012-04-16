564d3b46f8cdc3e1f82bd125a6f0c8a5df06474b
Watcher <Watcher@Watcher-One>
2012-04-16 14:58:32 +0200

    Enabled Logging for Error Messages in Release Mode for the Threaded Send Mail Function

--- yafsrc/YAF.Core/Services/YafSendMailThreaded.cs
+++ yafsrc/YAF.Core/Services/YafSendMailThreaded.cs
@@ -19,231 +19,240 @@
 
 namespace YAF.Core.Services
 {
-	#region Using
+    #region Using
 
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Net.Mail;
-	using System.Threading;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Mail;
+    using System.Threading;
 
-	using YAF.Classes.Data;
-	using YAF.Types;
-	using YAF.Types.Interfaces;
-	using YAF.Types.Objects;
-	using YAF.Utils;
+    using YAF.Classes.Data;
+    using YAF.Types;
+    using YAF.Types.Interfaces;
+    using YAF.Types.Objects;
+    using YAF.Utils;
 
-	#endregion
+    #endregion
 
-	/// <summary>
-	/// Separate class since SendThreaded isn't needed functionality for any instance except the <see cref="HttpModule"/> instance.
-	/// </summary>
-	public class YafSendMailThreaded : ISendMailThreaded
-	{
-		#region Constants and Fields
+    /// <summary>
+    /// Separate class since SendThreaded isn't needed functionality for any instance except the <see cref="HttpModule"/> instance.
+    /// </summary>
+    public class YafSendMailThreaded : ISendMailThreaded
+    {
+        #region Constants and Fields
 
-		/// <summary>
-		///   The _unique id.
-		/// </summary>
-		protected int _uniqueId;
+        /// <summary>
+        ///   The _unique id.
+        /// </summary>
+        protected int _uniqueId;
 
-		#endregion
+        #endregion
 
-		#region Constructors and Destructors
+        #region Constructors and Destructors
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="YafSendMailThreaded"/> class.
-		/// </summary>
-		/// <param name="sendMail">
-		/// The send mail. 
-		/// </param>
-		/// <param name="logger">
-		/// The logger.
-		/// </param>
-		public YafSendMailThreaded([NotNull] ISendMail sendMail, ILogger logger)
-		{
-			CodeContracts.ArgumentNotNull(sendMail, "sendMail");
+        /// <summary>
+        /// Initializes a new instance of the <see cref="YafSendMailThreaded"/> class.
+        /// </summary>
+        /// <param name="sendMail">
+        /// The send mail. 
+        /// </param>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        public YafSendMailThreaded([NotNull] ISendMail sendMail, ILogger logger)
+        {
+            CodeContracts.ArgumentNotNull(sendMail, "sendMail");
 
-			this.SendMail = sendMail;
-			this.Logger = logger;
-			var rand = new Random();
-			this._uniqueId = rand.Next();
-		}
+            this.SendMail = sendMail;
+            this.Logger = logger;
+            var rand = new Random();
+            this._uniqueId = rand.Next();
+        }
 
-		#endregion
+        #endregion
 
-		#region Public Properties
+        #region Public Properties
 
-		/// <summary>
-		/// Gets or sets Logger.
-		/// </summary>
-		public ILogger Logger { get; set; }
+        /// <summary>
+        /// Gets or sets Logger.
+        /// </summary>
+        public ILogger Logger { get; set; }
 
-		/// <summary>
-		///   Gets or sets SendMail.
-		/// </summary>
-		public ISendMail SendMail { get; set; }
+        /// <summary>
+        ///   Gets or sets SendMail.
+        /// </summary>
+        public ISendMail SendMail { get; set; }
 
-		#endregion
+        #endregion
 
-		#region Public Methods
+        #region Public Methods
 
-		/// <summary>
-		/// The send threaded.
-		/// </summary>
-		public void SendThreaded()
-		{
-			var mailMessages = new Dictionary<MailMessage, TypedMailList>();
+        /// <summary>
+        /// The send threaded.
+        /// </summary>
+        public void SendThreaded()
+        {
+            var mailMessages = new Dictionary<MailMessage, TypedMailList>();
 
-			try
-			{
-				IList<TypedMailList> mailList = this.GetMailListSafe();
+            try
+            {
+                IEnumerable<TypedMailList> mailList = this.GetMailListSafe();
 
-				this.ConstructMessageList(mailMessages, mailList);
+                this.ConstructMessageList(mailMessages, mailList);
 
-				this.SendMail.SendAllIsolated(
-					mailMessages.Select(x => x.Key), 
-					(message, ex) =>
-						{
-							if (ex is FormatException)
-							{
-								// email address is no good -- delete this email...
-								this.Logger.Debug("Invalid Email Address: {0}".FormatWith(ex.ToString()));
+                this.SendMail.SendAllIsolated(
+                    mailMessages.Select(x => x.Key),
+                    (message, ex) =>
+                        {
+                            if (ex is FormatException)
+                            {
 #if (DEBUG)
-								this.Logger.Warn("Invalid Email Address: {0}".FormatWith(ex.ToString()), ex.ToString());
+                                // email address is no good -- delete this email...
+                                this.Logger.Debug("Invalid Email Address: {0}".FormatWith(ex.ToString()), ex.ToString());
+#else
+                                // email address is no good -- delete this email...
+                                this.Logger.Warn("Invalid Email Address: {0}".FormatWith(ex.ToString()));
 #endif
-							}
-							else if (ex is SmtpException)
-							{
+                            }
+                            else if (ex is SmtpException)
+                            {
 #if (DEBUG)
-								this.Logger.Warn("SendMailThread SmtpException", ex.ToString());
-#endif
-								this.Logger.Debug("Send Exception: {0}".FormatWith(ex.ToString()));
+                                this.Logger.Debug("SendMailThread SmtpException", ex.ToString());
+#else
+                                this.Logger.Warn("Send Exception: {0}".FormatWith(ex.ToString()));
 
-								if (mailMessages.ContainsKey(message) && mailMessages[message].SendTries < 2)
-								{
-									// remove from the collection so it doesn't get deleted...
-									mailMessages.Remove(message);
-								}
-								else
-								{
-									this.Logger.Warn("SendMailThread Failed for the 2nd time:", ex.ToString());
-								}
-							}
-							else
-							{
-								// general exception...
-								this.Logger.Debug("Exception Thrown in SendMail Thread: " + ex.ToString());
+#endif
+                                if (mailMessages.ContainsKey(message) && mailMessages[message].SendTries < 2)
+                                {
+                                    // remove from the collection so it doesn't get deleted...
+                                    mailMessages.Remove(message);
+                                }
+                                else
+                                {
+                                    this.Logger.Warn("SendMailThread Failed for the 2nd time:", ex.ToString());
+                                }
+                            }
+                            else
+                            {
 #if (DEBUG)
-								this.Logger.Warn("SendMailThread General Exception", ex.ToString());
+                                 // general exception...
+                                this.Logger.Debug("SendMailThread General Exception", ex.ToString());
+#else
+                                // general exception...
+                                this.Logger.Warn("Exception Thrown in SendMail Thread: {0}".FormatWith(ex.ToString()));
 #endif
-							}
-						});
+                            }
+                        });
 
-				foreach (var message in mailMessages.Values)
-				{
-					// all is well, delete this message...
-					this.Logger.Debug("Deleting email to {0} (ID: {1})".FormatWith(message.ToUser, message.MailID));
-					LegacyDb.mail_delete(message.MailID);
-				}
-			}
-			catch (Exception ex)
-			{
-				// general exception...
-				this.Logger.Debug("Exception Thrown in SendMail Thread: " + ex);
+                foreach (var message in mailMessages.Values)
+                {
+                    // all is well, delete this message...
+                    this.Logger.Debug("Deleting email to {0} (ID: {1})".FormatWith(message.ToUser, message.MailID));
+                    LegacyDb.mail_delete(message.MailID);
+                }
+            }
+            catch (Exception ex)
+            {
 #if (DEBUG)
-				this.Logger.Warn("SendMailThread General Exception", ex.ToString());
+                // general exception...
+                this.Logger.Debug("SendMailThread General Exception", ex.ToString());
+#else
+                // general exception...
+                this.Logger.Warn("Exception Thrown in SendMail Thread: {0}".FormatWith(ex));
 #endif
-			}
-			finally
-			{
-				if (mailMessages.Any())
-				{
-					// dispose of all mail messages
-					mailMessages.Where(x => x.Key != null).Select(m => m.Key).ToList().ForEach(m => m.Dispose());
-				}
-			}
+            }
+            finally
+            {
+                if (mailMessages.Any())
+                {
+                    // dispose of all mail messages
+                    mailMessages.Where(x => x.Key != null).Select(m => m.Key).ToList().ForEach(m => m.Dispose());
+                }
+            }
 
-			this.Logger.Debug("SendMailThread exiting");
-		}
+            this.Logger.Debug("SendMailThread exiting");
+        }
 
-		#endregion
+        #endregion
 
-		#region Methods
+        #region Methods
 
-		/// <summary>
-		/// The construct message list.
-		/// </summary>
-		/// <param name="mailMessages">
-		/// The mail messages.
-		/// </param>
-		/// <param name="mailList">
-		/// The mail list.
-		/// </param>
-		private void ConstructMessageList(IDictionary<MailMessage, TypedMailList> mailMessages, IEnumerable<TypedMailList> mailList)
-		{
-			// construct mail message list...
-			foreach (var mail in mailList.Where(x => x.MailID.HasValue))
-			{
-				// Build a MailMessage
-				if (mail.FromUser.IsNotSet() || mail.ToUser.IsNotSet())
-				{
-					LegacyDb.mail_delete(mail.MailID.Value);
-					continue;
-				}
+        /// <summary>
+        /// The construct message list.
+        /// </summary>
+        /// <param name="mailMessages">
+        /// The mail messages.
+        /// </param>
+        /// <param name="mailList">
+        /// The mail list.
+        /// </param>
+        private void ConstructMessageList(
+            IDictionary<MailMessage, TypedMailList> mailMessages, IEnumerable<TypedMailList> mailList)
+        {
+            // construct mail message list...
+            foreach (var mail in mailList.Where(x => x.MailID.HasValue))
+            {
+                // Build a MailMessage
+                if (mail.FromUser.IsNotSet() || mail.ToUser.IsNotSet())
+                {
+                    LegacyDb.mail_delete(mail.MailID.Value);
+                    continue;
+                }
 
-				try
-				{
-					MailAddress toEmailAddress = mail.ToUserName.IsSet()
-					                             	? new MailAddress(mail.ToUser, mail.ToUserName)
-					                             	: new MailAddress(mail.ToUser);
-					MailAddress fromEmailAddress = mail.FromUserName.IsSet()
-					                               	? new MailAddress(mail.FromUser, mail.FromUserName)
-					                               	: new MailAddress(mail.FromUser);
+                try
+                {
+                    MailAddress toEmailAddress = mail.ToUserName.IsSet()
+                                                     ? new MailAddress(mail.ToUser, mail.ToUserName)
+                                                     : new MailAddress(mail.ToUser);
+                    MailAddress fromEmailAddress = mail.FromUserName.IsSet()
+                                                       ? new MailAddress(mail.FromUser, mail.FromUserName)
+                                                       : new MailAddress(mail.FromUser);
 
-					var newMessage = new MailMessage();
-					mailMessages.Add(newMessage, mail);
-					newMessage.Populate(fromEmailAddress, toEmailAddress, mail.Subject, mail.Body, mail.BodyHtml);
-				}
-				catch (FormatException ex)
-				{
-					// incorrect email format -- delete this message immediately
-					LegacyDb.mail_delete(mail.MailID);
-
-					this.Logger.Debug("Invalid Email Address: {0}".FormatWith(ex.ToString()));
+                    var newMessage = new MailMessage();
+                    mailMessages.Add(newMessage, mail);
+                    newMessage.Populate(fromEmailAddress, toEmailAddress, mail.Subject, mail.Body, mail.BodyHtml);
+                }
+                catch (FormatException ex)
+                {
+                    // incorrect email format -- delete this message immediately
+                    LegacyDb.mail_delete(mail.MailID);
 #if (DEBUG)
-					this.Logger.Warn("Invalid Email Address: {0}".FormatWith(ex.ToString()), ex.ToString());
+                    this.Logger.Debug("Invalid Email Address: {0}".FormatWith(ex.ToString()), ex.ToString());
+#else
+                    this.Logger.Warn("Invalid Email Address: {0}".FormatWith(ex.ToString()));
 #endif
-				}
-			}
-		}
+                }
+            }
+        }
 
-		/// <summary>
-		/// The get mail list safe.
-		/// </summary>
-		/// <returns>
-		/// </returns>
-		private IList<TypedMailList> GetMailListSafe()
-		{
-			List<TypedMailList> mailList;
+        /// <summary>
+        /// Gets the mail list safe.
+        /// </summary>
+        /// <returns>
+        /// The get mail list safe.
+        /// </returns>
+        private IEnumerable<TypedMailList> GetMailListSafe()
+        {
+            List<TypedMailList> mailList;
 
-			try
-			{
-				this.Logger.Debug("Retrieving queued mail...");
-				Thread.BeginCriticalRegion();
+            try
+            {
+                this.Logger.Debug("Retrieving queued mail...");
+                Thread.BeginCriticalRegion();
 
-				mailList = LegacyDb.MailList(this._uniqueId).ToList();
+                mailList = LegacyDb.MailList(this._uniqueId).ToList();
 
-				this.Logger.Debug("Got {0} Messages...".FormatWith(mailList.Count()));
-			}
-			finally
-			{
-				Thread.EndCriticalRegion();
-			}
+                this.Logger.Debug("Got {0} Messages...".FormatWith(mailList.Count()));
+            }
+            finally
+            {
+                Thread.EndCriticalRegion();
+            }
 
-			return mailList;
-		}
+            return mailList;
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
