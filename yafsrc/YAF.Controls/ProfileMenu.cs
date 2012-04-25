d385e2157978a3d7949d3cfbc597022ee7ab3149
Watcher <Watcher@Watcher-One>
2012-04-25 13:56:47 +0200

    Added Tooltip and Css Classes to the Profile Menu + Icons on my themes

--- yafsrc/YAF.Controls/ProfileMenu.cs
+++ yafsrc/YAF.Controls/ProfileMenu.cs
@@ -19,10 +19,11 @@
 
 namespace YAF.Controls
 {
-  #region Using
+    #region Using
 
     using System.Text;
     using System.Web.UI;
+
     using YAF.Classes;
     using YAF.Core;
     using YAF.Types;
@@ -30,122 +31,143 @@ namespace YAF.Controls
     using YAF.Types.Interfaces;
     using YAF.Utils;
 
-  #endregion
-
-    /// <summary>
-  /// Summary description for ForumUsers.
-  /// </summary>
-  public class ProfileMenu : BaseControl
-  {
-    #region Methods
-
-    /// <summary>
-    /// The render.
-    /// </summary>
-    /// <param name="writer">
-    /// The writer.
-    /// </param>
-    protected override void Render([NotNull] HtmlTextWriter writer)
-    {
-      var html = new StringBuilder(2000);
-
-      html.Append(@"<table cellspacing=""0"" cellpadding=""0"" class=""content"" id=""yafprofilemenu"">");
-
-      if (this.Get<YafBoardSettings>().AllowPrivateMessages)
-      {
-        html.AppendFormat(@"<tr class=""header2""><td>{0}</td></tr>", this.GetText("MESSENGER"));
-        html.AppendFormat(@"<tr><td class=""post""><ul id=""yafprofilemessenger"">");
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_pm, "v=in"), 
-          this.GetText("INBOX"));
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_pm, "v=out"), 
-          this.GetText("SENTITEMS"));
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_pm, "v=arch"), 
-          this.GetText("ARCHIVE"));
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.pmessage), 
-          this.GetText("NEW_MESSAGE"));
-        html.AppendFormat(@"</ul></td></tr>");
-      }
-
-      html.AppendFormat(
-        @"<tr class=""header2""><td>{0}</td></tr>", this.GetText("PERSONAL_PROFILE"));
-      html.AppendFormat(@"<tr><td class=""post""><ul id=""yafprofilepersonal"">");
-      html.AppendFormat(
-        @"<li><a href=""{0}"">{1}</a></li>", 
-        YafBuildLink.GetLink(ForumPages.profile, "u={0}", PageContext.PageUserID), 
-        this.GetText("VIEW_PROFILE"));
-      html.AppendFormat(
-        @"<li><a href=""{0}"">{1}</a></li>", 
-        YafBuildLink.GetLink(ForumPages.cp_editprofile), 
-        this.GetText("EDIT_PROFILE"));
-      if (!this.PageContext.IsGuest && this.Get<YafBoardSettings>().EnableThanksMod)
-      {
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.viewthanks, "u={0}", PageContext.PageUserID), 
-          this.GetText("ViewTHANKS", "TITLE"));
-      }
-
-      if (!this.PageContext.IsGuest && this.Get<YafBoardSettings>().EnableBuddyList & this.PageContext.UserHasBuddies)
-      {
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_editbuddies), 
-          this.GetText("EDIT_BUDDIES"));
-      }
-
-      if (!this.PageContext.IsGuest && (this.Get<YafBoardSettings>().EnableAlbum || (this.PageContext.NumAlbums > 0)))
-      {
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.albums, "u={0}", this.PageContext.PageUserID), 
-          this.GetText("EDIT_ALBUMS"));
-     }
-
-      if (this.Get<YafBoardSettings>().AvatarRemote || 
-          this.Get<YafBoardSettings>().AvatarUpload || 
-          this.Get<YafBoardSettings>().AvatarGallery || 
-          this.Get<YafBoardSettings>().AvatarGravatar)
-      {
-          html.AppendFormat(
-              @"<li><a href=""{0}"">{1}</a></li>",
-              YafBuildLink.GetLink(ForumPages.cp_editavatar),
-              this.GetText("EDIT_AVATAR"));
-      }
-
-        if (this.Get<YafBoardSettings>().AllowSignatures)
-      {
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_signature), 
-          this.GetText("SIGNATURE"));
-      }
-
-      html.AppendFormat(
-        @"<li><a href=""{0}"">{1}</a></li>", 
-        YafBuildLink.GetLink(ForumPages.cp_subscriptions), 
-        this.GetText("SUBSCRIPTIONS"));
-      if (this.Get<YafBoardSettings>().AllowPasswordChange)
-      {
-        html.AppendFormat(
-          @"<li><a href=""{0}"">{1}</a></li>", 
-          YafBuildLink.GetLink(ForumPages.cp_changepassword), 
-          this.GetText("CHANGE_PASSWORD"));
-      }
-
-      html.AppendFormat(@"</ul></td></tr>");
-      html.Append(@"</table>");
-
-      writer.Write(html.ToString());
-    }
-
     #endregion
-  }
+
+    /// <summary>
+    /// User Profile Menu in the User Control Panel
+    /// </summary>
+    public class ProfileMenu : BaseControl
+    {
+        #region Methods
+
+        /// <summary>
+        /// Render the Profile Menu
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter"/> object that receives the server control content.</param>
+        protected override void Render([NotNull] HtmlTextWriter writer)
+        {
+            var html = new StringBuilder(2000);
+
+            html.Append(@"<table cellspacing=""0"" cellpadding=""0"" class=""content"" id=""yafprofilemenu"">");
+
+            // Render Mailbox Items
+            if (this.Get<YafBoardSettings>().AllowPrivateMessages)
+            {
+                html.AppendFormat(@"<tr class=""header2""><td>{0}</td></tr>", this.GetText("MESSENGER"));
+
+                html.AppendFormat(@"<tr><td class=""post""><ul id=""yafprofilemessenger"">");
+
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{3}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_pm, "v=in"),
+                    this.GetText("INBOX"),
+                    ForumPages.cp_pm,
+                    this.GetText("TOOLBAR", "INBOX_TITLE"));
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_pm, "v=out"),
+                    this.GetText("SENTITEMS"),
+                    ForumPages.cp_pm);
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_pm, "v=arch"),
+                    this.GetText("ARCHIVE"), 
+                    ForumPages.cp_pm);
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.pmessage),
+                    this.GetText("NEW_MESSAGE"),
+                    ForumPages.pmessage);
+
+                html.AppendFormat(@"</ul></td></tr>");
+            }
+
+            // Render Personal Profile Items
+            html.AppendFormat(@"<tr class=""header2""><td>{0}</td></tr>", this.GetText("PERSONAL_PROFILE"));
+           
+            html.AppendFormat(@"<tr><td class=""post""><ul id=""yafprofilepersonal"">");
+            
+            html.AppendFormat(
+                @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                YafBuildLink.GetLink(ForumPages.profile, "u={0}", PageContext.PageUserID),
+                this.GetText("VIEW_PROFILE"),
+                    ForumPages.profile);
+            html.AppendFormat(
+                @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                YafBuildLink.GetLink(ForumPages.cp_editprofile),
+                this.GetText("EDIT_PROFILE"),
+                    ForumPages.cp_editprofile);
+
+            if (!this.PageContext.IsGuest && this.Get<YafBoardSettings>().EnableThanksMod)
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.viewthanks, "u={0}", PageContext.PageUserID),
+                    this.GetText("ViewTHANKS", "TITLE"),
+                    ForumPages.viewthanks);
+            }
+
+            if (!this.PageContext.IsGuest
+                && this.Get<YafBoardSettings>().EnableBuddyList & this.PageContext.UserHasBuddies)
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{3}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_editbuddies),
+                    this.GetText("EDIT_BUDDIES"),
+                    ForumPages.cp_editbuddies,
+                    this.GetText("TOOLBAR", "BUDDIES_TITLE"));
+            }
+
+            if (!this.PageContext.IsGuest
+                && (this.Get<YafBoardSettings>().EnableAlbum || (this.PageContext.NumAlbums > 0)))
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.albums, "u={0}", this.PageContext.PageUserID),
+                    this.GetText("EDIT_ALBUMS"),
+                    ForumPages.albums);
+            }
+
+            if (this.Get<YafBoardSettings>().AvatarRemote || this.Get<YafBoardSettings>().AvatarUpload
+                || this.Get<YafBoardSettings>().AvatarGallery || this.Get<YafBoardSettings>().AvatarGravatar)
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_editavatar),
+                    this.GetText("EDIT_AVATAR"),
+                    ForumPages.cp_editavatar);
+            }
+
+            if (this.Get<YafBoardSettings>().AllowSignatures)
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_signature),
+                    this.GetText("SIGNATURE"),
+                    ForumPages.cp_signature);
+            }
+
+            html.AppendFormat(
+                @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                YafBuildLink.GetLink(ForumPages.cp_subscriptions),
+                this.GetText("SUBSCRIPTIONS"),
+                    ForumPages.cp_subscriptions);
+
+            if (this.Get<YafBoardSettings>().AllowPasswordChange)
+            {
+                html.AppendFormat(
+                    @"<li class=""yafprofilemenu_{2}""><a href=""{0}"" title=""{1}"">{1}</a></li>",
+                    YafBuildLink.GetLink(ForumPages.cp_changepassword),
+                    this.GetText("CHANGE_PASSWORD"),
+                    ForumPages.cp_changepassword);
+            }
+
+            html.AppendFormat(@"</ul></td></tr>");
+            html.Append(@"</table>");
+
+            writer.Write(html.ToString());
+        }
+
+        #endregion
+    }
 }
