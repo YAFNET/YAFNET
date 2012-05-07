a89a4c7e4b133fb21843e97c17ccaef8c9ddd46e
MicScoTho <devnull@localhost>
2012-05-06 23:01:12 -0700

    MostActiveUser.cs: Normalize line endings and indentation

--- yafsrc/YAF.Controls/Statistics/MostActiveUser.cs
+++ yafsrc/YAF.Controls/Statistics/MostActiveUser.cs
@@ -19,152 +19,154 @@
 
 namespace YAF.Controls.Statistics
 {
-  #region Using
+    #region Using
 
-  using System;
-  using System.Data;
-  using System.Text;
-  using System.Web.UI;
+    using System;
+    using System.Data;
+    using System.Text;
+    using System.Web.UI;
 
-  using YAF.Core; using YAF.Types.Interfaces; using YAF.Types.Constants;
-  using YAF.Core.Services;
-  using YAF.Classes.Data;
-  using YAF.Utils;
-  using YAF.Types;
-  using YAF.Types.Constants;
-
-  #endregion
-
-  /// <summary>
-  /// The most active users.
-  /// </summary>
-  [ToolboxData("<{0}:MostActiveUsers runat=\"server\"></{0}:MostActiveUsers>")]
-  public class MostActiveUsers : BaseControl
-  {
-    #region Constants and Fields
-
-    /// <summary>
-    ///   The _display number.
-    /// </summary>
-    private int _displayNumber = 10;
-
-    /// <summary>
-    ///   The _last num of days.
-    /// </summary>
-    private int _lastNumOfDays = 7;
+    using YAF.Core;
+    using YAF.Types.Interfaces;
+    using YAF.Types.Constants;
+    using YAF.Core.Services;
+    using YAF.Classes.Data;
+    using YAF.Utils;
+    using YAF.Types;
+    using YAF.Types.Constants;
 
     #endregion
 
-    #region Properties
-
     /// <summary>
-    ///   Gets or sets DisplayNumber.
+    /// The most active users.
     /// </summary>
-    public int DisplayNumber
+    [ToolboxData("<{0}:MostActiveUsers runat=\"server\"></{0}:MostActiveUsers>")]
+    public class MostActiveUsers : BaseControl
     {
-      get
-      {
-        return this._displayNumber;
-      }
+        #region Constants and Fields
 
-      set
-      {
-        this._displayNumber = value;
-      }
+        /// <summary>
+        ///   The _display number.
+        /// </summary>
+        private int _displayNumber = 10;
+
+        /// <summary>
+        ///   The _last num of days.
+        /// </summary>
+        private int _lastNumOfDays = 7;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///   Gets or sets DisplayNumber.
+        /// </summary>
+        public int DisplayNumber
+        {
+            get
+            {
+                return this._displayNumber;
+            }
+
+            set
+            {
+                this._displayNumber = value;
+            }
+        }
+
+        /// <summary>
+        ///   Gets or sets LastNumOfDays.
+        /// </summary>
+        public int LastNumOfDays
+        {
+            get
+            {
+                return this._lastNumOfDays;
+            }
+
+            set
+            {
+                this._lastNumOfDays = value;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Renders the MostActiveUsers class.
+        /// </summary>
+        /// <param name="writer">
+        /// </param>
+        protected override void Render([NotNull] HtmlTextWriter writer)
+        {
+            int currentRank = 1;
+            string actRank = string.Empty;
+
+            DataTable rankDt = this.Get<IDataCache>().GetOrSet(
+              Constants.Cache.MostActiveUsers,
+              () =>
+              LegacyDb.user_activity_rank(
+                this.PageContext.PageBoardID, DateTime.UtcNow.AddDays(-this.LastNumOfDays), this.DisplayNumber),
+              TimeSpan.FromMinutes(5));
+
+            //// create XML data document...
+            // XmlDocument xml = new XmlDocument();
+
+            // rankDt.TableName = "UserActivityRank";
+            // xml.LoadXml( rankDt.DataSet.GetXml() );
+
+            //// transform using the MostActiveUser xslt...
+            // const string xsltFile = "YAF.Controls.Statistics.MostActiveUser.xslt";
+
+            // using ( Stream resourceStream = Assembly.GetAssembly( this.GetType() ).GetManifestResourceStream( xsltFile ) )
+            // {
+            // if ( resourceStream != null )
+            // {
+            // XslCompiledTransform myXslTrans = new XslCompiledTransform();
+
+            // //load the Xsl 
+            // myXslTrans.Load( XmlReader.Create( resourceStream ) );
+            // myXslTrans.Transform( xml.CreateNavigator(), xslArgs, writer );
+            // }
+            // }
+            writer.BeginRender();
+
+            var html = new StringBuilder();
+
+            html.AppendFormat(@"<div id=""{0}"" class=""yaf_activeuser"">", this.ClientID);
+            html.AppendFormat(@"<h2 class=""yaf_header"">{0}</h2>", "Most Active Users");
+            html.AppendFormat(@"<h4 class=""yaf_subheader"">Last {0} Days</h4>", this.LastNumOfDays);
+
+            html.AppendLine("<ol>");
+
+            // flush...
+            writer.Write(html.ToString());
+
+            foreach (DataRow row in rankDt.Rows)
+            {
+                writer.WriteLine("<li>");
+
+                // render UserLink...
+                var userLink = new UserLink { UserID = row.Field<int>("ID"), };
+                userLink.RenderControl(writer);
+
+                // render online image...
+                var onlineStatusImage = new OnlineStatusImage { UserID = row.Field<int>("ID") };
+                onlineStatusImage.RenderControl(writer);
+
+                writer.WriteLine(" ");
+                writer.WriteLine(@"<span class=""NumberOfPosts"">({0})</span>".FormatWith(row.Field<int>("NumOfPosts")));
+                writer.WriteLine("</li>");
+            }
+
+            writer.WriteLine("</ol>");
+            writer.WriteLine("</div>");
+            writer.EndRender();
+        }
+
+        #endregion
     }
-
-    /// <summary>
-    ///   Gets or sets LastNumOfDays.
-    /// </summary>
-    public int LastNumOfDays
-    {
-      get
-      {
-        return this._lastNumOfDays;
-      }
-
-      set
-      {
-        this._lastNumOfDays = value;
-      }
-    }
-
-    #endregion
-
-    #region Methods
-
-    /// <summary>
-    /// Renders the MostActiveUsers class.
-    /// </summary>
-    /// <param name="writer">
-    /// </param>
-    protected override void Render([NotNull] HtmlTextWriter writer)
-    {
-      int currentRank = 1;
-      string actRank = string.Empty;
-
-      DataTable rankDt = this.Get<IDataCache>().GetOrSet(
-        Constants.Cache.MostActiveUsers,
-        () =>
-        LegacyDb.user_activity_rank(
-          this.PageContext.PageBoardID, DateTime.UtcNow.AddDays(-this.LastNumOfDays), this.DisplayNumber),
-        TimeSpan.FromMinutes(5));
-
-      //// create XML data document...
-      // XmlDocument xml = new XmlDocument();
-
-      // rankDt.TableName = "UserActivityRank";
-      // xml.LoadXml( rankDt.DataSet.GetXml() );
-
-      //// transform using the MostActiveUser xslt...
-      // const string xsltFile = "YAF.Controls.Statistics.MostActiveUser.xslt";
-
-      // using ( Stream resourceStream = Assembly.GetAssembly( this.GetType() ).GetManifestResourceStream( xsltFile ) )
-      // {
-      // if ( resourceStream != null )
-      // {
-      // XslCompiledTransform myXslTrans = new XslCompiledTransform();
-
-      // //load the Xsl 
-      // myXslTrans.Load( XmlReader.Create( resourceStream ) );
-      // myXslTrans.Transform( xml.CreateNavigator(), xslArgs, writer );
-      // }
-      // }
-      writer.BeginRender();
-
-      var html = new StringBuilder();
-
-      html.AppendFormat(@"<div id=""{0}"" class=""yaf_activeuser"">", this.ClientID);
-      html.AppendFormat(@"<h2 class=""yaf_header"">{0}</h2>", "Most Active Users");
-      html.AppendFormat(@"<h4 class=""yaf_subheader"">Last {0} Days</h4>", this.LastNumOfDays);
-
-      html.AppendLine("<ol>");
-
-      // flush...
-      writer.Write(html.ToString());
-
-      foreach (DataRow row in rankDt.Rows)
-      {
-        writer.WriteLine("<li>");
-
-        // render UserLink...
-        var userLink = new UserLink { UserID = row.Field<int>("ID"), };
-        userLink.RenderControl(writer);
-
-        // render online image...
-        var onlineStatusImage = new OnlineStatusImage { UserID = row.Field<int>("ID") };
-        onlineStatusImage.RenderControl(writer);
-
-        writer.WriteLine(" ");
-        writer.WriteLine(@"<span class=""NumberOfPosts"">({0})</span>".FormatWith(row.Field<int>("NumOfPosts")));
-        writer.WriteLine("</li>");
-      }
-
-      writer.WriteLine("</ol>");
-      writer.WriteLine("</div>");
-      writer.EndRender();
-    }
-
-    #endregion
-  }
 }
