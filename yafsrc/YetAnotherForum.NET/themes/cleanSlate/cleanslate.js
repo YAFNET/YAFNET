b86caf11ab7b6e5f6f65c68b643f80e8252425d8
Watcher <Watcher@Watcher-One>
2012-06-23 09:25:15 +0200

    updated theme js files for the hovercard

--- yafsrc/YetAnotherForum.NET/themes/cleanSlate/cleanslate.js
+++ yafsrc/YetAnotherForum.NET/themes/cleanSlate/cleanslate.js
@@ -10,13 +10,13 @@
     jQuery(".yafnet .postContainer, .yafnet .postContainer_Alt").hover(function () {
         jQuery(this).find(".avatarimage").stop(true, true).fadeTo("fast", 1.0);
         jQuery(this).find('.postPosted .postedRight').stop(true, true).fadeIn('fast');
-        jQuery(this).find('.postInfoBottom div').stop(true, true).fadeIn('fast');
+        jQuery(this).find('.postInfoBottom div').eq(0).stop(true, true).fadeIn('fast');
     }, function () {
         jQuery(this).find(".avatarimage").stop().fadeTo("fast", 0.5);
         jQuery(this).find('.postPosted .postedRight').stop(true, true).fadeOut('slow');
-        jQuery(this).find('.postInfoBottom div').stop(true, true).fadeOut('slow');
+        jQuery(this).find('.postInfoBottom div').eq(0).stop(true, true).fadeOut('slow');
     });
 
     jQuery('.yafnet .postPosted .postedRight').hide();
-    jQuery('.yafnet .postInfoBottom div').hide();
+    jQuery('.yafnet .postInfoBottom div').eq(0).hide();
 });
