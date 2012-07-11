5c8832715fea8d4fae320253f0c3e0aeb58ba3c0
Jaben <jaben@yetanotherforum.net>
2012-07-11 01:38:31 -0400

    Cleaned up the code a bit and added attribution.

--- yafsrc/YAF.Core/Extensions/MailMessageExtensions.cs
+++ yafsrc/YAF.Core/Extensions/MailMessageExtensions.cs
@@ -16,109 +16,122 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 namespace YAF.Core
 {
-  #region Using
+    #region Using
 
-	using System;
-	using System.Linq;
-	using System.Net.Mail;
-  using System.Net.Mime;
-  using System.Text;
-  using System.Text.RegularExpressions;
+    using System;
+    using System.Linq;
+    using System.Net.Mail;
+    using System.Net.Mime;
+    using System.Text;
 
-  using YAF.Classes;
-  using YAF.Utils;
-  using YAF.Utils.Helpers.StringUtils;
-  using YAF.Types;
-
-  #endregion
-
-  /// <summary>
-  /// The mail message extensions.
-  /// </summary>
-  public static class MailMessageExtensions
-  {
-    #region Public Methods
-
-    /// <summary>
-    /// The populate.
-    /// </summary>
-    /// <param name="mailMessage">
-    /// The mail message.
-    /// </param>
-    /// <param name="fromAddress">
-    /// The from address.
-    /// </param>
-    /// <param name="toAddress">
-    /// The to address.
-    /// </param>
-    /// <param name="subject">
-    /// The subject.
-    /// </param>
-    /// <param name="bodyText">
-    /// The body text.
-    /// </param>
-    /// <param name="bodyHtml">
-    /// The body html.
-    /// </param>
-    /// <returns>
-    /// </returns>
-    [NotNull]
-    public static void Populate([NotNull] this MailMessage mailMessage, 
-      [NotNull] MailAddress fromAddress, 
-      [NotNull] MailAddress toAddress, 
-      [CanBeNull] string subject, 
-      [CanBeNull] string bodyText, 
-      [CanBeNull] string bodyHtml)
-    {
-      CodeContracts.ArgumentNotNull(mailMessage, "mailMessage");
-      CodeContracts.ArgumentNotNull(fromAddress, "fromAddress");
-      CodeContracts.ArgumentNotNull(toAddress, "toAddress");
-
-      mailMessage.To.Add(toAddress);
-      mailMessage.From = fromAddress;
-      mailMessage.Subject = subject;
-
-      // add default text view
-			mailMessage.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(bodyText, ContainsUnicodeCharacter(bodyText) ? Encoding.Unicode : Encoding.UTF8, MediaTypeNames.Text.Plain));
-
-      // see if html alternative is also desired...
-      if (bodyHtml.IsSet())
-      {
-        mailMessage.AlternateViews.Add(
-					AlternateView.CreateAlternateViewFromString(bodyHtml, ContainsUnicodeCharacter(bodyHtml) ? Encoding.Unicode : Encoding.UTF8, MediaTypeNames.Text.Html));
-      }
-    }
-
-		public static bool ContainsUnicodeCharacter(string input)
-		{
-			const int MaxAnsiCode = 255;
-
-			return input.ToCharArray().Any(c => c > MaxAnsiCode);
-		}
-
-    /// <summary>
-    /// Creates a SmtpClient and sends a MailMessage.
-    /// </summary>
-    /// <param name="message">
-    /// The message.
-    /// </param>
-    public static void Send([NotNull] this MailMessage message)
-    {
-      CodeContracts.ArgumentNotNull(message, "message");
-
-      var smtpSend = new SmtpClient { EnableSsl = Config.UseSMTPSSL };
-
-      // Tommy: solve random failure problem. Don't set this value to 1.
-      // See this: http://stackoverflow.com...tem-net-mail-has-issues 
-      smtpSend.ServicePoint.MaxIdleTime = 10;
-      smtpSend.ServicePoint.ConnectionLimit = 1;
-
-      // send the message...
-      smtpSend.Send(message);
-    }
+    using YAF.Classes;
+    using YAF.Types;
+    using YAF.Utils;
 
     #endregion
-  }
+
+    /// <summary>
+    /// The mail message extensions.
+    /// </summary>
+    public static class MailMessageExtensions
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Does the string contain any unicode characters?
+        /// From http://stackoverflow.com/questions/4459571/how-to-recognize-if-a-string-contains-unicode-chars
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The contains unicode character.
+        /// </returns>
+        public static bool ContainsUnicodeCharacter([NotNull] string input)
+        {
+            CodeContracts.ArgumentNotNull(input, "input");
+
+            const int MaxAnsiCode = 255;
+
+            return input.ToCharArray().Any(c => c > MaxAnsiCode);
+        }
+
+        /// <summary>
+        /// The populate.
+        /// </summary>
+        /// <param name="mailMessage">
+        /// The mail message. 
+        /// </param>
+        /// <param name="fromAddress">
+        /// The from address. 
+        /// </param>
+        /// <param name="toAddress">
+        /// The to address. 
+        /// </param>
+        /// <param name="subject">
+        /// The subject. 
+        /// </param>
+        /// <param name="bodyText">
+        /// The body text. 
+        /// </param>
+        /// <param name="bodyHtml">
+        /// The body html. 
+        /// </param>
+        [NotNull]
+        public static void Populate(
+            [NotNull] this MailMessage mailMessage, 
+            [NotNull] MailAddress fromAddress, 
+            [NotNull] MailAddress toAddress, 
+            [CanBeNull] string subject, 
+            [CanBeNull] string bodyText, 
+            [CanBeNull] string bodyHtml)
+        {
+            CodeContracts.ArgumentNotNull(mailMessage, "mailMessage");
+            CodeContracts.ArgumentNotNull(fromAddress, "fromAddress");
+            CodeContracts.ArgumentNotNull(toAddress, "toAddress");
+
+            mailMessage.To.Add(toAddress);
+            mailMessage.From = fromAddress;
+            mailMessage.Subject = subject;
+
+            // add default text view
+            mailMessage.AlternateViews.Add(
+                AlternateView.CreateAlternateViewFromString(
+                    bodyText, ContainsUnicodeCharacter(bodyText) ? Encoding.Unicode : Encoding.UTF8, MediaTypeNames.Text.Plain));
+
+            // see if html alternative is also desired...
+            if (bodyHtml.IsSet())
+            {
+                mailMessage.AlternateViews.Add(
+                    AlternateView.CreateAlternateViewFromString(
+                        bodyHtml, ContainsUnicodeCharacter(bodyHtml) ? Encoding.Unicode : Encoding.UTF8, MediaTypeNames.Text.Html));
+            }
+        }
+
+        /// <summary>
+        /// Creates a SmtpClient and sends a MailMessage.
+        /// </summary>
+        /// <param name="message">
+        /// The message. 
+        /// </param>
+        public static void Send([NotNull] this MailMessage message)
+        {
+            CodeContracts.ArgumentNotNull(message, "message");
+
+            var smtpSend = new SmtpClient { EnableSsl = Config.UseSMTPSSL };
+
+            // Tommy: solve random failure problem. Don't set this value to 1.
+            // See this: http://stackoverflow.com...tem-net-mail-has-issues 
+            smtpSend.ServicePoint.MaxIdleTime = 10;
+            smtpSend.ServicePoint.ConnectionLimit = 1;
+
+            // send the message...
+            smtpSend.Send(message);
+        }
+
+        #endregion
+    }
 }
