1c5f1264194a898bb56f07cc36fa22764111982c
dmitry <dmitry@lit-karmazin.kontur>
2012-08-06 18:45:43 +0600

    fix

--- yafsrc/YAF.Utils/Helpers/IPHelper.cs
+++ yafsrc/YAF.Utils/Helpers/IPHelper.cs
@@ -17,6 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Web;
@@ -38,6 +40,36 @@ namespace YAF.Utils.Helpers
   public static class IPHelper
   {
     #region Public Methods
+
+        /// <summary>
+        /// Gets User IP considering X-Forwarded-For and X-Real-IP HTTP headers
+        /// </summary>
+        /// <see cref="http://wiki.nginx.org/HttpRealipModule"/>
+        /// <see cref="http://en.wikipedia.org/wiki/X-Forwarded-For"/>
+        /// <see cref="http://dev.opera.com/articles/view/opera-mini-request-headers/#x-forwarded-for"/>
+        /// <param name="httpRequest"> </param>
+        /// <returns>
+        /// Client ip
+        /// </returns>
+        public static string GetUserRealIPAddress([NotNull] this HttpRequestBase httpRequest)
+        {
+            CodeContracts.ArgumentNotNull(httpRequest, "httpContext");
+            IPAddress ipAddress;
+            string ipString;
+            ipString = httpRequest.Headers["X-Forwarded-For"];
+            if (!String.IsNullOrEmpty(ipString))
+            {
+                string[] ipAddresses = ipString.Split(',');
+                string firstNonLocalAddress = ipAddresses.FirstOrDefault(ip =>IPAddress.TryParse(ipString.Split(',')[0].Trim(), out ipAddress) && !IsNonRoutableIpAddress(ipAddress));
+                if (!String.IsNullOrEmpty(firstNonLocalAddress))
+                    return firstNonLocalAddress;
+            }
+            ipString = httpRequest.Headers["X-Real-IP"];
+            if (!String.IsNullOrEmpty(ipString))
+                if (IPAddress.TryParse((ipString.Split(',').LastOrDefault() ?? "").Trim(), out ipAddress))
+                    return ipString;
+            return httpRequest.UserHostAddress;
+        }
 
     /// <summary>
     /// The ip str to long.
@@ -183,5 +215,123 @@ namespace YAF.Utils.Helpers
 
 
     #endregion
+
+		#region Private Methods
+
+        static List<string> nonRoutableIPv4Networks = new List<string>(){"10.0.0.0/8","172.16.0.0/12","192.168.0.0/16","169.254.0.0/16","127.0.0.0/8","0.0.0.0/8"};
+        static List<string> nonRoutableIPv6Networks = new List<string>() { "::/128", "::1/128", "2001:db8::/32", "fc00::/7", "::ffff:0:0/96" };
+        private static bool IsNonRoutableIpAddress(this IPAddress ipAddress)
+        {
+            //Reference: http://en.wikipedia.org/wiki/Reserved_IP_addresses
+            byte[] ipAddressBytes = ipAddress.GetAddressBytes();
+
+            switch (ipAddress.AddressFamily)
+            {
+                case AddressFamily.InterNetwork: return nonRoutableIPv4Networks.Any(network => IsIpAddressInRange(ipAddressBytes, network));
+                case AddressFamily.InterNetworkV6: return nonRoutableIPv6Networks.Any(network => IsIpAddressInRange(ipAddressBytes, network));
+                default: return true;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="http://stackoverflow.com/questions/8230728/is-there-a-function-that-can-take-an-ipaddress-as-string-and-tell-me-if-its-a-no"/>
+        /// <param name="ipAddressBytes"></param>
+        /// <param name="reservedIpAddress"></param>
+        /// <returns></returns>
+        private static bool IsIpAddressInRange(byte[] ipAddressBytes, string reservedIpAddress)
+        {
+            if (String.IsNullOrEmpty(reservedIpAddress))
+                return false;
+
+            if (ipAddressBytes == null)
+                return false;
+            string[] ipAddressSplit = reservedIpAddress.Split(new char[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+            if (ipAddressSplit.Length != 2)
+                return false;
+
+            string ipAddressRange = ipAddressSplit[0];
+            IPAddress ipAddress = null;
+            if (!IPAddress.TryParse(ipAddressRange, out ipAddress))
+                return false;
+            byte[] ipBytes = ipAddress.GetAddressBytes();
+            int bits = 0;
+            if (!int.TryParse(ipAddressSplit[1], out bits))
+                bits = 0;
+            byte[] maskBytes = null;
+            if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+            {
+                uint mask = ~(uint.MaxValue >> bits);
+                maskBytes = BitConverter.GetBytes(mask).Reverse().ToArray();
+            }
+            else if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                BitArray bitArray = new BitArray(128, false);
+                ShiftRight(bitArray, bits, true);
+                maskBytes = ConvertToByteArray(bitArray).Reverse().ToArray();
+            }
+
+            bool result = true;
+            for (int i = 0; i < ipBytes.Length; i++)
+                result &= (byte) (ipAddressBytes[i] & maskBytes[i]) == ipBytes[i];
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bitArray"></param>
+        /// <param name="shiftN"></param>
+        /// <param name="fillValue"></param>
+        private static void ShiftRight(BitArray bitArray, int shiftN, bool fillValue)
+        {
+            for (int i = shiftN; i < bitArray.Count; i++)
+            {
+                bitArray[i - shiftN] = bitArray[i];
+            }
+
+            for (int index = bitArray.Count - shiftN; index < bitArray.Count; index++)
+            {
+                bitArray[index] = fillValue;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bitArray"></param>
+        /// <returns></returns>
+        private static byte[] ConvertToByteArray(BitArray bitArray)
+        {
+            // pack (in this case, using the first bool as the lsb - if you want
+            // the first bool as the msb, reverse things ;-p)
+            int bytes = (bitArray.Length + 7)/8;
+            byte[] arr2 = new byte[bytes];
+            int bitIndex = 0;
+            int byteIndex = 0;
+
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                if (bitArray[i])
+                {
+                    arr2[byteIndex] |= (byte) (1 << bitIndex);
+                }
+
+                bitIndex++;
+                if (bitIndex == 8)
+                {
+                    bitIndex = 0;
+                    byteIndex++;
+                }
+            }
+
+            return arr2;
+        }
+
+
+        #endregion
   }
 }
