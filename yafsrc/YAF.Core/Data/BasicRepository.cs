62cf71396d9108f45f4d02473744152e1dd2cb25
Jaben <jaben@yetanotherforum.net>
2012-09-17 07:29:38 -0400

    Forgot the BasicRepository.

---
+++ yafsrc/YAF.Core/Data/BasicRepository.cs
@@ -0,0 +1,60 @@
+﻿/* Yet Another Forum.NET
+ * Copyright (C) 2006-2012 Jaben Cargman
+ * http://www.yetanotherforum.net/
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+namespace YAF.Core.Data
+{
+    #region Using
+
+    using YAF.Types.Interfaces;
+    using YAF.Types.Interfaces.Data;
+
+    #endregion
+
+    /// <summary>
+    /// The basic repository.
+    /// </summary>
+    /// <typeparam name="T">
+    /// </typeparam>
+    public class BasicRepository<T> : IRepository<T>
+        where T : IEntity
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BasicRepository{T}"/> class.
+        /// </summary>
+        /// <param name="dbFunction">
+        /// The db function.
+        /// </param>
+        public BasicRepository(IDbFunction dbFunction)
+        {
+            this.DbFunction = dbFunction;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the db function.
+        /// </summary>
+        public IDbFunction DbFunction { get; private set; }
+
+        #endregion
+    }
+}
