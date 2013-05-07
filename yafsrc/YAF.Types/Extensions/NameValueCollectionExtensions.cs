96f07a6bba3cb7e6ab7183c6ef495081f56c0ec8
Jaben <jaben@yetanotherforum.net>
2013-05-06 20:34:30 -0400

    Not sure why I didn't make this case insensitive before.
    Fixed that error... now the Digest works for me again.

--- yafsrc/YAF.Types/Extensions/NameValueCollectionExtensions.cs
+++ yafsrc/YAF.Types/Extensions/NameValueCollectionExtensions.cs
@@ -16,34 +16,49 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 namespace YAF.Types.Extensions
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Collections.Specialized;
     using System.Linq;
 
-    using YAF.Types;
-
     public static class NameValueCollectionExtensions
     {
+        #region Public Methods and Operators
+
         /// <summary>
-        /// Flattens a <see cref="NameValueCollection"/> to a simple string <see cref="IDictionary{TKey,TValue}"/>.
+        ///     Gets the first value of <paramref name="paramName" /> in the collection or default (Null).
         /// </summary>
-        /// <param name="collection">
-        /// </param>
-        /// <returns>
-        /// </returns>
-        [NotNull]
-        public static ILookup<string, string> ToLookup([NotNull] this NameValueCollection collection)
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static string GetFirstOrDefault(
+            [NotNull] this NameValueCollection collection, [NotNull] string paramName, IEqualityComparer<string> comparer = null)
         {
             CodeContracts.ArgumentNotNull(collection, "collection");
+            CodeContracts.ArgumentNotNull(paramName, "paramName");
 
-            return collection.Cast<string>().ToLookup(key => key, key => collection[key]);
+            return collection.ToLookup(comparer)[paramName].FirstOrDefault();
         }
 
         /// <summary>
-        /// Gets the value as an <see cref="IEnumerable"/> handling splitting the string if needed.
+        ///     Gets the first value of <paramref name="paramName" /> in the collection as T or default (Null).
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static T GetFirstOrDefaultAs<T>(
+            [NotNull] this NameValueCollection collection, [NotNull] string paramName, IEqualityComparer<string> comparer = null)
+        {
+            CodeContracts.ArgumentNotNull(collection, "collection");
+            CodeContracts.ArgumentNotNull(paramName, "paramName");
+
+            return collection.GetFirstOrDefault(paramName, comparer).ToType<T>();
+        }
+
+        /// <summary>
+        ///     Gets the value as an <see cref="IEnumerable" /> handling splitting the string if needed.
         /// </summary>
         /// <param name="collection"></param>
         /// <returns>Does not return null.</returns>
@@ -56,24 +71,20 @@ namespace YAF.Types.Extensions
         }
 
         /// <summary>
-        /// Gets the first value of <paramref name="paramName"/> in the collection or default (Null).
+        ///     Flattens a <see cref="NameValueCollection" /> to a simple string <see cref="IDictionary{TKey,TValue}" />.
         /// </summary>
-        /// <param name="collection"></param>
-        /// <returns></returns>
-        public static string GetFirstOrDefault([NotNull] this NameValueCollection collection, [NotNull] string paramName)
+        /// <param name="collection">
+        /// </param>
+        /// <returns>
+        /// </returns>
+        [NotNull]
+        public static ILookup<string, string> ToLookup([NotNull] this NameValueCollection collection, IEqualityComparer<string> comparer = null)
         {
             CodeContracts.ArgumentNotNull(collection, "collection");
-            CodeContracts.ArgumentNotNull(paramName, "paramName");
 
-            return collection.ToLookup()[paramName].FirstOrDefault();
+            return collection.Cast<string>().ToLookup(key => key, key => collection[key], comparer ?? StringComparer.OrdinalIgnoreCase);
         }
 
-        public static T GetFirstOrDefaultAs<T>([NotNull] this NameValueCollection collection, [NotNull] string paramName)
-        {
-            CodeContracts.ArgumentNotNull(collection, "collection");
-            CodeContracts.ArgumentNotNull(paramName, "paramName");
-
-            return collection.GetFirstOrDefault(paramName).ToType<T>();
-        }
+        #endregion
     }
 }
