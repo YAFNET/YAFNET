5e64ed3b92dbe1d0feca02b4d56f354f98ae9e5a
w8tcha <thewatcher@watchersnet.de>
2013-05-23 14:34:32 +0200

    testing a close button on the hovercard

--- yafsrc/YetAnotherForum.NET/resources/js/jquery.hovercard.js
+++ yafsrc/YetAnotherForum.NET/resources/js/jquery.hovercard.js
@@ -14,7 +14,7 @@
             openOnTop: false,
             cardImgSrc: "",
             detailsHTML: "",
-            loadingHTML: "Loading1...",
+            loadingHTML: "Loading...",
             errorHTML: "Sorry, no data found.",
             twitterScreenName: '',
             showTwitterCard: false,
@@ -131,9 +131,17 @@
                     
                     $("body").on("keydown", function (event) {
                         if (event.keyCode === 27) {
-                            closeHoverCard($(this));
+                            closeHoverCard($this);
                         }
                     });
+					
+					var closeButton = curHCDetails.find(".s-close").eq(0);
+					
+					console.log(closeButton);
+					
+					closeButton.click(function() {
+						closeHoverCard($this);
+					});
 
                     //Callback function                    
                     options.onHoverIn.call(this);
@@ -223,8 +231,9 @@
                             $('body').append(script);
                         }
                         curHCDetails.append('<span class="s-action"><a href="https://twitter.com/' + username + '" class="twitter-follow-button" data-show-count="false" data-show-name="false" data-button="grey" data-width="65px" class="twitter-follow-button">Follow</a></span>');
-
+						curHCDetails.append('<span class="s-action s-close"><a href="javascript:void(0)"></a></span>');
                     }
+					
                     break;
                 case "facebook":
                     {
@@ -255,6 +264,7 @@
                                 curHCDetails.append('<span class="s-action">' + $('#fb-like' + profileData.id).html() + '</span>');
                             } else {
                                 curHCDetails.append('<span class="s-action"><div class="fb-like" id="fb-like' + profileData.id + '"><iframe src="//www.facebook.com/plugins/like.php?href=' + profileData.link + ';send=false&amp;layout=standard&amp;width=90&amp;show_faces=false&amp;action=like&amp;layout=button_count&amp;font&amp;height=21&amp" scrolling="no" frameborder="0" style="border:none; overflow:hidden;width:77px;height:21px" allowTransparency="true"></iframe></div></span>');
+								curHCDetails.append('<span class="s-action s-close"><a href="javascript:void(0)"></a></span>');
                             }
                         };
                     }
@@ -308,6 +318,8 @@
                         errorHTML = options.errorHTML;
                         customCallback = function() {
                         };
+						
+						curHCDetails.append('<span class="s-action s-close"><a href="javascript:void(0)"></a></span>');
                     }
                     break;
                 default:
