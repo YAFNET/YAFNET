37a6d01ffd3bdd3da7cc558212ab60dc50b4fdc0
w8tcha <thewatcher@watchersnet.de>
2013-06-01 17:30:34 +0200

    added catch handler to the twitter login button, if the button throws an exception the page is still shown

--- yafsrc/YetAnotherForum.NET/pages/login.ascx.cs
+++ yafsrc/YetAnotherForum.NET/pages/login.ascx.cs
@@ -319,15 +319,22 @@ namespace YAF.Pages
 
                 if (twitterHolder.Visible)
                 {
-                    // Redirect the user to Twitter for authorization.
-                    twitterLogin.Attributes.Add(
-                        "onclick",
-                        YafSingleSignOnUser.GenerateTwitterLoginUrl(true));
+                    try
+                    {
+                        var twitterLoginUrl = YafSingleSignOnUser.GenerateTwitterLoginUrl(true);
 
-                    twitterLogin.InnerHtml =
-                        @"<img src=""{0}"" alt=""{1}"" title=""{1}"" style=""margin:0;"">".FormatWith(
-                            "{0}images/twitter_signin.png".FormatWith(YafForumInfo.ForumClientFileRoot),
-                            this.GetText("LOGIN", "TWITTER_LOGIN"));
+                        // Redirect the user to Twitter for authorization.
+                        twitterLogin.Attributes.Add("onclick", twitterLoginUrl);
+
+                        twitterLogin.InnerHtml =
+                            "<img src=\"{0}\" alt=\"{1}\" title=\"{1}\" style=\"margin:0;\">".FormatWith(
+                                "{0}images/twitter_signin.png".FormatWith(YafForumInfo.ForumClientFileRoot),
+                                this.GetText("LOGIN", "TWITTER_LOGIN"));
+                    }
+                    catch (Exception)
+                    {
+                        twitterHolder.Visible = false;
+                    }
                 }
             }
 
