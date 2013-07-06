1e3e0482d1dfdc8c1880fbdee15ce6d19d76376c
w8tcha <thewatcher@watchersnet.de>
2013-07-06 18:30:15 +0200

    urls with tilde '~' signs are now corretly parsed

--- yafsrc/YAF.Core/Services/YafFormatMessage.cs
+++ yafsrc/YAF.Core/Services/YafFormatMessage.cs
@@ -59,7 +59,7 @@ namespace YAF.Core.Services
 
         private static readonly Regex _rgxUrl1 =
             new Regex(
-                @"(?<before>^|[ ]|\[[A-Za-z0-9]\]|\[\*\]|[A-Za-z0-9])(?<!href="")(?<!src="")(?<inner>(http://|https://|ftp://)(?:[\w-]+\.)+[\w-]+(?:/[\w-./?+%#&=;:,]*)?)",
+                @"(?<before>^|[ ]|\[[A-Za-z0-9]\]|\[\*\]|[A-Za-z0-9])(?<!href="")(?<!src="")(?<inner>(http://|https://|ftp://)(?:[\w-]+\.)+[\w-]+(?:/[\w-./?+%#&=;:,~]*)?)",
                 _Options | RegexOptions.Compiled);
 
         private static readonly Regex _rgxUrl2 =
@@ -69,7 +69,7 @@ namespace YAF.Core.Services
 
         private static readonly Regex _rgxUrl3 =
             new Regex(
-                @"(?<before>^|[ ]|\[[A-Za-z0-9]\]|\[\*\]|[A-Za-z0-9])(?<!http://)(?<inner>www\.(?:[\w-]+\.)+[\w-]+(?:/[\w-./?%+#&=;,]*)?)",
+                @"(?<before>^|[ ]|\[[A-Za-z0-9]\]|\[\*\]|[A-Za-z0-9])(?<!http://)(?<inner>www\.(?:[\w-]+\.)+[\w-]+(?:/[\w-./?%+#&=;,~]*)?)",
                 _Options | RegexOptions.Compiled);
 
         #endregion
