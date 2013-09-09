43b0152651b56fab37345052546760bd1fe4e2ad
Ingo Herbote <thewatcher@watchersnet.de>
2013-09-09 13:38:02 +0200

    made the page links data table public for the dnn module + added bootstrap css classes

--- yafsrc/YAF.Controls/PageLinks.cs
+++ yafsrc/YAF.Controls/PageLinks.cs
@@ -18,230 +18,213 @@
  */
 namespace YAF.Controls
 {
-  #region Using
+    #region Using
 
-  using System;
-  using System.Data;
-  using System.Web.UI;
+    using System.Data;
+    using System.Web.UI;
 
-  using YAF.Core;
-  using YAF.Types.Extensions;
-  using YAF.Types.Interfaces; using YAF.Types.Constants;
-  using YAF.Classes.Data;
-  using YAF.Utils;
-  using YAF.Types;
-  using YAF.Types.Constants;
-
-  #endregion
-
-  /// <summary>
-  /// Summary description for PageLinks.
-  /// </summary>
-  public class PageLinks : BaseControl
-  {
-    #region Properties
-
-    /// <summary>
-    ///   Gets or sets LinkedPageLinkID.
-    /// </summary>
-    [CanBeNull]
-    public string LinkedPageLinkID
-    {
-      get
-      {
-        if (this.ViewState["LinkedPageLinkID"] != null)
-        {
-          return this.ViewState["LinkedPageLinkID"].ToString();
-        }
-
-        return null;
-      }
-
-      set
-      {
-        this.ViewState["LinkedPageLinkID"] = value;
-      }
-    }
-
-    /// <summary>
-    ///   Gets or sets PageLinkDT.
-    /// </summary>
-    [CanBeNull]
-    protected DataTable PageLinkDT
-    {
-      get
-      {
-        if (this.ViewState["PageLinkDT"] != null)
-        {
-          return this.ViewState["PageLinkDT"] as DataTable;
-        }
-
-        return null;
-      }
-
-      set
-      {
-        this.ViewState["PageLinkDT"] = value;
-      }
-    }
+    using YAF.Classes.Data;
+    using YAF.Core;
+    using YAF.Types;
+    using YAF.Types.Constants;
+    using YAF.Types.Extensions;
+    using YAF.Utils;
 
     #endregion
 
-    #region Public Methods
-
     /// <summary>
-    /// The add forum links.
+    /// Page Links Control.
     /// </summary>
-    /// <param name="forumID">
-    /// The forum id.
-    /// </param>
-    public void AddForumLinks(int forumID)
+    public class PageLinks : BaseControl
     {
-      this.AddForumLinks(forumID, false);
-    }
+        #region Properties
 
-    /// <summary>
-    /// The add forum links.
-    /// </summary>
-    /// <param name="forumID">
-    /// The forum id.
-    /// </param>
-    /// <param name="noForumLink">
-    /// The no forum link.
-    /// </param>
-    public void AddForumLinks(int forumID, bool noForumLink)
-    {
-      using (DataTable dtLinks = LegacyDb.forum_listpath(forumID))
-      {
-        foreach (DataRow row in dtLinks.Rows)
+        /// <summary>
+        ///   Gets or sets LinkedPageLinkID.
+        /// </summary>
+        [CanBeNull]
+        public string LinkedPageLinkID
         {
-          if (noForumLink && Convert.ToInt32(row["ForumID"]) == forumID)
-          {
-            this.AddLink(row["Name"].ToString(), string.Empty);
-          }
-          else
-          {
-            this.AddLink(row["Name"].ToString(), YafBuildLink.GetLink(ForumPages.topics, "f={0}", row["ForumID"]));
-          }
-        }
-      }
-    }
+            get
+            {
+                return this.ViewState["LinkedPageLinkID"] != null ? this.ViewState["LinkedPageLinkID"].ToString() : null;
+            }
 
-    /// <summary>
-    /// The add link.
-    /// </summary>
-    /// <param name="title">
-    /// The title.
-    /// </param>
-    public void AddLink([NotNull] string title)
-    {
-      this.AddLink(title, string.Empty);
-    }
-
-    /// <summary>
-    /// The add link.
-    /// </summary>
-    /// <param name="title">
-    /// The title.
-    /// </param>
-    /// <param name="url">
-    /// The url.
-    /// </param>
-    public void AddLink([NotNull] string title, [NotNull] string url)
-    {
-      DataTable dt = this.PageLinkDT;
-
-      if (dt == null)
-      {
-        dt = new DataTable();
-        dt.Columns.Add("Title", typeof(string));
-        dt.Columns.Add("URL", typeof(string));
-        this.PageLinkDT = dt;
-      }
-
-      DataRow dr = dt.NewRow();
-      dr["Title"] = title;
-      dr["URL"] = url;
-      dt.Rows.Add(dr);
-    }
-
-    /// <summary>
-    /// Clear all Links
-    /// </summary>
-    public void Clear()
-    {
-      if (this.PageLinkDT != null)
-      {
-        this.PageLinkDT = null;
-      }
-    }
-
-    #endregion
-
-    #region Methods
-
-    /// <summary>
-    /// The render.
-    /// </summary>
-    /// <param name="writer">
-    /// The writer.
-    /// </param>
-    protected override void Render([NotNull] HtmlTextWriter writer)
-    {
-      DataTable linkDataTable = null;
-
-      if (this.LinkedPageLinkID.IsSet())
-      {
-        // attempt to get access to the other control...
-        var plControl = this.Parent.FindControl(this.LinkedPageLinkID) as PageLinks;
-
-        if (plControl != null)
-        {
-          // use the other data stream...
-          linkDataTable = plControl.PageLinkDT;
-        }
-      }
-      else
-      {
-        // use the data table from this control...
-        linkDataTable = this.PageLinkDT;
-      }
-
-      if (linkDataTable == null || linkDataTable.Rows.Count == 0)
-      {
-        return;
-      }
-
-      writer.WriteLine(@"<div id=""{0}"" class=""yafPageLink"">".FormatWith(this.ClientID));
-
-      bool bFirst = true;
-      foreach (DataRow row in linkDataTable.Rows)
-      {
-        if (!bFirst)
-        {
-          writer.WriteLine(@"<span class=""linkSeperator"">&nbsp;&#187;&nbsp;</span>");
-        }
-        else
-        {
-          bFirst = false;
+            set
+            {
+                this.ViewState["LinkedPageLinkID"] = value;
+            }
         }
 
-        string title = this.HtmlEncode(row["Title"].ToString().Trim());
-        string url = row["URL"].ToString().Trim();
-
-        if (url.IsNotSet())
+        /// <summary>
+        ///   Gets or sets PageLinkDT.
+        /// </summary>
+        [CanBeNull]
+        protected DataTable PageLinkDT
         {
-          writer.WriteLine(@"<span class=""currentPageLink"">{0}</span>".FormatWith(title));
-        }
-        else
-        {
-          writer.WriteLine(@"<a href=""{0}"">{1}</a>".FormatWith(url, title));
-        }
-      }
+            get
+            {
+                if (this.ViewState["PageLinkDT"] != null)
+                {
+                    return this.ViewState["PageLinkDT"] as DataTable;
+                }
 
-      writer.WriteLine("</div>");
+                return null;
+            }
+
+            set
+            {
+                this.ViewState["PageLinkDT"] = value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Adds the forum links.
+        /// </summary>
+        /// <param name="forumID">The forum id.</param>
+        public void AddForumLinks(int forumID)
+        {
+            this.AddForumLinks(forumID, false);
+        }
+
+        /// <summary>
+        /// Adds the forum links.
+        /// </summary>
+        /// <param name="forumID">
+        /// The forum id.
+        /// </param>
+        /// <param name="noForumLink">
+        /// The no forum link.
+        /// </param>
+        public void AddForumLinks(int forumID, bool noForumLink)
+        {
+            using (DataTable dtLinks = LegacyDb.forum_listpath(forumID))
+            {
+                foreach (DataRow row in dtLinks.Rows)
+                {
+                    if (noForumLink && row["ForumID"].ToType<int>() == forumID)
+                    {
+                        this.AddLink(row["Name"].ToString(), string.Empty);
+                    }
+                    else
+                    {
+                        this.AddLink(
+                            row["Name"].ToString(),
+                            YafBuildLink.GetLink(ForumPages.topics, "f={0}", row["ForumID"]));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add a link.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        public void AddLink([NotNull] string title)
+        {
+            this.AddLink(title, string.Empty);
+        }
+
+        /// <summary>
+        /// Add a link.
+        /// </summary>
+        /// <param name="title">
+        /// The title.
+        /// </param>
+        /// <param name="url">
+        /// The url.
+        /// </param>
+        public void AddLink([NotNull] string title, [NotNull] string url)
+        {
+            DataTable dt = this.PageLinkDT;
+
+            if (dt == null)
+            {
+                dt = new DataTable();
+                dt.Columns.Add("Title", typeof(string));
+                dt.Columns.Add("URL", typeof(string));
+                this.PageLinkDT = dt;
+            }
+
+            DataRow dr = dt.NewRow();
+            dr["Title"] = title;
+            dr["URL"] = url;
+            dt.Rows.Add(dr);
+        }
+
+        /// <summary>
+        /// Clear all Links
+        /// </summary>
+        public void Clear()
+        {
+            this.PageLinkDT = null;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sends server control content to a provided <see cref="T:System.Web.UI.HtmlTextWriter" /> object, which writes the content to be rendered on the client.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the server control content.</param>
+        protected override void Render([NotNull] HtmlTextWriter writer)
+        {
+            DataTable linkDataTable = null;
+
+            if (this.LinkedPageLinkID.IsSet())
+            {
+                // attempt to get access to the other control...
+                var plControl = this.Parent.FindControl(this.LinkedPageLinkID) as PageLinks;
+
+                if (plControl != null)
+                {
+                    // use the other data stream...
+                    linkDataTable = plControl.PageLinkDT;
+                }
+            }
+            else
+            {
+                // use the data table from this control...
+                linkDataTable = this.PageLinkDT;
+            }
+
+            if (linkDataTable == null || linkDataTable.Rows.Count == 0)
+            {
+                return;
+            }
+
+            writer.WriteLine(@"<div id=""{0}"" class=""yafPageLink breadcrumb"">".FormatWith(this.ClientID));
+
+            var first = true;
+
+            foreach (DataRow row in linkDataTable.Rows)
+            {
+                if (!first)
+                {
+                    writer.WriteLine(@"<span class=""linkSeperator divider"">&nbsp;&#187;&nbsp;</span>");
+                }
+                else
+                {
+                    first = false;
+                }
+
+                string title = this.HtmlEncode(row["Title"].ToString().Trim());
+                string url = row["URL"].ToString().Trim();
+
+                writer.WriteLine(
+                    url.IsNotSet()
+                        ? @"<span class=""currentPageLink active"">{0}</span>".FormatWith(title)
+                        : @"<a href=""{0}"">{1}</a>".FormatWith(url, title));
+            }
+
+            writer.WriteLine("</div>");
+        }
+
+        #endregion
     }
-
-    #endregion
-  }
 }
