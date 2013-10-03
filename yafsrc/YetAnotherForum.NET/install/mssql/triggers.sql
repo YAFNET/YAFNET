5e09c4d9fbadd96262f22057995680695fe668ec
Jaben <jaben@yetanotherforum.net>
2013-10-02 22:57:02 -0400

    Cleaning up triggers SQL.

--- yafsrc/YetAnotherForum.NET/install/mssql/triggers.sql
+++ yafsrc/YetAnotherForum.NET/install/mssql/triggers.sql
@@ -1,58 +1,56 @@
 /*
 ** Triggers
 */
-
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}Active_insert]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}Active_insert]
-go
-
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}Forum_update]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}Forum_update]
-go
-
-/*
-CREATE TRIGGER [{databaseOwner}].[{objectQualifier}Forum_update] ON [{databaseOwner}].[{objectQualifier}Forum] FOR UPDATE AS
-BEGIN
-	IF UPDATE(LastTopicID) OR UPDATE(LastMessageID)
-	BEGIN	
-		-- recursively update the forum
-		DECLARE @ParentID int		
-
-		SET @ParentID = (SELECT TOP 1 ParentID FROM inserted)
-		
-		WHILE (@ParentID IS NOT NULL)
-		BEGIN
-			UPDATE a SET
-				a.LastPosted = b.LastPosted,
-				a.LastTopicID = b.LastTopicID,
-				a.LastMessageID = b.LastMessageID,
-				a.LastUserID = b.LastUserID,
-				a.LastUserName = b.LastUserName
-			FROM
-				[{databaseOwner}].[{objectQualifier}Forum]] a, inserted b
-			WHERE
-				a.ForumID = @ParentID AND ((a.LastPosted < b.LastPosted) OR a.LastPosted IS NULL);
-			
-			SET @ParentID = (SELECT ParentID FROM [{databaseOwner}].[{objectQualifier}Forum] WHERE ForumID = @ParentID)
-		END
-	END
-END
-*/
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}Active_insert]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}Active_insert]
 GO
 
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}Group_update]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}Group_update]
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}Forum_update]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}Forum_update]
 GO
 
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}Group_insert]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}Group_insert]
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}Group_update]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}Group_update]
 GO
 
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}UserGroup_insert]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}UserGroup_insert]
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}Group_insert]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}Group_insert]
 GO
 
-if exists(select 1 from sys.objects where object_id=object_id(N'[{databaseOwner}].[{objectQualifier}UserGroup_delete]') and type in (N'TR'))
-	drop trigger [{databaseOwner}].[{objectQualifier}UserGroup_delete]
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}UserGroup_insert]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}UserGroup_insert]
 GO
 
+IF EXISTS (
+		SELECT 1
+		FROM sys.objects
+		WHERE object_id = object_id(N'[{databaseOwner}].[{objectQualifier}UserGroup_delete]')
+			AND type IN (N'TR')
+		)
+	DROP TRIGGER [{databaseOwner}].[{objectQualifier}UserGroup_delete]
+GO
