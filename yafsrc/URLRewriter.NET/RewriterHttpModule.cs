5db8931d75f28f5c5af5cf5b01d101b03af75783
Ingo Herbote <thewatcher@watchersnet.de>
2013-10-16 12:56:58 +0200

    rewriter module is now completly turned off if rewriting is disabled in the appsettings

--- yafsrc/URLRewriter.NET/RewriterHttpModule.cs
+++ yafsrc/URLRewriter.NET/RewriterHttpModule.cs
@@ -1,117 +1,137 @@
-// UrlRewriter - A .NET URL Rewriter module
+/* UrlRewriter - A .NET URL Rewriter module
 // Version 2.0
 //
 // Copyright 2007 Intelligencia
 // Copyright 2007 Seth Yates
-// 
-
-using System;
-using System.IO;
-using System.Net;
-using System.Web;
-using System.Resources;
-using System.Reflection;
-using Intelligencia.UrlRewriter.Configuration;
-using Intelligencia.UrlRewriter.Utilities;
+*/ 
 
 namespace Intelligencia.UrlRewriter
 {
-	/// <summary>
-	/// Rewrites urls based on patterns and conditions specified in the configuration file.  
-	/// This class cannot be inherited.
-	/// </summary>
-	public sealed class RewriterHttpModule : IHttpModule
-	{
-		/// <summary>
-		/// Initialises the module.
-		/// </summary>
-		/// <param name="context">The application context.</param>
-		void IHttpModule.Init(HttpApplication context)
-		{
-			context.BeginRequest += new EventHandler(BeginRequest);
-		}
+    using System;
+    using System.Web;
 
-		/// <summary>
-		/// Disposes of the module.
-		/// </summary>
-		void IHttpModule.Dispose()
-		{
-		}
+    using Intelligencia.UrlRewriter.Configuration;
+    using Intelligencia.UrlRewriter.Utilities;
 
-		/// <summary>
-		/// Configuration of the rewriter.
-		/// </summary>
-		public static RewriterConfiguration Configuration
-		{
-			get
-			{
-				return RewriterConfiguration.Current;
-			}
-		}
+    using YAF.Classes;
 
-		/// <summary>
-		/// Resolves an Application-path relative location
-		/// </summary>
-		/// <param name="location">The location</param>
-		/// <returns>The absolute location.</returns>
-		public static string ResolveLocation(string location)
-		{
-			return _rewriter.ResolveLocation(location);
-		}
-
-		/// <summary>
-		/// The original query string.
-		/// </summary>
-		public static string OriginalQueryString
-		{
-			get
-			{
-				return _rewriter.OriginalQueryString;
-			}
-			set
-			{
-				_rewriter.OriginalQueryString = value;
-			}
-		}
+    /// <summary>
+    /// Rewrites URLs based on patterns and conditions specified in the configuration file.  
+    /// This class cannot be inherited.
+    /// </summary>
+    public sealed class RewriterHttpModule : IHttpModule
+    {
+        /// <summary>
+        /// The _rewriter
+        /// </summary>
+        private static readonly RewriterEngine _Rewriter = new RewriterEngine(
+            new HttpContextFacade(),
+            RewriterConfiguration.Current);
 
         /// <summary>
-        /// The final querystring, after rewriting.
+        /// Gets or sets the original query string.
+        /// </summary>
+        public static string OriginalQueryString
+        {
+            get
+            {
+                return _Rewriter.OriginalQueryString;
+            }
+
+            set
+            {
+                _Rewriter.OriginalQueryString = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the final query string, after rewriting.
         /// </summary>
         public static string QueryString
         {
             get
             {
-                return _rewriter.QueryString;
+                return _Rewriter.QueryString;
             }
+
             set
             {
-                _rewriter.QueryString = value;
+                _Rewriter.QueryString = value;
             }
         }
 
         /// <summary>
-        /// The raw url, before rewriting.
+        /// Gets or sets the raw url, before rewriting.
         /// </summary>
         public static string RawUrl
         {
             get
             {
-                return _rewriter.RawUrl;
+                return _Rewriter.RawUrl;
             }
+
             set
             {
-                _rewriter.RawUrl = value;
+                _Rewriter.RawUrl = value;
             }
         }
 
+        /// <summary>
+        /// Gets the Configuration of the rewriter.
+        /// </summary>
+        /// <value>
+        /// The configuration.
+        /// </value>
+        public static RewriterConfiguration Configuration
+        {
+            get
+            {
+                return RewriterConfiguration.Current;
+            }
+        }
+
+        /// <summary>
+        /// Resolves an Application-path relative location
+        /// </summary>
+        /// <param name="location">The location</param>
+        /// <returns>The absolute location.</returns>
+        public static string ResolveLocation(string location)
+        {
+            return _Rewriter.ResolveLocation(location);
+        }
+
+        /// <summary>
+        /// Initializes a module and prepares it to handle requests.
+        /// </summary>
+        /// <param name="context">The application context.</param>
+        void IHttpModule.Init(HttpApplication context)
+        {
+            if (!Config.EnableURLRewriting)
+            {
+                return;
+            }
+
+            context.BeginRequest += this.BeginRequest;
+        }
+
+        /// <summary>
+        /// Disposes of the module.
+        /// </summary>
+        void IHttpModule.Dispose()
+        {
+        }
+
+        /// <summary>
+        /// Begins the request.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void BeginRequest(object sender, EventArgs e)
-		{
-			// Add our PoweredBy header
-			HttpContext.Current.Response.AddHeader(Constants.HeaderXPoweredBy, Configuration.XPoweredBy);
+        {
+            // Add our PoweredBy header
+            HttpContext.Current.Response.AddHeader(Constants.HeaderXPoweredBy, Configuration.XPoweredBy);
 
-			_rewriter.Rewrite();
-		}
-
-		private static RewriterEngine _rewriter = new RewriterEngine(new HttpContextFacade(), RewriterConfiguration.Current);
-	}
+            _Rewriter.Rewrite();
+        }
+    }
 }
