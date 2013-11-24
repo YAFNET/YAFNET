3c2c5795729c1df7b3decfd06c042aba9f0c845c
Ingo Herbote <thewatcher@watchersnet.de>
2013-11-24 05:50:14 +0100

    small optimizations

--- yafsrc/YetAnotherForum.NET/controls/DisplayAd.ascx.cs
+++ yafsrc/YetAnotherForum.NET/controls/DisplayAd.ascx.cs
@@ -19,66 +19,59 @@
  */
 namespace YAF.Controls
 {
-  #region Using
+    #region Using
 
-  using System;
+    using System;
 
-  using YAF.Classes;
-  using YAF.Core;
-  using YAF.Types;
-  using YAF.Types.Flags;
-  using YAF.Types.Interfaces;
-
-    #endregion
-
-  /// <summary>
-  /// DisplayAd Class
-  /// </summary>
-  public partial class DisplayAd : BaseUserControl
-  {
-    #region Properties
-
-    /// <summary>
-    ///   Gets or sets a value indicating whether IsAlt.
-    /// </summary>
-    public bool IsAlt { get; set; }
+    using YAF.Classes;
+    using YAF.Core;
+    using YAF.Types;
+    using YAF.Types.Flags;
+    using YAF.Types.Interfaces;
 
     #endregion
 
-    #region Methods
-
     /// <summary>
-    /// The get post class.
+    /// Display Ad Control
     /// </summary>
-    /// <returns>
-    /// Returns the post class.
-    /// </returns>
-    [NotNull]
-    protected string GetPostClass()
+    public partial class DisplayAd : BaseUserControl
     {
-      return this.IsAlt ? "post_alt" : "post";
+        #region Properties
+
+        /// <summary>
+        ///   Gets or sets a value indicating whether Is Alt. Row
+        /// </summary>
+        public bool IsAlt { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the post class.
+        /// </summary>
+        /// <returns>
+        /// Returns the post class.
+        /// </returns>
+        [NotNull]
+        protected string GetPostClass()
+        {
+            return this.IsAlt ? "post_alt" : "post";
+        }
+
+        /// <summary>
+        /// Handles the Load event of the Page control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Page_Load([NotNull] object sender, [NotNull] EventArgs e)
+        {
+            this.AdMessage.Message = this.Get<YafBoardSettings>().AdPost;
+            this.AdMessage.Signature = this.GetText("AD_SIGNATURE");
+
+            this.AdMessage.MessageFlags = new MessageFlags { IsLocked = true, NotFormatted = true };
+        }
+
+        #endregion
     }
-
-    /// <summary>
-    /// The page_ load.
-    /// </summary>
-    /// <param name="sender">
-    /// The sender.
-    /// </param>
-    /// <param name="e">
-    /// The e.
-    /// </param>
-    protected void Page_Load([NotNull] object sender, [NotNull] EventArgs e)
-    {
-      this.AdMessage.Message = this.Get<YafBoardSettings>().AdPost;
-      this.AdMessage.Signature = this.GetText("AD_SIGNATURE");
-
-      MessageFlags messageFlags = this.AdMessage.MessageFlags;
-      messageFlags.IsLocked = true;
-      messageFlags.NotFormatted = true;
-      this.AdMessage.MessageFlags = messageFlags;
-    }
-
-    #endregion
-  }
 }
