513d42c037b4462ddef8907a49b17b318ed3bde9
Jaben <jaben@yetanotherforum.net>
2013-12-16 10:13:46 -0500

    Improved send mail using .NET v4.0 conventions (dispose pattern -- also not creating multiple smtp clients per send).

--- yafsrc/YAF.Core/Services/YafSendMail.cs
+++ yafsrc/YAF.Core/Services/YafSendMail.cs
@@ -19,73 +19,80 @@
 
 namespace YAF.Core.Services
 {
-  #region Using
+    #region Using
 
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Net.Mail;
-  using System.Threading;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Mail;
 
-  using YAF.Core.Extensions;
-  using YAF.Types;
-  using YAF.Types.Interfaces;
-
-  #endregion
-
-  /// <summary>
-  /// Functions to send email via SMTP
-  /// </summary>
-  public class YafSendMail : ISendMail
-  {
-    #region Public Methods
-
-    /// <summary>
-    /// Sends all MailMessages via the SmtpClient. Doesn't handle any exceptions.
-    /// </summary>
-    /// <param name="messages">
-    /// The messages.
-    /// </param>
-    public void SendAll([NotNull] IEnumerable<MailMessage> messages)
-    {
-      CodeContracts.VerifyNotNull(messages, "messages");
-
-      messages.ToList().ForEach(m => m.Send());
-    }
-
-    /// <summary>
-    /// The send all isolated.
-    /// </summary>
-    /// <param name="messages">
-    /// The messages.
-    /// </param>
-    /// <param name="handleExceptionAction">
-    /// The handle exception action.
-    /// </param>
-    public void SendAllIsolated([NotNull] IEnumerable<MailMessage> messages, [CanBeNull] Action<MailMessage, Exception> handleExceptionAction)
-    {
-      CodeContracts.VerifyNotNull(messages, "messages");
-
-      foreach (var message in messages.ToList())
-      {
-        try
-        {
-          // send the message...
-          message.Send();
-
-          // sleep for a 1/20 of a second...
-          Thread.Sleep(50);
-        }
-        catch (Exception ex)
-        {
-          if (handleExceptionAction != null)
-          {
-            handleExceptionAction(message, ex);
-          }
-        }
-      }
-    }
+    using YAF.Classes;
+    using YAF.Types;
+    using YAF.Types.Interfaces;
 
     #endregion
-  }
+
+    /// <summary>
+    ///     Functions to send email via SMTP
+    /// </summary>
+    public class YafSendMail : ISendMail
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Sends all MailMessages via the SmtpClient. Doesn't handle any exceptions.
+        /// </summary>
+        /// <param name="messages">
+        ///     The messages.
+        /// </param>
+        public void SendAll([NotNull] IEnumerable<MailMessage> messages)
+        {
+            CodeContracts.VerifyNotNull(messages, "messages");
+
+            using (var smtpClient = new SmtpClient { EnableSsl = Config.UseSMTPSSL })
+            {
+                // send the message...
+                foreach (var m in messages.ToList())
+                {
+                    smtpClient.Send(m);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     The send all isolated.
+        /// </summary>
+        /// <param name="messages">
+        ///     The messages.
+        /// </param>
+        /// <param name="handleExceptionAction">
+        ///     The handle exception action.
+        /// </param>
+        public void SendAllIsolated([NotNull] IEnumerable<MailMessage> messages, [CanBeNull] Action<MailMessage, Exception> handleExceptionAction)
+        {
+            CodeContracts.VerifyNotNull(messages, "messages");
+
+            using (var smtpClient = new SmtpClient { EnableSsl = Config.UseSMTPSSL })
+            {
+                // send the message...
+                foreach (var m in messages.ToList())
+                {
+                    try
+                    {
+                        // send the message...
+                        smtpClient.Send(m);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (handleExceptionAction != null)
+                        {
+                            handleExceptionAction(m, ex);
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
 }
