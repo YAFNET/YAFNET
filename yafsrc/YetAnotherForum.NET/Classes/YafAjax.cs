05046836e496fe1d4dbdf0ba88c878146092c71c
Ingo Herbote <thewatcher@watchersnet.de>
2014-04-05 18:09:04 +0200

    fixed #125 username encoding in the thanks list

--- yafsrc/YetAnotherForum.NET/Classes/YafAjax.cs
+++ yafsrc/YetAnotherForum.NET/Classes/YafAjax.cs
@@ -30,7 +30,6 @@ namespace YAF.Classes
     using System.Collections.Generic;
     using System.Data;
     using System.Linq;
-    using System.Web;
     using System.Web.Script.Services;
     using System.Web.Security;
     using System.Web.Services;
@@ -42,6 +41,7 @@ namespace YAF.Classes
     using YAF.Types.Constants;
     using YAF.Types.Extensions;
     using YAF.Types.Interfaces;
+    using YAF.Utils.Helpers.StringUtils;
 
     #endregion
 
@@ -226,13 +226,15 @@ namespace YAF.Classes
 
             var username =
                 LegacyDb.message_AddThanks(
-                    UserMembershipHelper.GetUserIDFromProviderUserKey(membershipUser.ProviderUserKey), messageId, this.Get<YafBoardSettings>().EnableDisplayName);
+                    UserMembershipHelper.GetUserIDFromProviderUserKey(membershipUser.ProviderUserKey),
+                    messageId,
+                    this.Get<YafBoardSettings>().EnableDisplayName);
 
             // if the user is empty, return a null object...
             return username.IsNotSet()
                        ? null
                        : YafThankYou.CreateThankYou(
-                           this.Get<HttpServerUtilityBase>().HtmlEncode(username),
+                           new UnicodeEncoder().XSSEncode(username),
                            "BUTTON_THANKSDELETE",
                            "BUTTON_THANKSDELETE_TT",
                            messageId);
