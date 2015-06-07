63ac31c26cf29c416033341507771f4b14831e2d
watcher <thewwatcher@watchersnet.de>
2015-06-07 06:24:53 +0200

    added missing contenttype to ajax call

--- yafsrc/YetAnotherForum.NET/Classes/Utilities/JavaScriptBlocks.cs
+++ yafsrc/YetAnotherForum.NET/Classes/Utilities/JavaScriptBlocks.cs
@@ -912,6 +912,7 @@ namespace YAF.Utilities
                 url: '{2}YafAjax.asmx/GetTopics',
                 type: 'POST',
                 dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
                 minimumInputLength: 0,
                 data: function(params) {{
                     return {{
