b928a11d7e9aa01ef4bdf5a039ed74c533b353dc
Ingo Herbote <thewatcher@watchersnet.de>
2016-02-07 15:48:41 +0100

    Updated Copyright

--- yafsrc/YAF.Types/Extensions/DataTableExtensions.cs
+++ yafsrc/YAF.Types/Extensions/DataTableExtensions.cs
@@ -1,7 +1,7 @@
 ﻿/* Yet Another Forum.NET
  * Copyright (C) 2003-2005 Bjørnar Henden
  * Copyright (C) 2006-2013 Jaben Cargman
- * Copyright (C) 2014-2015 Ingo Herbote
+ * Copyright (C) 2014-2016 Ingo Herbote
  * http://www.yetanotherforum.net/
  * 
  * Licensed to the Apache Software Foundation (ASF) under one
