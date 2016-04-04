e9ce16d0fde6464214b259380accca8ad447fe5d
Ingo Herbote <thewatcher@watchersnet.de>
2016-04-04 11:04:14 +0200

    Switched from LESS to SASS

--- yafsrc/GlobalAssemblyInfo.cs
+++ yafsrc/GlobalAssemblyInfo.cs
@@ -26,4 +26,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("2.2.3.0")]
 [assembly: AssemblyFileVersion("2.2.3.0")]
-[assembly: AssemblyCopyright("Copyright © 2006-2015 YetAnotherForum.NET")]
+[assembly: AssemblyCopyright("Copyright © YetAnotherForum.NET")]
