cedba0cd07bac931e00759be3c8749e37c9d609d
w8tcha <thewatcher@watchersnet.de>
2018-01-10 10:05:14 +0100

    updated batch files

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -4,5 +4,5 @@
 @SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
 @SET LANGDIR=EN
 
-.nuget\nuget.exe restore YetAnotherForum.NET.sln
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" YetAnotherForum.NET.sln /p:Configuration=Release /p:Platform="Any CPU" /p:WarningLevel=0;CreatePackages=true
+.nuget\nuget.exe restore YAF.NET.sln
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" YAF.NET.sln /p:Configuration=Release /p:Platform="Any CPU" /p:WarningLevel=0;CreatePackages=true
