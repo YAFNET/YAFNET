c9f71c584ba4c1f8d5f8a0f8ad79d72f4cf03c4a
w8tcha <thewatcher@watchersnet.de>
2018-07-22 00:52:07 +0200

    bump to 2.24.13

--- yafsrc/GlobalAssemblyInfo.cs
+++ yafsrc/GlobalAssemblyInfo.cs
@@ -24,6 +24,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.2.4.12")]
-[assembly: AssemblyFileVersion("2.2.4.12")]
+[assembly: AssemblyVersion("2.2.4.13")]
+[assembly: AssemblyFileVersion("2.2.4.13")]
 [assembly: AssemblyCopyright("Copyright © YetAnotherForum.NET")]
--- yafsrc/YAF.Utils/YafForumInfo.cs
+++ yafsrc/YAF.Utils/YafForumInfo.cs
@@ -122,7 +122,7 @@ namespace YAF.Utils
                 const int Major = 2;
                 const byte Minor = 2;
                 const byte Build = 4;
-                const byte Sub = 12;
+                const byte Sub = 13;
 
                 const ReleaseType ReleaseType = ReleaseType.Regular;
                 const byte ReleaseNumber = 0;
@@ -149,7 +149,7 @@ namespace YAF.Utils
         /// <summary>
         /// Gets the Current YAF Build Date
         /// </summary>
-        public static DateTime AppVersionDate => new DateTime(2018, 05, 20);
+        public static DateTime AppVersionDate => new DateTime(2018, 07, 22);
 
         /// <summary>
         /// Creates a string that is the YAF Application Version from a long value
