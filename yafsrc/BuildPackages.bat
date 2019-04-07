6fc873980fec20ec52b77ebd765b67d6b945ccb2
w8tcha <thewatcher@watchersnet.de>
2019-04-07 19:31:19 +0200

    upgraded buildscripts

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -1,7 +1,7 @@
 @SET FrameworkSDKDir=
 @SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
 @SET LANGDIR=EN
-@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
 @SET CONFIGURATION=Release
 
 %MSBUILDPATH%  YAF.NET.sln /t:restore /p:Configuration=Release /p:Platform="Any CPU" /p:WarningLevel=0;CreatePackages=true
