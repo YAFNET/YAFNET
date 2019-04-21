2bfd6a6410d45731074775d77c2a3d58057eb141
w8tcha <thewatcher@watchersnet.de>
2019-04-21 04:45:55 +0200

    upgraded

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -1,6 +1,6 @@
 @SET FrameworkSDKDir=
 @SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
 @SET LANGDIR=EN
-@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
 
 %MSBUILDPATH% YetAnotherForum.NET.sln /p:Configuration=Release /p:Platform="Any CPU" /p:WarningLevel=0;CreatePackages=true
--- yafsrc/build.bat
+++ yafsrc/build.bat
@@ -1,8 +1,7 @@
 @SET FrameworkSDKDir=
 @SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
 @SET LANGDIR=EN
-@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
 @SET CONFIGURATION=Release
 
-
-%MSBUILDPATH% YetAnotherForum.NET.sln /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+%MSBUILDPATH% YetAnotherForum.NET.sln /t:restore /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
