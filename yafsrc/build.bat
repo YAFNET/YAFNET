0f47bc8267ae108de6630b59013d78db664146ee
w8tcha <thewatcher@watchersnet.de>
2019-04-21 04:49:06 +0200

    Update build.bat

--- yafsrc/build.bat
+++ yafsrc/build.bat
@@ -4,4 +4,4 @@
 @SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
 @SET CONFIGURATION=Release
 
-%MSBUILDPATH% YetAnotherForum.NET.sln /t:restore /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+%MSBUILDPATH% YetAnotherForum.NET.sln /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
