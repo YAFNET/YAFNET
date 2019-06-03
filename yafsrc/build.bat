9ea6d38623b89d80d995d28447db8fe69daaa3f1
w8tcha <thewatcher@watchersnet.de>
2019-06-03 20:44:48 +0200

    updated msbuild path

--- yafsrc/build.bat
+++ yafsrc/build.bat
@@ -1,7 +1,16 @@
-@SET FrameworkSDKDir=
-@SET PATH=%FrameworkDir%;%FrameworkSDKDir%;%PATH%
-@SET LANGDIR=EN
-@SET MSBUILDPATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MsBuild.exe"
 @SET CONFIGURATION=Release
 
-%MSBUILDPATH% YAF.NET.sln /t:restore /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -nologo -latest -property installationPath > temp.txt
+set /p $MSBUILDROOT=<temp.txt
+del temp.txt
+
+"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationVersion > temp.txt
+set /p $MSBUILDVER=<temp.txt
+del temp.txt
+
+@set $MSBUILDPATH="%$MSBUILDROOT%\MsBuild\Current\Bin\MSBuild.exe"
+
+%$MSBUILDPATH% YAF.NET.sln /t:restore /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+
+
