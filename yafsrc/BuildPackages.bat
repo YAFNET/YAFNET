4c9ccd5a8595ebf23af6d6a3908db7fea2d5b290
AlbertoP57 <alberto.pasqualini@outlook.com>
2019-06-13 08:43:43 +0200

    batch files proposal

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -6,8 +6,12 @@ del temp.txt
 
 "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationVersion > temp.txt
 set /p $MSBUILDVER=<temp.txt
-for /f "tokens=1 delims=." %%G in (temp.txt) do set Current=%%G.0
 del temp.txt
+
+for /f "tokens=1 delims=." %%G in ("%$MSBUILDVER%") do set Current=%%G.0
+Rem VS2017 => ~\MSBuild\15.0\Bin\MSBuild.exe
+Rem VS2019 => ~\MsBuild\Current\Bin\MSBuild.exe
+If "%Current%" NEQ "15.0" set Current=Current
 
 @set $MSBUILDPATH="%$MSBUILDROOT%\MsBuild\%Current%\Bin\MSBuild.exe"
 
