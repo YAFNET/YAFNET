004e21a158d3367277fca922ce690fc6f46aed61
w8tcha <thewatcher@watchersnet.de>
2019-08-11 04:29:26 +0200

    updated buildscripts

--- yafsrc/YAF.Utils/YafForumInfo.cs
+++ yafsrc/YAF.Utils/YafForumInfo.cs
@@ -121,7 +121,7 @@ namespace YAF.Utils
         /// <summary>
         /// Gets the Current YAF Build Date
         /// </summary>
-        public static DateTime AppVersionDate => new DateTime(2019, 08, 04);
+        public static DateTime AppVersionDate => new DateTime(2019, 08, 11);
 
         /// <summary>
         /// Creates a string that is the YAF Application Version from a long value
