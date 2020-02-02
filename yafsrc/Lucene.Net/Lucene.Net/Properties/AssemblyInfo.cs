0ac8ac176b8171adbc8bc1dcaed6511cbb761da1
w8tcha <thewatcher@watchersnet.de>
2020-02-02 12:41:34 +0100

    updated lucene

--- yafsrc/Lucene.Net/Lucene.Net/Properties/AssemblyInfo.cs
+++ yafsrc/Lucene.Net/Lucene.Net/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  * 
- * https://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,11 @@
  * limitations under the License.
  */
 
+using YAF.Lucene.Net.Support;
 using System;
+using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 //
 // General Information about an assembly is controlled through the following 
