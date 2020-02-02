7af0dc1e200ba5b960c4cdd14003fa3272d13d7d
w8tcha <thewatcher@watchersnet.de>
2020-02-02 15:08:44 +0100

    bump build date

--- yafsrc/YAF.Utils/BoardInfo.cs
+++ yafsrc/YAF.Utils/BoardInfo.cs
@@ -121,7 +121,7 @@ namespace YAF.Utils
         /// <summary>
         /// Gets the Current YAF Build Date
         /// </summary>
-        public static DateTime AppVersionDate => new DateTime(2020, 01, 26, 02, 30, 00);
+        public static DateTime AppVersionDate => new DateTime(2020, 02, 02, 15, 08, 00);
 
         /// <summary>
         /// Creates a string that is the YAF Application Version from a long value
