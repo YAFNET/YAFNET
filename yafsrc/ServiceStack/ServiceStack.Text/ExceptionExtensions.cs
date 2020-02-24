6d68c5799703c81761a22e841b18ad41f41a6916
w8tcha <thewatcher@watchersnet.de>
2020-02-24 04:15:03 +0100

    [FIXED #531]

---
+++ yafsrc/ServiceStack/ServiceStack.Text/ExceptionExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) ServiceStack, Inc. All Rights Reserved.
+// License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+using System;
+
+namespace ServiceStack
+{
+    public static class ExceptionExtensions
+    {
+        public static Exception GetInnerMostException(this Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
+        }
+    }
+}
