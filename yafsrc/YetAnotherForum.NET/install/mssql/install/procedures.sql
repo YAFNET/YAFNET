01daa6eadd92aad154ce9281221e84625fbaff20
w8tcha <thewatcher@watchersnet.de>
2020-04-05 10:56:28 +0200

    [FIXED] topic deleting

--- yafsrc/YetAnotherForum.NET/install/mssql/install/procedures.sql
+++ yafsrc/YetAnotherForum.NET/install/mssql/install/procedures.sql
@@ -3902,7 +3902,6 @@ BEGIN
 
         DELETE FROM  [{databaseOwner}].[{objectQualifier}topic] WHERE TopicMovedID = @TopicID
 
-        delete [{databaseOwner}].[{objectQualifier}Activity] where TopicID = @TopicID
         DELETE  [{databaseOwner}].[{objectQualifier}Attachment] WHERE MessageID IN (SELECT MessageID FROM  [{databaseOwner}].[{objectQualifier}message] WHERE TopicID = @TopicID)
         DELETE  [{databaseOwner}].[{objectQualifier}MessageHistory] WHERE MessageID IN (SELECT MessageID FROM  [{databaseOwner}].[{objectQualifier}message] WHERE TopicID = @TopicID)
 
@@ -3946,6 +3945,8 @@ BEGIN
 
 		EXEC [{databaseOwner}].[{objectQualifier}pollgroup_remove] @pollID, @TopicID, null, null, null, 0, 0
 
+        delete [{databaseOwner}].[{objectQualifier}TopicTag] where TopicID = @TopicID
+		delete [{databaseOwner}].[{objectQualifier}Activity] where TopicID = @TopicID
         DELETE  [{databaseOwner}].[{objectQualifier}WatchTopic] WHERE TopicID = @TopicID
         DELETE  [{databaseOwner}].[{objectQualifier}TopicReadTracking] WHERE TopicID = @TopicID
         DELETE  [{databaseOwner}].[{objectQualifier}FavoriteTopic]  WHERE TopicID = @TopicID
