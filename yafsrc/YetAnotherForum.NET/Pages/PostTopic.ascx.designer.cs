cbdb9567db64f0474129a096b8cc207e503df094
w8tcha <thewatcher@watchersnet.de>
2020-04-09 05:32:25 +0200

    removed not needes css

--- yafsrc/YetAnotherForum.NET/Pages/PostTopic.ascx.designer.cs
+++ yafsrc/YetAnotherForum.NET/Pages/PostTopic.ascx.designer.cs
@@ -303,15 +303,6 @@ namespace YAF.Pages
         protected global::YAF.Web.Controls.ThemeButton Cancel;
 
         /// <summary>
-        /// LastPosts1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::YAF.Controls.LastPosts LastPosts1;
-
-        /// <summary>
         /// UploadDialog control.
         /// </summary>
         /// <remarks>
