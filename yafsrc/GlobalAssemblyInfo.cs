bc56de44a7ad0ef49d3240492858386449ca8702
w8tcha <thewatcher@watchersnet.de>
2020-06-02 03:41:15 +0200

    Bump version to 2.31.6

--- yafsrc/GlobalAssemblyInfo.cs
+++ yafsrc/GlobalAssemblyInfo.cs
@@ -24,6 +24,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.1.5")]
-[assembly: AssemblyFileVersion("2.3.1.5")]
+[assembly: AssemblyVersion("2.3.1.6")]
+[assembly: AssemblyFileVersion("2.3.1.6")]
 [assembly: AssemblyCopyright("Copyright © YetAnotherForum.NET")]
