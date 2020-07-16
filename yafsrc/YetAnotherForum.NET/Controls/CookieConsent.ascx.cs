e36b15eb8a004f30788603de7fa537305f7dff53
w8tcha <thewatcher@watchersnet.de>
2020-07-16 15:30:45 +0200

    refresh after cookie set

--- yafsrc/YetAnotherForum.NET/Controls/CookieConsent.ascx.cs
+++ yafsrc/YetAnotherForum.NET/Controls/CookieConsent.ascx.cs
@@ -68,7 +68,8 @@ namespace YAF.Controls
             this.PageContext.Get<HttpResponseBase>().SetCookie(
                 new HttpCookie("YAF-AcceptCookies", "true") { Expires = DateTime.UtcNow.AddYears(1) });
 
-            //TODO:Reload Page
+            this.Response.Redirect(this.Request.RawUrl);
+
             this.DataBind();
         }
     }
