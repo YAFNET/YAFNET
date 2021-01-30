54d2fbd637cb5eadc6457fe3ddfdf33125af9012
w8tcha <thewatcher@watchersnet.de>
2021-01-30 02:57:11 +0100

    [FIXED #603] subforums are not saved

--- yafsrc/YAF.Core/Model/ForumRepositoryExtensions.cs
+++ yafsrc/YAF.Core/Model/ForumRepositoryExtensions.cs
@@ -1,7 +1,7 @@
 /* Yet Another Forum.NET
  * Copyright (C) 2003-2005 Bjørnar Henden
  * Copyright (C) 2006-2013 Jaben Cargman
- * Copyright (C) 2014-2020 Ingo Herbote
+ * Copyright (C) 2014-2021 Ingo Herbote
  * https://www.yetanotherforum.net/
  * 
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -140,6 +140,7 @@ namespace YAF.Core.Model
                 return repository.Insert(
                     new Forum
                         {
+                            ParentID = parentID,
                             Name = name,
                             Description = description,
                             SortOrder = sortOrder,
