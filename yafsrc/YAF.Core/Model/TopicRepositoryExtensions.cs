81471651158d954ee547b5f97ce6a76148f5b50c
Ingo <thewatcher@watchersnet.de>
2022-03-04 12:27:33 +0100

    Update TopicRepositoryExtensions.cs

--- yafsrc/YAF.Core/Model/TopicRepositoryExtensions.cs
+++ yafsrc/YAF.Core/Model/TopicRepositoryExtensions.cs
@@ -184,7 +184,7 @@ namespace YAF.Core.Model
         }
 
         /// <summary>
-        /// The unanswered as data table.
+        /// Returns topics which have no replies.
         /// </summary>
         /// <param name="repository">
         /// The repository.
@@ -208,7 +208,7 @@ namespace YAF.Core.Model
         /// Indicates if the Table should contain the last Access Date
         /// </param>
         /// <returns>
-        /// Returns the List with the Topics Unanswered
+        /// Returns the List with the Topics
         /// </returns>
         public static List<PagedTopic> ListUnansweredPaged(
             this IRepository<Topic> repository,
@@ -232,7 +232,7 @@ namespace YAF.Core.Model
         }
 
         /// <summary>
-        /// Returns Topics Unread by a user
+        /// Lists the Active Topics
         /// </summary>
         /// <param name="repository">
         /// The repository.
@@ -256,7 +256,7 @@ namespace YAF.Core.Model
         /// Indicates if the Table should contain the last Access Date
         /// </param>
         /// <returns>
-        /// Returns the List with the Topics Unread be a PageUserId
+        /// Returns the the Active Topics
         /// </returns>
         public static List<PagedTopic> ListActivePaged(
             this IRepository<Topic> repository,
@@ -279,13 +279,13 @@ namespace YAF.Core.Model
         }
 
         /// <summary>
-        /// Returns Topics Unread by a user
+        /// List all Topics Watched by a user
         /// </summary>
         /// <param name="repository">
         /// The repository.
         /// </param>
-        /// <param name="pageUserId">
-        /// The page user id.
+        /// <param name="userId">
+        /// The user id.
         /// </param>
         /// <param name="sinceDate">
         /// The since Date.
@@ -303,11 +303,11 @@ namespace YAF.Core.Model
         /// Indicates if the Table should contain the last Access Date
         /// </param>
         /// <returns>
-        /// Returns the List with the Topics Unread be a PageUserId
+        /// Returns the List with the Topics Watched by a user
         /// </returns>
-        public static List<PagedTopic> ListUnreadPaged(
+        public static List<PagedTopic> ListWatchedPaged(
             this IRepository<Topic> repository,
-            [NotNull] int pageUserId,
+            [NotNull] int userId,
             [NotNull] DateTime sinceDate,
             [NotNull] DateTime toDate,
             [NotNull] int pageIndex,
@@ -316,13 +316,134 @@ namespace YAF.Core.Model
         {
             CodeContracts.VerifyNotNull(repository);
 
-            return repository.ListPaged(
-                pageUserId,
-                pageIndex,
-                pageSize,
-                findLastRead,
-                (t, x, c) => x.UserID == pageUserId && x.ReadAccess &&
-                             (t.Flags & 8) != 8 && t.TopicMovedID == null && t.LastPosted != null && t.LastPosted > sinceDate && t.LastPosted < toDate);
+            Expression<Func<Topic, bool>> whereCriteria = t =>
+                (t.Flags & 8) != 8 && t.TopicMovedID == null && t.LastPosted != null && t.LastPosted > sinceDate
+                && t.LastPosted < toDate;
+
+            var expression = OrmLiteConfig.DialectProvider.SqlExpression<WatchTopic>();
+
+            return repository.DbAccess.Execute(
+                db =>
+                {
+                    expression.Join<User>((t, u) => u.ID == t.UserID)
+                        .Join<Topic>((a, b) => b.ID == a.TopicID).Join<Topic, Forum>((t, f) => f.ID == t.ForumID)
+                        .Where<WatchTopic>(b => b.UserID == userId);
+
+                    expression.Where(whereCriteria);
+
+                    // -- count total
+                    var countTotalExpression = db.Connection.From<Topic>().Join<Forum>((t, f) => f.ID == t.ForumID)
+                        .Join<Forum, ActiveAccess>((f, x) => x.ForumID == f.ID)
+                        .Join<Forum, Category>((f, c) => c.ID == f.CategoryID);
+
+                    countTotalExpression.Where(whereCriteria);
+
+                    expression.OrderByDescending<Topic>(t => t.LastPosted).Page(pageIndex + 1, pageSize);
+
+                    var countTotalSql = countTotalExpression
+                        .Select(Sql.Count($"{countTotalExpression.Column<Topic>(x => x.ID)}")).ToSelectStatement();
+
+                    // -- count deleted posts
+                    var countDeletedExpression = db.Connection.From<Message>(db.Connection.TableAlias("mes"));
+                    countDeletedExpression.Where(
+                        $@"mes.{countDeletedExpression.Column<Message>(x => x.TopicID)}={expression.Column<Topic>(x => x.ID, true)}
+                                    and (mes.{countDeletedExpression.Column<Message>(x => x.Flags)} & 8) = 8
+                                    and mes.{countDeletedExpression.Column<Message>(x => x.UserID)}={userId}");
+                    var countDeletedSql = countDeletedExpression.Select(Sql.Count("1")).ToSelectStatement();
+
+                    var lastTopicAccessSql = "NULL";
+                    var lastForumAccessSql = "NULL";
+
+                    if (findLastRead)
+                    {
+                        var topicAccessExpression =
+                            db.Connection.From<TopicReadTracking>(db.Connection.TableAlias("y"));
+                        topicAccessExpression.Where(
+                            $@"y.{topicAccessExpression.Column<TopicReadTracking>(y => y.TopicID)}={expression.Column<Topic>(x => x.ID, true)}
+                                    and y.{topicAccessExpression.Column<TopicReadTracking>(y => y.UserID)}={userId}");
+                        lastTopicAccessSql = topicAccessExpression.Select(
+                                $"{topicAccessExpression.Column<TopicReadTracking>(x => x.LastAccessDate)}").Limit(1)
+                            .ToSelectStatement();
+
+                        var forumAccessExpression =
+                            db.Connection.From<ForumReadTracking>(db.Connection.TableAlias("x"));
+                        forumAccessExpression.Where(
+                            $@"x.{forumAccessExpression.Column<ForumReadTracking>(x => x.ForumID)}={expression.Column<Topic>(x => x.ForumID, true)}
+                                    and x.{forumAccessExpression.Column<ForumReadTracking>(x => x.UserID)}={userId}");
+                        lastForumAccessSql = forumAccessExpression.Select(
+                                $"{forumAccessExpression.Column<ForumReadTracking>(x => x.LastAccessDate)}").Limit(1)
+                            .ToSelectStatement();
+                    }
+
+                    // -- last user
+                    var lastUserNameExpression = db.Connection.From<User>(db.Connection.TableAlias("usr"));
+                    lastUserNameExpression.Where(
+                        $@"usr.{lastUserNameExpression.Column<User>(x => x.ID)}=
+                                   {expression.Column<Topic>(x => x.LastUserID, true)}");
+                    var lastUserNameSql = lastUserNameExpression.Select(
+                        $"{lastUserNameExpression.Column<User>(x => x.Name)}").Limit(1).ToSelectStatement();
+
+                    var lastUserDisplayNameSql = lastUserNameExpression.Select(
+                        $"{lastUserNameExpression.Column<User>(x => x.DisplayName)}").Limit(1).ToSelectStatement();
+
+                    var lastUserSuspendedSql = lastUserNameExpression.Select(
+                        $"{lastUserNameExpression.Column<User>(x => x.Suspended)}").Limit(1).ToSelectStatement();
+
+                    var lastUserStyleSql = lastUserNameExpression.Select(
+                        $"{lastUserNameExpression.Column<User>(x => x.UserStyle)}").Limit(1).ToSelectStatement();
+
+                    // -- first message
+                    var firstMessageExpression = db.Connection.From<Message>(db.Connection.TableAlias("fm"));
+                    firstMessageExpression.Where(
+                        $@"fm.{firstMessageExpression.Column<Message>(x => x.TopicID)}=
+                                   {OrmLiteConfig.DialectProvider.IsNullFunction(expression.Column<Topic>(x => x.TopicMovedID, true), expression.Column<Topic>(x => x.ID, true))}
+                                   and fm.{firstMessageExpression.Column<Message>(x => x.Position)} = 0");
+                    var firstMessageSql = firstMessageExpression.Select(
+                        $"{firstMessageExpression.Column<Message>(x => x.MessageText)}").Limit(1).ToSelectStatement();
+
+                    expression.Select<Topic, User, Forum>(
+                        (c, b, d) => new
+                        {
+                            ForumID = d.ID,
+                            TopicID = c.ID,
+                            c.Posted,
+                            LinkTopicID = c.TopicMovedID != null ? c.TopicMovedID : c.ID,
+                            c.TopicMovedID,
+                            Subject = c.TopicName,
+                            c.Description,
+                            c.Status,
+                            c.Styles,
+                            c.UserID,
+                            Starter = c.UserName != null ? c.UserName : b.Name,
+                            StarterDisplay = c.UserDisplayName != null ? c.UserDisplayName : b.DisplayName,
+                            Replies = c.NumPosts - 1,
+                            NumPostsDeleted = Sql.Custom($"({countDeletedSql})"),
+                            c.Views,
+                            c.LastPosted,
+                            c.LastUserID,
+                            LastUserName = Sql.Custom($"({lastUserNameSql})"),
+                            LastUserDisplayName = Sql.Custom($"({lastUserDisplayNameSql})"),
+                            LastUserSuspended = Sql.Custom($"({lastUserSuspendedSql})"),
+                            LastUserStyle = Sql.Custom($"({lastUserStyleSql})"),
+                            c.LastMessageFlags,
+                            c.LastMessageID,
+                            LastTopicID = c.ID,
+                            c.LinkDate,
+                            TopicFlags = c.Flags,
+                            c.Priority,
+                            c.PollID,
+                            ForumFlags = d.Flags,
+                            FirstMessage = Sql.Custom($"({firstMessageSql})"),
+                            StarterStyle = b.UserStyle,
+                            StarterSuspended = b.Suspended,
+                            LastForumAccess = Sql.Custom($"({lastForumAccessSql})"),
+                            LastTopicAccess = Sql.Custom($"({lastTopicAccessSql})"),
+                            c.TopicImage,
+                            TotalRows = Sql.Custom($"({countTotalSql})")
+                        });
+
+                    return db.Connection.Select<PagedTopic>(expression);
+                });
         }
 
         /// <summary>
