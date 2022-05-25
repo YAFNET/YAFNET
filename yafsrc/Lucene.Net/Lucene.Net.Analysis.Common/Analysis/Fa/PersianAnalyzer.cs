505faeaad373354ad5601c6a0a9ed2c857d0e237
Ingo <thewatcher@watchersnet.de>
2022-05-25 13:12:20 +0200

    update

--- yafsrc/Lucene.Net/Lucene.Net.Analysis.Common/Analysis/Fa/PersianAnalyzer.cs
+++ yafsrc/Lucene.Net/Lucene.Net.Analysis.Common/Analysis/Fa/PersianAnalyzer.cs
@@ -9,6 +9,8 @@ using System.IO;
 
 namespace YAF.Lucene.Net.Analysis.Fa
 {
+    using YAF.Lucene.Net.Analysis.Miscellaneous;
+
     /*
      * Licensed to the Apache Software Foundation (ASF) under one or more
      * contributor license agreements.  See the NOTICE file distributed with
