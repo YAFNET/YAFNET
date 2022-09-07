ed504d6e8245d965ffbbe4785360d2a750c07992
Ingo <thewatcher@watchersnet.de>
2022-09-07 08:51:18 +0200

    updated ss

--- yafsrc/ServiceStack/ServiceStack.OrmLite/OrmLiteSchemaModifyApi.cs
+++ yafsrc/ServiceStack/ServiceStack.OrmLite/OrmLiteSchemaModifyApi.cs
@@ -113,6 +113,12 @@ public static class OrmLiteSchemaModifyApi
     {
         var modelDef = ModelDefinition<T>.Definition;
         var fieldDef = modelDef.GetFieldDefinition(field);
+
+        if (fieldDef.Name != OrmLiteConfig.IdField)
+        {
+            fieldDef.IsPrimaryKey = false;
+        }
+
         dbConn.AddColumn(typeof(T), fieldDef);
     }
 
@@ -237,7 +243,7 @@ public static class OrmLiteSchemaModifyApi
     {
         var modelDef = ModelDefinition<T>.Definition;
         var fieldDef = modelDef.GetFieldDefinition(field);
-        dbConn.RenameColumn(typeof(T), dbConn.GetNamingStrategy().GetColumnName(fieldDef.FieldName), oldColumn);
+        dbConn.RenameColumn(typeof(T), oldColumn, dbConn.GetNamingStrategy().GetColumnName(fieldDef.FieldName));
     }
 
     public static void RenameColumn<T>(this IDbConnection dbConn, string oldColumn, string newColumn) =>
