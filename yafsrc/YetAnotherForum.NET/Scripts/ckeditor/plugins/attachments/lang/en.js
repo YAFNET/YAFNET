2fee6dbb234d8bed5fa47a9965da6fe02e80f8a6
Ingo <thewatcher@watchersnet.de>
2022-09-21 09:57:15 +0200

    [FIXED #776] wrong language file

--- yafsrc/YetAnotherForum.NET/Scripts/ckeditor/plugins/attachments/lang/en.js
+++ yafsrc/YetAnotherForum.NET/Scripts/ckeditor/plugins/attachments/lang/en.js
@@ -1,5 +1,6 @@
-﻿CKEDITOR.plugins.setLang("albumsbrowser","en",{
-	title: "Select an Album Image",
-	loading: "Loading Images",
-	noAlbums: "You havent uploaded any User Albums"
+﻿CKEDITOR.plugins.setLang("attachments","en",{
+	title: "Select or Upload an Attachment",
+	loading: "Loading Attachments",
+	noAlbums: "You don't have any attachments yet, to add one click on the &lt;strong&gt;Upload New File(s)&lt;/strong&gt; button",
+	upload: "Upload New File(s)"
 });
