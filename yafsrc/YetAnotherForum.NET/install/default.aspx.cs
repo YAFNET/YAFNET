ee2b3a78a0c916d3a685a98be8c0dc6a8c8378fe
Ingo <thewatcher@watchersnet.de>
2023-01-01 10:23:55 +0100

    updated copyright

--- yafsrc/YetAnotherForum.NET/install/default.aspx.cs
+++ yafsrc/YetAnotherForum.NET/install/default.aspx.cs
@@ -1,7 +1,7 @@
 ﻿/* Yet Another Forum.NET
  * Copyright (C) 2003-2005 Bjørnar Henden
  * Copyright (C) 2006-2013 Jaben Cargman
- * Copyright (C) 2014-2022 Ingo Herbote
+ * Copyright (C) 2014-2023 Ingo Herbote
  * https://www.yetanotherforum.net/
  *
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -27,6 +27,7 @@ namespace YAF.Install;
 using System.Configuration;
 using System.Security.Permissions;
 using System.Web.UI;
+
 using YAF.Core.Context;
 using YAF.Types.Interfaces.Data;
 using YAF.Types.Models.Identity;
