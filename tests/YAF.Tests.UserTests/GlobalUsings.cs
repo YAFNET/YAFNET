76e3308dbc348d5ca3b38ae11eee686090a811e3
Ingo <thewatcher@watchersnet.de>
2023-01-01 11:15:36 +0100

    removed ui tests

--- tests/YAF.Tests.UserTests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Global using directives
-
-global using NUnit.Framework;
-
-global using OpenQA.Selenium;
-global using OpenQA.Selenium.Chrome;
-
-global using YAF.Tests.Utils;
-global using YAF.Tests.Utils.SetUp;
