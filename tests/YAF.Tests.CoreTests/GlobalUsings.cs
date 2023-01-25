0bea9c09891bac449f9989103914781f4fc109fe
Ingo <thewatcher@watchersnet.de>
2023-01-25 11:34:54 +0100

    updated tests

--- tests/YAF.Tests.CoreTests/GlobalUsings.cs
+++ tests/YAF.Tests.CoreTests/GlobalUsings.cs
@@ -1,15 +1,6 @@
 // Global using directives
 
-global using Autofac;
-
-global using HttpSimulator;
 
 global using NUnit.Framework;
 
-global using YAF.Core;
 global using YAF.Core.Helpers;
-global using YAF.Core.Utilities;
-global using YAF.Tests.Utils;
-global using YAF.Types.Attributes;
-global using YAF.Types.Interfaces;
-global using YAF.Types.Interfaces.Services;
