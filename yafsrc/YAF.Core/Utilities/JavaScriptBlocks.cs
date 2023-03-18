f1991c4661e1f3f6b8782bd5ea64d965d4c05e1e
Ingo <thewatcher@watchersnet.de>
2023-03-18 12:20:20 +0100

    [FIXED #885] removed not working textselection plugin

--- yafsrc/YAF.Core/Utilities/JavaScriptBlocks.cs
+++ yafsrc/YAF.Core/Utilities/JavaScriptBlocks.cs
@@ -584,7 +584,7 @@ function blurTextBox(txtTitleId, id, isAlbum) {{
 
         return $@"{Config.JQueryAlias}(document).ready(function() {{
                       var yafCKEditor = {Config.JQueryAlias}(""#{editorId}"").ckeditor({{
-                          extraPlugins: ""bbcode,mentions,highlight,bbcodeselector,bbcodeextensions,syntaxhighlight,emoji,wordcount,autolink,albumsbrowser,{(uploadAllowed ? "attachments," : "")}quote,codemirror,textselection"",
+                          extraPlugins: ""bbcode,mentions,highlight,bbcodeselector,bbcodeextensions,syntaxhighlight,emoji,wordcount,autolink,albumsbrowser,{(uploadAllowed ? "attachments," : "")}quote,codemirror"",
                           removePlugins: 'bidi,div,filebrowser,flash,format,forms,horizontalrule,iframe,liststyle,pagebreak,showborders,stylescombo,templates{contextMenuRemove}',
                           toolbar: [{toolbar}],
                           entities_greek: false,
