8ee45960a7d5dfe3c35fd311a232d12644215b7c
Ingo <thewatcher@watchersnet.de>
2023-05-01 03:48:47 +0200

    updated build date

--- yafsrc/YAF.Core/Services/BoardInfo.cs
+++ yafsrc/YAF.Core/Services/BoardInfo.cs
@@ -58,7 +58,7 @@ public class BoardInfo : IHaveServiceLocator
     /// <summary>
     /// Gets the Current YAF Build Date
     /// </summary>
-    public DateTime AppVersionDate => new(2023, 03,12, 08, 00, 00);
+    public DateTime AppVersionDate => new(2023, 05, 01, 03, 48, 00);
 
     /// <summary>
     /// Creates a string that is the YAF Application Version from a long value
