947b4688f4ae4b959cd1c4bc66f93ba5ca07eaee
Ingo <thewatcher@watchersnet.de>
2023-06-10 14:52:56 +0200

    [FIXED] user voting check

--- yafsrc/YAF.Core/Model/PollVoteRepositoryExtensions.cs
+++ yafsrc/YAF.Core/Model/PollVoteRepositoryExtensions.cs
@@ -1,4 +1,4 @@
-/* Yet Another Forum.NET
+﻿/* Yet Another Forum.NET
  * Copyright (C) 2003-2005 Bjørnar Henden
  * Copyright (C) 2006-2013 Jaben Cargman
  * Copyright (C) 2014-2023 Ingo Herbote
@@ -56,7 +56,7 @@ public static class PollVoteRepositoryExtensions
     {
         CodeContracts.VerifyNotNull(repository);
 
-        return repository.Get(p => p.UserID == userId);
+        return repository.Get(p => p.UserID == userId && p.PollID == pollId);
     }
 
     /// <summary>
