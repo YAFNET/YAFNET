643fbaa988bf27a7496e16231a723b53d428d15d
Ingo <thewatcher@watchersnet.de>
2023-07-13 10:31:11 +0200

    updates

--- yafsrc/ServiceStack/ServiceStack.OrmLite/BulkInsertConfig.cs
+++ yafsrc/ServiceStack/ServiceStack.OrmLite/BulkInsertConfig.cs
@@ -11,20 +11,47 @@ using System.Collections.Generic;
 
 namespace ServiceStack.OrmLite;
 
+/// <summary>
+/// Enum BulkInsertMode
+/// </summary>
 public enum BulkInsertMode
 {
+    /// <summary>
+    /// The binary
+    /// </summary>
     Binary,
 
+    /// <summary>
+    /// The CSV
+    /// </summary>
     Csv,
 
+    /// <summary>
+    /// The SQL
+    /// </summary>
     Sql,
 }
 
+/// <summary>
+/// Class BulkInsertConfig.
+/// </summary>
 public class BulkInsertConfig
 {
+    /// <summary>
+    /// Gets or sets the size of the batch.
+    /// </summary>
+    /// <value>The size of the batch.</value>
     public int BatchSize { get; set; } = 1000;
 
+    /// <summary>
+    /// Gets or sets the mode.
+    /// </summary>
+    /// <value>The mode.</value>
     public BulkInsertMode Mode { get; set; } = BulkInsertMode.Csv;
 
+    /// <summary>
+    /// Gets or sets the insert fields.
+    /// </summary>
+    /// <value>The insert fields.</value>
     public ICollection<string>? InsertFields { get; set; } = null;
 }
