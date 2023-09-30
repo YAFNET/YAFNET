33a586f1bfa1a81a55bcc306a3fee65b51b7a9e7
Ingo <thewatcher@watchersnet.de>
2023-09-30 12:59:28 +0200

    converted the bootstrap-email converter to .NET

--- yafsrc/YetAnotherForum.NET/wwwroot/lib/themes/journal/bootstrap_email.config.rb
+++
@@ -1,4 +0,0 @@
-BootstrapEmail.configure do |config|
-   # Defaults to ./bootstrap-email.scss or ./app/assets/stylesheets/bootstrap-email.scss in rails
-  config.sass_email_location = File.expand_path('bootstrap-email.scss', __dir__)
-end
