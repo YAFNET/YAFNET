2b02f42271e3a473fd7de3695083ee6a905ea097
Ingo <thewatcher@watchersnet.de>
2024-02-23 06:37:44 +0100

    bump bootstrap to 5.3.3

--- yafsrc/YetAnotherForum.NET/Scripts/forumExtensions.js
+++ yafsrc/YetAnotherForum.NET/Scripts/forumExtensions.js
@@ -506,7 +506,7 @@
             }
         }
     }
-    const VERSION = "5.3.2";
+    const VERSION = "5.3.3";
     class BaseComponent extends Config {
         constructor(element, config) {
             super();
@@ -563,9 +563,9 @@
             if (hrefAttribute.includes("#") && !hrefAttribute.startsWith("#")) {
                 hrefAttribute = `#${hrefAttribute.split("#")[1]}`;
             }
-            selector = hrefAttribute && hrefAttribute !== "#" ? parseSelector(hrefAttribute.trim()) : null;
+            selector = hrefAttribute && hrefAttribute !== "#" ? hrefAttribute.trim() : null;
         }
-        return selector;
+        return selector ? selector.split(",").map(sel => parseSelector(sel)).join(",") : null;
     };
     const SelectorEngine = {
         find(selector, element = document.documentElement) {
@@ -3700,7 +3700,10 @@
         br: [],
         col: [],
         code: [],
+        dd: [],
         div: [],
+        dl: [],
+        dt: [],
         em: [],
         hr: [],
         h1: [],
