7d8d13be987e9e1642f4597fd4109a50b1bf1fba
Ingo <thewatcher@watchersnet.de>
2024-03-13 06:46:14 +0100

    [FIXED] send notifications only to approved and not deleted users

--- yafsrc/YAF.Core/Model/UserRepositoryExtensions.cs
+++ yafsrc/YAF.Core/Model/UserRepositoryExtensions.cs
@@ -723,14 +723,14 @@ public static class UserRepositoryExtensions
 
         expression.Join<User>((a, b) => b.ID == a.UserID).Where<WatchTopic, User>(
             (a, b) => b.ID != userId && b.NotificationType != 10 && b.NotificationType != 20 &&
-                      a.TopicID == topicId).Select<User>(x => x);
+                      a.TopicID == topicId && (b.Flags & 2) != 2 && (b.Flags & 32) != 32).Select<User>(x => x);
 
         var expression2 = OrmLiteConfig.DialectProvider.SqlExpression<WatchForum>();
 
         expression2.Join<User>((a, b) => b.ID == a.UserID).Join<Topic>((a, c) => c.ForumID == a.ForumID)
             .Where<WatchForum, User, Topic>(
                 (a, b, c) => b.ID != userId && b.NotificationType != 10 && b.NotificationType != 20 &&
-                             c.ID == topicId).Select<User>(x => x);
+                             c.ID == topicId && (b.Flags & 2) != 2 && (b.Flags & 32) != 32).Select<User>(x => x);
 
         return repository.DbAccess.Execute(
                 db => db.Connection.Select<User>(
