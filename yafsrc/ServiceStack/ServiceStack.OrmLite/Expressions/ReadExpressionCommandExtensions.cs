e33ebd66cdfb16ff276716197e1ce757c36a33fb
Ingo <thewatcher@watchersnet.de>
2024-06-29 01:28:06 +0200

    remove inconsistent limit for some Single queries

--- yafsrc/ServiceStack/ServiceStack.OrmLite/Expressions/ReadExpressionCommandExtensions.cs
+++ yafsrc/ServiceStack/ServiceStack.OrmLite/Expressions/ReadExpressionCommandExtensions.cs
@@ -359,7 +359,7 @@ static internal class ReadExpressionCommandExtensions
     /// <returns>T.</returns>
     static internal T Single<T>(this IDbCommand dbCmd, SqlExpression<T> q)
     {
-        string sql = q.Limit(1).SelectInto<T>(QueryType.Select);
+        var sql = q.SelectInto<T>(QueryType.Single);
 
         return dbCmd.ExprConvertTo<T>(sql, q.Params, onlyFields: q.OnlyFields);
     }
