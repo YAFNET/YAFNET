11419372d7f98d5316bdd876bfe6a9be795261fa
Ingo <thewatcher@watchersnet.de>
2025-08-02 13:19:24 +0200

    added lighthouse tests

---
+++ yafsrc/Tests/YAFNET.Tests.LighthouseTests/GlobalUsings.cs
@@ -0,0 +1,9 @@
+// Global using directives
+
+global using System;
+global using System.Threading.Tasks;
+
+global using NUnit.Framework;
+
+global using YAF.Tests.Utils;
+global using YAF.Tests.Utils.Extensions;
---
+++ yafsrc/Tests/YAFNET.Tests.LighthouseTests/PageTests.cs
@@ -0,0 +1,70 @@
+﻿/* Yet Another Forum.NET
+ * Copyright (C) 2003-2005 Bjørnar Henden
+ * Copyright (C) 2006-2013 Jaben Cargman
+ * Copyright (C) 2014-2025 Ingo Herbote
+ * https://www.yetanotherforum.net/
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using LighthousePlaywright.Net;
+using LighthousePlaywright.Net.Objects;
+
+namespace YAF.Tests.AdminTests;
+
+/// <summary>
+/// The pages test
+/// </summary>
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class PageTests : TestBase
+{
+    /// <summary>
+    /// Basic test to check if all admin pages load without error
+    /// </summary>
+    [Test]
+    public Task PagePerformanceTest()
+    {
+        return this.Base.PlaywrightFixture.GotoPageAsync(
+            this.Base.TestSettings.TestForumUrl,
+            async _ =>
+            {
+                var lh = new Lighthouse(new Options
+                {
+                    Reports = new Reports
+                    {
+                        Formats = new Formats
+                        {
+                            Html = true
+                        }
+                    }
+                });
+
+                var res = await lh.RunAsync($"{this.Base.TestSettings.TestForumUrl}");
+
+                Assert.That(res.Accessibility > 0.9m, Is.True, $"Value was: {res.Accessibility}");
+
+                Assert.That(res.BestPractices > 0.9m, Is.True, $"Value was: {res.BestPractices}");
+
+                Assert.That(res.Seo > 0.9m, Is.True, $"Value was: {res.Seo}");
+
+                Assert.That(res.Performance > 0.6m, Is.True, $"Value was: {res.Performance}");
+            },
+            this.BrowserType);
+    }
+}
