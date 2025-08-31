6a26611052cd9d06ef18baca73a855adb239d269
Ingo <thewatcher@watchersnet.de>
2025-08-31 07:55:56 +0200

    Update build.bat

--- yafsrc/build.bat
+++ yafsrc/build.bat
@@ -1,6 +1,6 @@
 dotnet restore ./yafsrc/YAF.NET-SqlServer.slnx
 
-dotnet build YAF.NET-SqlServer.slnx /p:Configuration=%CONFIGURATION% /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+dotnet build YAF.NET-SqlServer.slnx /p:Configuration=Release /p:Platform="Any CPU" /p:WarningLevel=0 /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 
 
