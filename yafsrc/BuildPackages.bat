8cba06a4ca67fc186627c486a4992bc3b11669c1
Ingo <thewatcher@watchersnet.de>
2025-09-15 06:36:53 +0200

    not needed

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -1,5 +1,3 @@
-dotnet restore YAF.NET-SqlServer.slnx
-
 dotnet build YAF.NET-MySql.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
 dotnet build YAF.NET-PostgreSQL.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
 dotnet build YAF.NET-Sqlite.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
