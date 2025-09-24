e08b82a72e2213a4a0d7b2a8ff51bd1676f0368e
Ingo <thewatcher@watchersnet.de>
2025-09-24 12:56:24 +0200

    fixed compile sqlite version

--- yafsrc/BuildPackages.bat
+++ yafsrc/BuildPackages.bat
@@ -5,7 +5,7 @@ dotnet clean YAF.NET-PostgreSQL.slnx
 dotnet build YAF.NET-PostgreSQL.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 dotnet clean YAF.NET-Sqlite.slnx
-dotnet build YAF.NET-Sqlite.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
+dotnet build YAF.NET-Sqlite.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly
 
 dotnet clean YAF.NET-SqlServer.slnx
 dotnet build YAF.NET-SqlServer.slnx /p:Configuration=Release /p:Platform="Any CPU" /t:Clean,Build /p:WarningLevel=0;CreatePackages=true /flp1:logfile=errors.txt;errorsonly %1 %2 %3 %4 %5 %6 %7 %8 %9
