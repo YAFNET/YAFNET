6b82b148a64effa2df5a625beebbd0c2158067db
Ingo <thewatcher@watchersnet.de>
2026-03-07 07:17:32 +0100

    added loading animation to the chat

--- yafsrc/YetAnotherForum.NET/wwwroot/lib/forum/notificationHub.ts
+++ yafsrc/YetAnotherForum.NET/wwwroot/lib/forum/notificationHub.ts
@@ -1,28 +1,30 @@
 ﻿import * as signalR from '@microsoft/signalr';
 
-const connection = new signalR.HubConnectionBuilder()
-	.withUrl('/NotificationHub')
-	.withAutomaticReconnect().configureLogging(signalR.LogLevel.Error)
-	.build();
+if (document.querySelector('.dropdown-notify')) {
+	const connection = new signalR.HubConnectionBuilder()
+		.withUrl('/NotificationHub')
+		.withAutomaticReconnect().configureLogging(signalR.LogLevel.Error)
+		.build();
 
-connection.on('newActivityAsync', (alerts: number) => {
-	const alert = document.getElementById('notificationAlert') as HTMLElement;
-	const notifyLink = document.getElementById('notificationLink') as HTMLElement;
+	connection.on('newActivityAsync', (alerts: number) => {
+		const alert = document.getElementById('notificationAlert') as HTMLElement;
+		const notifyLink = document.getElementById('notificationLink') as HTMLElement;
 
-	if (alerts > 0 && notifyLink.classList.contains('d-none')) {
-		notifyLink.classList.toggle('d-none');
-		alert.classList.toggle('d-none');
-	}
-});
+		if (alerts > 0 && notifyLink.classList.contains('d-none')) {
+			notifyLink.classList.toggle('d-none');
+			alert.classList.toggle('d-none');
+		}
+	});
 
-connection.start();
+	connection.start();
 
-document.addEventListener('DOMContentLoaded', () => {
-	const alert = document.getElementById('notificationAlert') as HTMLElement;
-	const notifyLink = document.getElementById('notificationLink') as HTMLElement;
+	document.addEventListener('DOMContentLoaded', () => {
+		const alert = document.getElementById('notificationAlert') as HTMLElement;
+		const notifyLink = document.getElementById('notificationLink') as HTMLElement;
 
-	if (alert !== null && parseInt(alert.dataset.alerts || '0', 10) > 0) {
-		notifyLink.classList.toggle('d-none');
-		alert.classList.toggle('d-none');
-	}
-});
+		if (alert !== null && parseInt(alert.dataset.alerts || '0', 10) > 0) {
+			notifyLink.classList.toggle('d-none');
+			alert.classList.toggle('d-none');
+		}
+	});
+}
