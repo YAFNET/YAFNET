3cb0ee7fbc01f8d3e84eaddf55350aced2dd4bfc
Ingo <thewatcher@watchersnet.de>
2026-04-26 07:22:22 +0200

    bump to 4.0.5

--- yafsrc/YAFNET.Core/Services/BoardInfo.cs
+++ yafsrc/YAFNET.Core/Services/BoardInfo.cs
@@ -57,7 +57,7 @@ public class BoardInfo(IServiceLocator serviceLocator) : IHaveServiceLocator
     /// <summary>
     /// Gets the Current YAF Build Date
     /// </summary>
-    public DateTime AppVersionDate { get; set; } = new (2026, 04, 23, 06, 11, 00, DateTimeKind.Utc);
+    public DateTime AppVersionDate { get; set; } = new (2026, 04, 26, 07, 16, 00, DateTimeKind.Utc);
 
     /// <summary>
     /// Creates a string that is the YAF Application Version from a long value
@@ -72,7 +72,7 @@ public class BoardInfo(IServiceLocator serviceLocator) : IHaveServiceLocator
             Major = 4,
             Minor = 0,
             Build = 5,
-            ReleaseType = ReleaseType.BETA,
+            ReleaseType = ReleaseType.Regular,
             ReleaseNumber = 0
         };
 
