4d16c02edc25285f64ad888a94a0188fec833034
Ingo <thewatcher@watchersnet.de>
2026-08-02 05:41:24 +0200

    bump to 3.2.14

--- yafsrc/YAF.Configuration/BoardInfo.cs
+++ yafsrc/YAF.Configuration/BoardInfo.cs
@@ -59,7 +59,7 @@ public static class BoardInfo
     /// <summary>
     /// Gets the Current YAF Build Date
     /// </summary>
-    public static DateTime AppVersionDate => new(2026, 07, 28, 15, 21, 00, DateTimeKind.Utc);
+    public static DateTime AppVersionDate => new(2026, 08, 02, 05, 28, 00, DateTimeKind.Utc);
 
     /// <summary>
     /// Creates a string that is the YAF Application Version from a long value
@@ -74,7 +74,7 @@ public static class BoardInfo
                               Major = 3,
                               Minor = 2,
                               Build = 14,
-                              ReleaseType = ReleaseType.BETA,
+                              ReleaseType = ReleaseType.Regular,
                               ReleaseNumber = 0
                           };
 
