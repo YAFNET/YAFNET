d5f470b885fa0071e3d524d26345bf73d46e25be
Ingo <thewatcher@watchersnet.de>
2026-08-09 11:14:06 +0200

    updated packages

--- yafsrc/YAF.Configuration/BoardInfo.cs
+++ yafsrc/YAF.Configuration/BoardInfo.cs
@@ -59,7 +59,7 @@ public static class BoardInfo
     /// <summary>
     /// Gets the Current YAF Build Date
     /// </summary>
-    public static DateTime AppVersionDate => new(2026, 08, 02, 05, 28, 00, DateTimeKind.Utc);
+    public static DateTime AppVersionDate => new(2026, 08, 09, 05, 28, 00, DateTimeKind.Utc);
 
     /// <summary>
     /// Creates a string that is the YAF Application Version from a long value
@@ -74,7 +74,7 @@ public static class BoardInfo
                               Major = 3,
                               Minor = 2,
                               Build = 14,
-                              ReleaseType = ReleaseType.Regular,
+                              ReleaseType = ReleaseType.BETA,
                               ReleaseNumber = 0
                           };
 
