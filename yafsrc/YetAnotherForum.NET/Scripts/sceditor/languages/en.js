e2eea25ba4523da7b6419294b066b71ba72046b0
Ingo <thewatcher@watchersnet.de>
2026-08-19 07:51:16 +0200

    updated sceditor

--- yafsrc/YetAnotherForum.NET/Scripts/sceditor/languages/en.js
+++ yafsrc/YetAnotherForum.NET/Scripts/sceditor/languages/en.js
@@ -1,3 +1,3 @@
-/* SCEditor v3.2.17 | (C) 2017-2026, Sam Clarke | sceditor.com/license */
-"use strict";sceditor.locale["en-GB"]={"Font Color":"Font Colour",Center:"Centre",dateFormat:"day/month/year"},sceditor.locale.en=sceditor.locale["en-GB"];
+/* SCEditor v3.2.18 | (C) 2017-2026, Sam Clarke | sceditor.com/license */
+!function(){"use strict";sceditor.locale["en-GB"]={"Font Color":"Font Colour",Center:"Centre",dateFormat:"day/month/year"},sceditor.locale.en=sceditor.locale["en-GB"]}();
 //# sourceMappingURL=en.js.map
